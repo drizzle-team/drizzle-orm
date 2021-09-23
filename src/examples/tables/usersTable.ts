b3d5e3e34139a3d6292094babf34effebdc60575
AndriiSherman <sherman@lambda.direct>
2021-09-23 21:04:40 +0300

    Move full User Model to examples

--- src/examples/tables/usersTable.ts
+++ src/examples/tables/usersTable.ts
@@ -5,16 +5,16 @@ import { rolesEnum } from '../types/rolesType';
 export default class UsersTable extends AbstractTable<UsersTable> {
   public id = this.int('id').autoIncrement().primaryKey();
   public phone = this.varchar('phone', { size: 256 });
-  // public fullName = this.varchar('full_name', { size: 512 });
-  // public test = this.decimal('test', { notNull: true, precision: 100, scale: 2 });
-  // public test1 = this.bigint('test1');
+  public fullName = this.varchar('full_name', { size: 512 });
+  public test = this.decimal('test', { notNull: true, precision: 100, scale: 2 });
+  public test1 = this.bigint('test1');
   public createdAt = this.timestamp('created_at', { notNull: true });
   public updatedAt = this.timestamp('updated_at');
 
   public role = this.type(rolesEnum, 'name_in_table', { notNull: true });
 
-  // public phoneFullNameIndex = this.index([this.phone, this.fullName]);
-  // public phoneIndex = this.index(this.phone);
+  public phoneFullNameIndex = this.index([this.phone, this.fullName]);
+  public phoneIndex = this.index(this.phone);
 
   public tableName(): string {
     return 'users';
