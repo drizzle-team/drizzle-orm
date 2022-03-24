d72e718a21d628a0f6a1ac39b6256b05e17de091
AndriiSherman <sherman@lambda.direct>
2022-03-24 19:37:27 +0200

    Add rawValue import from column

--- orm/src/columns/index.ts
+++ orm/src/columns/index.ts
@@ -1,4 +1,7 @@
-export { Column, ExtractColumnType, Defaults } from './column';
+/* eslint-disable import/no-cycle */
+export {
+  Column, ExtractColumnType, Defaults, rawValue,
+} from './column';
 export { default as PgBigDecimal } from './types/pgBigDecimal';
 export { default as PgBigInt } from './types/pgBigInt';
 export { default as PgBoolean } from './types/pgBoolean';
