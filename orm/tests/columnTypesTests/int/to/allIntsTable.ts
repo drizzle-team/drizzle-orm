02659c8d6cc9882b050a0302bec5ee6a7e1b95a1
BohdanSV <83227580+BogdanMGL@users.noreply.github.com>
2022-06-01 11:51:37 +0300

    complete suite  "Insert 1 -> Update 1 -> Delete 1" including exception cases (#21)
    * complete suite  "Insert 1 -> Update 1 -> Delete
    1" including exception cases
    
    * Added suite
    
    * removed console.log
    
    * added variables for default values
    
    Co-authored-by: AndriiSherman <sherman@lambda.direct>

--- orm/tests/columnTypesTests/int/to/allIntsTable.ts
+++ orm/tests/columnTypesTests/int/to/allIntsTable.ts
@@ -1,15 +1,21 @@
 import AbstractTable from '../../../../src/tables/abstractTable';
 
+export const defaultInt = 1;
+export const notNullWithDefault = 1;
+
 export default class AllIntsTable extends AbstractTable<AllIntsTable> {
   public primaryInt = this.int('primary_key').primaryKey();
   public serialInt = this.serial('serial_int');
 
   public simpleInt = this.int('simple_int');
   public notNullInt = this.int('not_null_int').notNull();
-  public intWithDefault = this.int('int_with_default').defaultValue(1);
-  public notNullIntWithDefault = this.int('not_null_int_with_default').notNull().defaultValue(1);
+  public intWithDefault = this.int('int_with_default').defaultValue(defaultInt);
+  public notNullIntWithDefault = this.int('not_null_int_with_default').notNull().defaultValue(notNullWithDefault);
   public uniqueInt = this.int('unique_int').unique();
   public notNullUniqueInt = this.int('not_null_unique_int').notNull().unique();
+
+  public simpleIntIndex = this.uniqueIndex(this.simpleInt);
+  public intWithDefaultIndex = this.index(this.intWithDefault);
 
   public tableName(): string {
     return 'table_with_all_ints';
