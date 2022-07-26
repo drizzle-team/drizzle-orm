271769f2ecd1f3733ace7799f662d01cd10fcb91
Dan Kochetov <danil.kochetov@gmail.com>
2022-07-26 13:11:34 +0300

    Remove brand from pg

--- drizzle-orm-pg/src/branded-types.ts
+++ drizzle-orm-pg/src/branded-types.ts
@@ -2,6 +2,4 @@ import { ColumnDriverParam } from 'drizzle-orm/branded-types';
 
 import { PgColumnDriverDataType } from './connection';
 
-export const brand = Symbol('brand');
-
 export type PgColumnDriverParam<T extends PgColumnDriverDataType = PgColumnDriverDataType> = ColumnDriverParam<T>;
