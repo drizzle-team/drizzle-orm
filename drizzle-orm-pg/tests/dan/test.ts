addc32338d4f13390c3f4052750271aac421df12
Dan Kochetov <danil.kochetov@gmail.com>
2022-08-06 19:12:25 +0300

    Add 1:1 relation with FK example

--- drizzle-orm-pg/tests/dan/test.ts
+++ drizzle-orm-pg/tests/dan/test.ts
@@ -181,15 +181,3 @@ Expect<
 		};
 	}[], typeof join2>
 >;
-
-// const complexJoin1 = await db.users.select({ id: users.id }).leftJoin();
-
-const test1 = pgTable('test1_table', {
-	id: serial('id').primaryKey(),
-	test2Id: integer('test2_id').references(() => test2.id),
-});
-
-const test2 = pgTable('test2_table', {
-	id: serial('id').primaryKey(),
-	test1Id: integer('test1_id'), /* .references(() => test1.id) */
-});
