1d0e1eb6453ceddedd399cdaccdfcc69f41177ab
Alex Blokh <aleksandrblokh@gmail.com>
2022-11-21 17:17:34 +0200

    cloudflare test fixes

--- cloudflare/index.ts
+++ cloudflare/index.ts
@@ -24,25 +24,55 @@ interface Methods {
 }
 
 function injectDB(request: Request, env: Env) {
-	const db = new SQLiteD1Connector(env.DB).connect();
-	request.db = db;
+	// const db = new SQLiteD1Connector(env.DB).connect();
+	// request.db = db;
 }
 
-const router = Router<Request, Methods>({ base: '/users' });
+const router = Router<Request, Methods>({ base: '/' });
 
-router.get('/', injectDB, async (req: Request, env: Env) => {
-	const { db } = req;
-	// const rows = await db.select(users).execute();
-	const rows = await env.DB.prepare('select * from users').all();
+router.get('/all', injectDB, async (req: Request, env: Env) => {
+	const rows = await env.DB.prepare('select id as z, email as y, name as x, lower(name), upper(email) from users').all();
+	console.log(rows);
 	return json(rows);
 });
 
-router.post('/', injectDB, async (req: Request, env: Env) => {
-	const { db } = req;
+router.get('/raw', injectDB, async (req: Request, env: Env) => {
+	const rows = await env.DB.prepare('select id as z, email as y, name as x, lower(name), upper(email) from users').raw();
+	console.log(rows);
+	return json(rows);
+});
+
+router.post('/1', injectDB, async (req: Request, env: Env) => {
 	const { name, email } = await req.json!();
-	// await db.insert(users).values({ name, email }).execute();
-	await env.DB.prepare('insert into users (name, email) values (?, ?)').bind(name, email).run();
-	return json({ status: 'ok' });
+	const res = await env.DB.prepare('insert into users (name, email) values (?, ?) returning *')
+		.bind(name, email)
+		.run();
+	
+	return json({ res });
+});
+
+router.post('/2', injectDB, async (req: Request, env: Env) => {
+	const { name, email } = await req.json!();
+	const res = await env.DB.prepare('insert into users (name, email) values (?, ?) returning *')
+		.bind(name, email)
+		.all();
+	return json({ res });
+});
+
+router.post('/3', injectDB, async (req: Request, env: Env) => {
+	const { name, email } = await req.json!();
+	const res = await env.DB.prepare('insert into users (name, email) values (?, ?) returning *')
+		.bind(name, email)
+		.first();
+	return json({ res });
+});
+
+router.post('/4', injectDB, async (req: Request, env: Env) => {
+	const { name, email } = await req.json!();
+	const res = await env.DB.prepare('insert into users (name, email) values (?, ?) returning *')
+		.bind(name, email)
+		.raw();
+	return json({ res });
 });
 
 export default {
