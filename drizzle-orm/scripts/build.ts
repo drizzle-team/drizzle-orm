f98a2c0b8f1926c47aa1c4a15f1f4ff75b0c2a32
Dan Kochetov <danil.kochetov@gmail.com>
2023-04-25 20:31:30 +0300

    Add missing fields to "exports" in package.json
    Closes #511

--- drizzle-orm/scripts/build.ts
+++ drizzle-orm/scripts/build.ts
@@ -5,13 +5,17 @@ import { entries } from '../rollup.common';
 function updateAndCopyPackageJson() {
 	const pkg = fs.readJSONSync('package.json');
 
-	pkg.exports = entries.reduce<Record<string, { import: string; require: string }>>((acc, entry) => {
-		const exportsEntry = entry === 'index' ? '.' : './' + entry.replace(/\/index$/, '');
-		const importEntry = `./${entry}.mjs`;
-		const requireEntry = `./${entry}.cjs`;
-		acc[exportsEntry] = { import: importEntry, require: requireEntry };
-		return acc;
-	}, {});
+	pkg.exports = entries.reduce<Record<string, { import: string; require: string; default: string; types: string }>>(
+		(acc, entry) => {
+			const exportsEntry = entry === 'index' ? '.' : './' + entry.replace(/\/index$/, '');
+			const importEntry = `./${entry}.mjs`;
+			const requireEntry = `./${entry}.cjs`;
+			const typesEntry = `./${entry}.d.ts`;
+			acc[exportsEntry] = { import: importEntry, require: requireEntry, default: importEntry, types: typesEntry };
+			return acc;
+		},
+		{},
+	);
 
 	fs.writeJSONSync('dist/package.json', pkg, { spaces: 2 });
 }
