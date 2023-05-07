ee114d7aacd7330ba9d5d3127fb56413c6392f73
AndriiSherman <andreysherman11@gmail.com>
2023-05-07 18:50:20 +0300

    Broken infer

---
+++ integration-tests/tests/relational/pg.schema.ts
@@ -0,0 +1,128 @@
+import {
+  type AnyPgColumn,
+  bigint,
+  boolean,
+  pgTable,
+  primaryKey,
+  serial,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
+
+import { relations } from "drizzle-orm";
+
+export const usersTable = pgTable("users", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  verified: boolean("verified").notNull().default(false),
+  invitedBy: bigint("invited_by", { mode: "number" }).references(
+    (): AnyPgColumn => usersTable.id
+  ),
+});
+export const usersConfig = relations(usersTable, ({ one, many }) => ({
+  invitee: one(usersTable, {
+    fields: [usersTable.invitedBy],
+    references: [usersTable.id],
+  }),
+  groups: many(groupsTable),
+  posts: many(postsTable),
+}));
+
+export const groupsTable = pgTable("groups", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  description: text("description"),
+});
+export const groupsConfig = relations(groupsTable, ({ many }) => ({
+  users: many(usersTable),
+}));
+
+export const usersToGroupsTable = pgTable(
+  "users_to_groups",
+  {
+    id: serial("id").primaryKey(),
+    userId: bigint("user_id", { mode: "number" }).references(
+      () => usersTable.id
+    ),
+    groupId: bigint("group_id", { mode: "number" }).references(
+      () => groupsTable.id
+    ),
+  },
+  (t) => ({
+    pk: primaryKey(t.userId, t.groupId),
+  })
+);
+export const usersToGroupsConfig = relations(usersToGroupsTable, ({ one }) => ({
+  group: one(groupsTable, {
+    fields: [usersToGroupsTable.groupId],
+    references: [groupsTable.id],
+  }),
+  user: one(usersTable, {
+    fields: [usersToGroupsTable.userId],
+    references: [usersTable.id],
+  }),
+}));
+
+export const postsTable = pgTable("posts", {
+  id: serial("id").primaryKey(),
+  content: text("content").notNull(),
+  ownerId: bigint("owner_id", { mode: "number" }).references(
+    () => usersTable.id
+  ),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+});
+export const postsConfig = relations(postsTable, ({ one, many }) => ({
+  author: one(usersTable, {
+    fields: [postsTable.ownerId],
+    references: [usersTable.id],
+  }),
+  comments: many(commentsTable),
+}));
+
+export const commentsTable = pgTable("comments", {
+  id: serial("id").primaryKey(),
+  content: text("content").notNull(),
+  creator: bigint("creator", { mode: "number" }).references(
+    () => usersTable.id
+  ),
+  postId: bigint("post_id", { mode: "number" }).references(() => postsTable.id),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+});
+export const commentsConfig = relations(commentsTable, ({ one, many }) => ({
+  post: one(postsTable, {
+    fields: [commentsTable.postId],
+    references: [postsTable.id],
+  }),
+  author: one(usersTable, {
+    fields: [commentsTable.creator],
+    references: [usersTable.id],
+  }),
+  likes: many(commentLikesTable),
+}));
+
+export const commentLikesTable = pgTable("comment_likes", {
+  id: serial("id").primaryKey(),
+  creator: bigint("creator", { mode: "number" }).references(
+    () => usersTable.id
+  ),
+  commentId: bigint("comment_id", { mode: "number" }).references(
+    () => commentsTable.id
+  ),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+});
+export const commentLikesConfig = relations(commentLikesTable, ({ one }) => ({
+  comment: one(commentsTable, {
+    fields: [commentLikesTable.commentId],
+    references: [commentsTable.id],
+  }),
+  author: one(usersTable, {
+    fields: [commentLikesTable.creator],
+    references: [usersTable.id],
+  }),
+}));
