ced4daf6efa99f525e2fd8461beba223e30e98cd
AndriiSherman <andreysherman11@gmail.com>
2023-05-07 23:27:35 +0300

    K & string are friends now

--- drizzle-orm/src/relations.ts
+++ drizzle-orm/src/relations.ts
@@ -178,7 +178,7 @@ export type TablesRelationalConfig = Record<string, TableRelationalConfig>;
 
 export type ExtractTablesWithRelations<TSchema extends Record<string, unknown>> = {
 	[K in keyof TSchema as TSchema[K] extends Table ? K : never]: TSchema[K] extends Table ? {
-			tsName: string;
+			tsName: K & string;
 			dbName: TSchema[K]['_']['name'];
 			columns: TSchema[K]['_']['columns'];
 			relations: ExtractTableRelationsFromSchema<TSchema, TSchema[K]['_']['name']>;
