d4b863577cc11db67c60c2f9482706ac67442a08
MrRahulRamkumar <rahulawesomeramkumar@gmail.com>
2023-05-07 15:32:27 +0530

    Updated `libsql.test.ts`

--- integration-tests/tests/libsql.test.ts
+++ integration-tests/tests/libsql.test.ts
@@ -97,6 +97,12 @@ const _pkExample = sqliteTable('pk_example', {
 	compositePk: primaryKey(table.id, table.name),
 }));
 
+const bigIntExample = sqliteTable('big_int_example', {
+  id: integer('id').primaryKey(),
+  name: text('name').notNull(),
+  bigInt: blob('big_int', { mode: 'bigint' }).notNull()
+});
+
 test.before(async (t) => {
 	const ctx = t.context;
 	const url = process.env['LIBSQL_URL'];
@@ -139,6 +145,7 @@ test.beforeEach(async (t) => {
 	await ctx.db.run(sql`drop table if exists ${coursesTable}`);
 	await ctx.db.run(sql`drop table if exists ${courseCategoriesTable}`);
 	await ctx.db.run(sql`drop table if exists ${orders}`);
+	await ctx.db.run(sql`drop table if exists ${bigIntExample}`);
 
 	await ctx.db.run(sql`
 		create table ${usersTable} (
@@ -186,6 +193,34 @@ test.beforeEach(async (t) => {
 			quantity integer not null
 		)
 	`);
+
+  await ctx.db.run(sql`
+    create table ${bigIntExample} (
+      id integer primary key,
+      name text not null,
+      big_int integer not null
+    )
+  `);
+});
+
+test.serial('insert bigint values', async (t) => {
+  const { db } = t.context;
+
+  await db.insert(bigIntExample).values({ name: 'one', bigInt: BigInt("0") }).run();
+  await db.insert(bigIntExample).values({ name: 'two', bigInt: BigInt("127") }).run();
+  await db.insert(bigIntExample).values({ name: 'three', bigInt: BigInt("32767") }).run();
+  await db.insert(bigIntExample).values({ name: 'four', bigInt: BigInt("1234567890") }).run();
+  await db.insert(bigIntExample).values({ name: 'five', bigInt: BigInt("12345678900987654321") }).run();
+
+  const result = await db.select().from(bigIntExample).all();
+  console.log(result)
+   t.deepEqual(result, [
+    { id: 1, name: 'one', bigInt: BigInt("0") },
+    { id: 2, name: 'two', bigInt: BigInt("127") },
+    { id: 3, name: 'three', bigInt: BigInt("32767") },
+    { id: 4, name: 'four', bigInt: BigInt("1234567890") },
+	{ id: 5, name: 'five', bigInt: BigInt("12345678900987654321") },
+  ]);
 });
 
 test.serial('select all fields', async (t) => {
