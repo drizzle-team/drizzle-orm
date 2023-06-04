2335b3edc9a82dd0adbbd4ccd23ef90125c07d40
LeonAlvarez <leon@leon-design.es>
2023-06-04 09:35:56 +0200

    fix postgres.js.tests.ts

--- integration-tests/tests/postgres.js.test.ts
+++ integration-tests/tests/postgres.js.test.ts
@@ -771,7 +771,7 @@ test.serial('build query insert with onConflict do update', async (t) => {
 
 	t.deepEqual(query, {
 		sql: 'insert into "users" ("name", "jsonb") values ($1, $2) on conflict ("id") do update set "name" = $3',
-		params: ['John', '["foo","bar"]', 'John1'],
+		params: ['John', ["foo","bar"], 'John1'],
 	});
 });
 
@@ -785,7 +785,7 @@ test.serial('build query insert with onConflict do update / multiple columns', a
 
 	t.deepEqual(query, {
 		sql: 'insert into "users" ("name", "jsonb") values ($1, $2) on conflict ("id","name") do update set "name" = $3',
-		params: ['John', '["foo","bar"]', 'John1'],
+		params: ['John', ["foo","bar"], 'John1'],
 	});
 });
 
@@ -799,7 +799,7 @@ test.serial('build query insert with onConflict do nothing', async (t) => {
 
 	t.deepEqual(query, {
 		sql: 'insert into "users" ("name", "jsonb") values ($1, $2) on conflict do nothing',
-		params: ['John', '["foo","bar"]'],
+		params: ['John', ["foo","bar"]],
 	});
 });
 
@@ -813,7 +813,7 @@ test.serial('build query insert with onConflict do nothing + target', async (t) 
 
 	t.deepEqual(query, {
 		sql: 'insert into "users" ("name", "jsonb") values ($1, $2) on conflict ("id") do nothing',
-		params: ['John', '["foo","bar"]'],
+		params: ['John', ["foo","bar"]],
 	});
 });
 
