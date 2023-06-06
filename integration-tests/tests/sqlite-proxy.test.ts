0f55e0416f4fb69a20c3b50ae7899d6f8aa50fa4
Neilzon <neilcviloria@gmail.com>
2023-06-05 23:07:42 -0600

    add sqlite proxy tests

--- integration-tests/tests/sqlite-proxy.test.ts
+++ integration-tests/tests/sqlite-proxy.test.ts
@@ -1112,3 +1112,28 @@ test.serial('select + .get() for empty result', async (t) => {
 
 	await db.run(sql`drop table ${users}`);
 });
+test.serial('update by limit', async (t) => {
+  const { db } = t.context;
+
+  const users = sqliteTable('users', {
+    id: integer('id').primaryKey(),
+    name: text('name'),
+  });
+
+  db.run(sql`drop table if exists ${users}`);
+  db.run(sql`create table ${users} (id integer primary key, name text)`);
+
+  db.insert(users).values({ id: 16, name: 'John' }).run();
+  db.insert(users).values({ id: 42, name: 'Avery' }).run();
+
+  t.notThrows(async () => await db.update(users).set({ name: 'JJ Abrams' }).limit(1).run());
+
+  const allUsers = await db.select().from(users).all();
+
+  t.deepEqual(allUsers, [
+    { id: 16, name: 'JJ Abrams' },
+    { id: 42, name: 'Avery' },
+  ]);
+
+  db.run(sql`drop table ${users}`);
+});
