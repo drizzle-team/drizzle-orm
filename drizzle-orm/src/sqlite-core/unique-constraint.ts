19fe0100150b1cac0d9e69944844de2e7c7e5b26
AndriiSherman <andreysherman11@gmail.com>
2023-07-06 18:21:58 +0300

    Add name option to sqlite unique constraints

--- drizzle-orm/src/sqlite-core/unique-constraint.ts
+++ drizzle-orm/src/sqlite-core/unique-constraint.ts
@@ -2,8 +2,12 @@ import { entityKind } from '~/entity';
 import { type AnySQLiteTable, SQLiteTable } from './table';
 import { type AnySQLiteColumn } from './columns';
 
-export function unique(): UniqueOnConstraintBuilder {
-	return new UniqueOnConstraintBuilder();
+export function uniqueKeyName(table: AnySQLiteTable, columns: string[]) {
+	return `${table[SQLiteTable.Symbol.Name]}_${columns.join('_')}_unique`
+}
+
+export function unique(name?: string): UniqueOnConstraintBuilder {
+	return new UniqueOnConstraintBuilder(name);
 }
 
 export class UniqueConstraintBuilder {
@@ -21,15 +25,24 @@ export class UniqueConstraintBuilder {
 
 	/** @internal */
 	build(table: AnySQLiteTable): UniqueConstraint {
-		return new UniqueConstraint(table, this.columns);
+		return new UniqueConstraint(table, this.columns, this.name);
 	}
 }
 
 export class UniqueOnConstraintBuilder {
 	static readonly [entityKind]: string = 'SQLiteUniqueOnConstraintBuilder';
 
+	/** @internal */
+	name?: string;
+
+	constructor(
+		name?: string,
+	) {
+		this.name = name;
+	}
+
 	on(...columns: [AnySQLiteColumn, ...AnySQLiteColumn[]]) {
-		return new UniqueConstraintBuilder(columns);
+		return new UniqueConstraintBuilder(columns, this.name);
 	}
 }
 
@@ -37,9 +50,11 @@ export class UniqueConstraint {
 	static readonly [entityKind]: string = 'SQLiteUniqueConstraint';
 
 	readonly columns: AnySQLiteColumn<{}>[];
+	readonly name?: string;
 
-	constructor(readonly table: AnySQLiteTable, columns: AnySQLiteColumn<{}>[]) {
+	constructor(readonly table: AnySQLiteTable, columns: AnySQLiteColumn<{}>[], name?: string) {
 		this.columns = columns;
+		this.name = name ?? uniqueKeyName(this.table, this.columns.map((column) => column.name));
 	}
 
 	getName() {
