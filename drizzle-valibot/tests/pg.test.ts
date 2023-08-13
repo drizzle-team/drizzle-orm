29bf83ba3f0d1df1b8df8a95bbf1e65449d7eb3c
Bulbang <b.tkachuk26@gmail.com>
2023-08-13 19:39:54 +0300

    Valibot compatibility

---
+++ drizzle-valibot/tests/pg.test.ts
@@ -0,0 +1,196 @@
+import test from 'ava';
+import {
+	char,
+	date,
+	integer,
+	pgEnum,
+	pgTable,
+	serial,
+	text,
+	timestamp,
+	varchar,
+} from 'drizzle-orm/pg-core';
+import { createInsertSchema, createSelectSchema } from '../src';
+import { expectSchemaShape } from './utils';
+import {
+	array,
+	email,
+	enumType,
+	maxLength,
+	minLength,
+	minValue,
+	nullable,
+	number,
+	object,
+	optional,
+	parse,
+	string,
+	date as valiDate,
+} from 'valibot';
+
+export const roleEnum = pgEnum('role', ['admin', 'user']);
+
+const users = pgTable('users', {
+	a: integer('a').array(),
+	id: serial('id').primaryKey(),
+	name: text('name'),
+	email: text('email').notNull(),
+	birthdayString: date('birthday_string').notNull(),
+	birthdayDate: date('birthday_date', { mode: 'date' }).notNull(),
+	createdAt: timestamp('created_at').notNull().defaultNow(),
+	role: roleEnum('role').notNull(),
+	roleText: text('role1', { enum: ['admin', 'user'] }).notNull(),
+	roleText2: text('role2', { enum: ['admin', 'user'] })
+		.notNull()
+		.default('user'),
+	profession: varchar('profession', { length: 20 }).notNull(),
+	initials: char('initials', { length: 2 }).notNull(),
+});
+
+const testUser = {
+	a: [1, 2, 3],
+	id: 1,
+	name: 'John Doe',
+	email: 'john.doe@example.com',
+	birthdayString: '1990-01-01',
+	birthdayDate: new Date('1990-01-01'),
+	createdAt: new Date(),
+	role: 'admin' as const,
+	roleText: 'admin' as const,
+	roleText2: 'admin' as const,
+	profession: 'Software Engineer',
+	initials: 'JD',
+};
+
+test('users insert valid user', (t) => {
+	const schema = createInsertSchema(users);
+
+	t.deepEqual(parse(schema, testUser), testUser);
+});
+
+test('users insert invalid varchar', (t) => {
+	const schema = createInsertSchema(users);
+
+	t.throws(
+		() =>
+			parse(schema, {
+				...testUser,
+				profession: 'Chief Executive Officer',
+			}),
+		undefined
+	);
+});
+
+test('users insert invalid char', (t) => {
+	const schema = createInsertSchema(users);
+
+	t.throws(() => parse(schema, { ...testUser, initials: 'JoDo' }), undefined);
+});
+
+test('users insert schema', (t) => {
+	const actual = createInsertSchema(users, {
+		id: () => number([minValue(0)]),
+		email: () => string([email()]),
+		roleText: enumType(['user', 'manager', 'admin']),
+	});
+
+	() => {
+		{
+			createInsertSchema(users, {
+				// @ts-expect-error (missing property)
+				foobar: number(),
+			});
+		}
+
+		{
+			createInsertSchema(users, {
+				// @ts-expect-error (invalid type)
+				id: 123,
+			});
+		}
+	};
+
+	const expected = object({
+		a: optional(nullable(array(number()))),
+		id: optional(number([minValue(0)])),
+		name: optional(nullable(string())),
+		email: string(),
+		birthdayString: string(),
+		birthdayDate: valiDate(),
+		createdAt: optional(valiDate()),
+		role: enumType(['admin','user']),
+		roleText: enumType(['user', 'manager', 'admin']),
+		roleText2: optional(enumType([ 'admin', 'user'])),
+		profession: string([maxLength(20), minLength(1)]),
+		initials: string([maxLength(2), minLength(1)]),
+	});
+
+	expectSchemaShape(t, expected).from(actual);
+});
+
+test('users insert schema w/ defaults', (t) => {
+	const actual = createInsertSchema(users);
+
+	const expected = object({
+		a: optional(nullable(array(number()))),
+		id: optional(number()),
+		name: optional(nullable(string())),
+		email: string(),
+		birthdayString: string(),
+		birthdayDate: valiDate(),
+		createdAt: optional(valiDate()),
+		role: enumType(['admin','user']),
+		roleText: enumType(['admin', 'user', ]),
+		roleText2: optional(enumType([ 'admin', 'user'])),
+		profession: string([maxLength(20), minLength(1)]),
+		initials: string([maxLength(2), minLength(1)]),
+	});
+
+	expectSchemaShape(t, expected).from(actual);
+});
+
+test('users select schema', (t) => {
+	const actual = createSelectSchema(users, {
+		id: () => number([minValue(0)]),
+		email: () => string(),
+		roleText: enumType(['user', 'manager', 'admin']),
+	});
+
+	const expected = object({
+		a: nullable(array(number())),
+		id: number([minValue(0)]),
+		name: nullable(string()),
+		email: string(),
+		birthdayString: string(),
+		birthdayDate: valiDate(),
+		createdAt: valiDate(),
+		role: enumType(['admin', 'user',]),
+		roleText: enumType(['user', 'manager', 'admin']),
+		roleText2: enumType(['admin', 'user', ]),
+		profession: string([maxLength(20), minLength(1)]),
+		initials: string([maxLength(2), minLength(1)]),
+	});
+
+	expectSchemaShape(t, expected).from(actual);
+});
+
+test('users select schema w/ defaults', (t) => {
+	const actual = createSelectSchema(users);
+
+	const expected = object({
+		a: nullable(array(number())),
+		id: number(),
+		name: nullable(string()),
+		email: string(),
+		birthdayString: string(),
+		birthdayDate: valiDate(),
+		createdAt: valiDate(),
+		role: enumType([ 'admin', 'user',]),
+		roleText: enumType(['admin', 'user',]),
+		roleText2: enumType(['admin', 'user',]),
+		profession: string([maxLength(20), minLength(1)]),
+		initials: string([maxLength(2), minLength(1)]),
+	});
+
+	expectSchemaShape(t, expected).from(actual);
+});
---
+++ drizzle-valibot/tests/sqlite.test.ts
@@ -0,0 +1,188 @@
+import test from 'ava';
+import {
+	blob,
+	integer,
+	numeric,
+	real,
+	sqliteTable,
+	text,
+} from 'drizzle-orm/sqlite-core';
+import { createInsertSchema, createSelectSchema, jsonSchema } from '../src';
+import { expectSchemaShape } from './utils';
+import {
+	type Output,
+	enumType,
+	minValue,
+	number,
+	object,
+	parse,
+	string,
+	bigint as valibigint,
+	date as valiDate,
+	nullable,
+	boolean,
+	optional,
+} from 'valibot';
+
+const blobJsonSchema = object({
+	foo: string(),
+});
+
+const users = sqliteTable('users', {
+	id: integer('id').primaryKey(),
+	blobJson: blob('blob', { mode: 'json' })
+		.$type<Output<typeof blobJsonSchema>>()
+		.notNull(),
+	blobBigInt: blob('blob', { mode: 'bigint' }).notNull(),
+	numeric: numeric('numeric').notNull(),
+	createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
+	createdAtMs: integer('created_at_ms', { mode: 'timestamp_ms' }).notNull(),
+	boolean: integer('boolean', { mode: 'boolean' }).notNull(),
+	real: real('real').notNull(),
+	text: text('text', { length: 255 }),
+	role: text('role', { enum: ['admin', 'user'] })
+		.notNull()
+		.default('user'),
+});
+
+const testUser = {
+	id: 1,
+	blobJson: { foo: 'bar' },
+	blobBigInt: BigInt(123),
+	numeric: '123.45',
+	createdAt: new Date(),
+	createdAtMs: new Date(),
+	boolean: true,
+	real: 123.45,
+	text: 'foobar',
+	role: 'admin' as const,
+};
+
+test('users insert valid user', (t) => {
+	const schema = createInsertSchema(users);
+	//
+	t.deepEqual(parse(schema, testUser), testUser);
+});
+
+test('users insert invalid text length', (t) => {
+	const schema = createInsertSchema(users);
+	t.throws(
+		() => parse(schema, { ...testUser, text: 'a'.repeat(256) }),
+		undefined
+	);
+});
+
+test('users insert schema', (t) => {
+	const actual = createInsertSchema(users, {
+		id: () => number([minValue(0)]),
+		blobJson: blobJsonSchema,
+		role: enumType(['admin', 'user', 'manager']),
+	});
+
+	() => {
+		{
+			createInsertSchema(users, {
+				// @ts-expect-error (missing property)
+				foobar: number(),
+			});
+		}
+
+		{
+			createInsertSchema(users, {
+				// @ts-expect-error (invalid type)
+				id: 123,
+			});
+		}
+	};
+
+	const expected = object({
+		id: optional(number([minValue(0)])),
+		blobJson: blobJsonSchema,
+		blobBigInt: valibigint(),
+		numeric: string(),
+		createdAt: valiDate(),
+		createdAtMs: valiDate(),
+		boolean: boolean(),
+		real: number(),
+		text: optional(nullable(string())),
+		role: optional(enumType(['admin', 'user', 'manager'])),
+	});
+
+	expectSchemaShape(t, expected).from(actual);
+});
+
+test('users insert schema w/ defaults', (t) => {
+	const actual = createInsertSchema(users);
+
+	const expected = object({
+		id: optional(number()),
+		blobJson: jsonSchema,
+		blobBigInt: valibigint(),
+		numeric: string(),
+		createdAt: valiDate(),
+		createdAtMs: valiDate(),
+		boolean: boolean(),
+		real: number(),
+		text: optional(nullable(string())),
+		role: optional(enumType(['admin', 'user'])),
+	});
+
+	expectSchemaShape(t, expected).from(actual);
+});
+
+test('users select schema', (t) => {
+	const actual = createSelectSchema(users, {
+		blobJson: jsonSchema,
+		role: enumType(['admin', 'user', 'manager']),
+	});
+
+	() => {
+		{
+			createSelectSchema(users, {
+				// @ts-expect-error (missing property)
+				foobar: number(),
+			});
+		}
+
+		{
+			createSelectSchema(users, {
+				// @ts-expect-error (invalid type)
+				id: 123,
+			});
+		}
+	};
+
+	const expected = object({
+		id: number(),
+		blobJson: jsonSchema,
+		blobBigInt: valibigint(),
+		numeric: string(),
+		createdAt: valiDate(),
+		createdAtMs: valiDate(),
+		boolean: boolean(),
+		real: number(),
+		text: nullable(string()),
+		role: enumType(['admin', 'user', 'manager']),
+	});
+
+	expectSchemaShape(t, expected).from(actual);
+});
+
+test('users select schema w/ defaults', (t) => {
+	const actual = createSelectSchema(users);
+
+	const expected = object({
+		id: number(),
+		blobJson: jsonSchema,
+		blobBigInt: valibigint(),
+		numeric: string(),
+		createdAt: valiDate(),
+		createdAtMs: valiDate(),
+		boolean: boolean(),
+		real: number(),
+		text: nullable(string()),
+		role: enumType(['admin', 'user']),
+	});
+
+	expectSchemaShape(t, expected).from(actual);
+});
