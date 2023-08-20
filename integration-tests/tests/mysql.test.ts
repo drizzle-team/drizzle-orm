988a8b9eeeef99147643c6223c256136b3a7fb8b
Angelelz <angellopez20@gmail.com>
2023-08-20 15:26:42 -0400

    added tests for utc config

--- integration-tests/tests/mysql.test.ts
+++ integration-tests/tests/mysql.test.ts
@@ -1908,3 +1908,46 @@ test.serial('update undefined', async (t) => {
 
 	await db.execute(sql`drop table ${users}`);
 });
+
+test.serial('utc config for datetime', async (t) => {
+	const { db } = t.context;
+
+	await db.execute(sql`drop table if exists \`datestable\``);
+	await db.execute(
+		sql`
+			create table \`datestable\` (
+				\`datetime_utc\` datetime(3),
+				\`datetime\` datetime(3),
+				\`datetime_as_string\` datetime
+			)
+		`,
+	);
+  const datesTable2 = mysqlTable('datestable', {
+    datetimeUTC: datetime('datetime_utc', { fsp: 3, utc: true }),
+    datetime: datetime('datetime', { fsp: 3 }),
+    datetimeAsString: datetime('datetime_as_string', { mode: 'string' }),
+  });
+
+	const dateObj = new Date('2022-11-11');
+  const dateUtc = new Date(Date.now());
+
+	await db.insert(datesTable2).values({
+    datetimeUTC: dateUtc,
+    datetime: dateObj,
+		datetimeAsString: '2022-11-11 12:12:12',
+	});
+
+	const res = await db.select().from(datesTable2);
+
+	t.assert(res[0]?.datetime instanceof Date); // eslint-disable-line no-instanceof/no-instanceof
+	t.assert(res[0]?.datetimeUTC instanceof Date); // eslint-disable-line no-instanceof/no-instanceof
+	t.assert(typeof res[0]?.datetimeAsString === 'string');
+
+	t.deepEqual(res, [{
+    datetimeUTC: dateUtc,
+		datetime: new Date('2022-11-11'),
+		datetimeAsString: '2022-11-11 12:12:12',
+	}]);
+
+	await db.execute(sql`drop table if exists \`datestable\``);
+});
