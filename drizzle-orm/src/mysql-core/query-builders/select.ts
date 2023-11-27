190159e928b73923fd0f0943775973734286fd31
realmikesolo <aeryoff@gmail.com>
2023-11-27 10:20:04 +0100

    Add jsdoc for MySQL set operations

--- drizzle-orm/src/mysql-core/query-builders/select.ts
+++ drizzle-orm/src/mysql-core/query-builders/select.ts
@@ -409,16 +409,196 @@ export abstract class MySqlSelectQueryBuilderBase<
 		};
 	}
 
+	/**
+	 * Adds `union` set operator to the query.
+	 * 
+	 * Calling this method will combine the result sets of the `select` statements and remove any duplicate rows that appear across them.
+	 * 
+	 * See docs: {@link https://orm.drizzle.team/docs/set-operations#union}
+	 * 
+	 * @example
+	 * 
+	 * ```ts
+	 * // Select all unique names from customers and users tables
+	 * await db.select({ name: users.name })
+	 *   .from(users)
+	 *   .union(
+	 *     db.select({ name: customers.name }).from(customers)
+	 *   );
+	 * // or
+	 * import { union } from 'drizzle-orm/mysql-core'
+	 * 
+	 * await union(
+	 *   db.select({ name: users.name }).from(users), 
+	 *   db.select({ name: customers.name }).from(customers)
+	 * );
+	 * ```
+	 */
 	union = this.createSetOperator('union', false);
 
+	/**
+	 * Adds `union all` set operator to the query.
+	 * 
+	 * Calling this method will combine the result-set of the `select` statements and keep all duplicate rows that appear across them.
+	 * 
+	 * See docs: {@link https://orm.drizzle.team/docs/set-operations#union-all}
+	 * 
+	 * @example
+	 * 
+	 * ```ts
+	 * // Select all transaction ids from both online and in-store sales
+	 * await db.select({ transaction: onlineSales.transactionId })
+	 *   .from(onlineSales)
+	 *   .unionAll(
+	 *     db.select({ transaction: inStoreSales.transactionId }).from(inStoreSales)
+	 *   );
+	 * // or
+	 * import { unionAll } from 'drizzle-orm/mysql-core'
+	 * 
+	 * await unionAll(
+	 *   db.select({ transaction: onlineSales.transactionId }).from(onlineSales),
+	 *   db.select({ transaction: inStoreSales.transactionId }).from(inStoreSales)
+	 * );
+	 * ```
+	 */
 	unionAll = this.createSetOperator('union', true);
 
+	/**
+	 * Adds `intersect` set operator to the query.
+	 * 
+	 * Calling this method will retain only the rows that are present in both result sets and eliminate duplicates.
+	 * 
+	 * See docs: {@link https://orm.drizzle.team/docs/set-operations#intersect}
+	 * 
+	 * @example
+	 * 
+	 * ```ts
+	 * // Select course names that are offered in both departments A and B
+	 * await db.select({ courseName: depA.courseName })
+	 *   .from(depA)
+	 *   .intersect(
+	 *     db.select({ courseName: depB.courseName }).from(depB)
+	 *   );
+	 * // or
+	 * import { intersect } from 'drizzle-orm/mysql-core'
+	 * 
+	 * await intersect(
+	 *   db.select({ courseName: depA.courseName }).from(depA),
+	 *   db.select({ courseName: depB.courseName }).from(depB)
+	 * );
+	 * ```
+	 */
 	intersect = this.createSetOperator('intersect', false);
 
+	/**
+	 * Adds `intersect all` set operator to the query.
+	 * 
+	 * Calling this method will retain only the rows that are present in both result sets including all duplicates.
+	 * 
+	 * See docs: {@link https://orm.drizzle.team/docs/set-operations#intersect-all}
+	 * 
+	 * @example
+	 * 
+	 * ```ts
+	 * // Select all products and quantities that are ordered by both regular and VIP customers
+	 * await db.select({ 
+	 *   productId: regularCustomerOrders.productId, 
+	 *   quantityOrdered: regularCustomerOrders.quantityOrdered
+	 * })
+	 * .from(regularCustomerOrders)
+	 * .intersectAll(
+	 *   db.select({ 
+	 *     productId: vipCustomerOrders.productId, 
+	 *     quantityOrdered: vipCustomerOrders.quantityOrdered 
+	 *   })
+	 *   .from(vipCustomerOrders)
+	 * );
+	 * // or
+	 * import { intersectAll } from 'drizzle-orm/mysql-core'
+	 * 
+	 * await intersectAll(
+	 *   db.select({
+	 *     productId: regularCustomerOrders.productId,
+	 *     quantityOrdered: regularCustomerOrders.quantityOrdered
+	 *   })
+	 *   .from(regularCustomerOrders),
+	 *   db.select({
+	 *     productId: vipCustomerOrders.productId,
+	 *     quantityOrdered: vipCustomerOrders.quantityOrdered
+	 *   })
+	 *   .from(vipCustomerOrders)
+	 * );
+	 * ```
+	 */
 	intersectAll = this.createSetOperator('intersect', true);
 
+	/**
+	 * Adds `except` set operator to the query.
+	 * 
+	 * Calling this method will retrieve all unique rows from the left query, except for the rows that are present in the result set of the right query.
+	 * 
+	 * See docs: {@link https://orm.drizzle.team/docs/set-operations#except}
+	 * 
+	 * @example
+	 * 
+	 * ```ts
+	 * // Select all courses offered in department A but not in department B
+	 * await db.select({ courseName: depA.courseName })
+	 *   .from(depA)
+	 *   .except(
+	 *     db.select({ courseName: depB.courseName }).from(depB)
+	 *   );
+	 * // or
+	 * import { except } from 'drizzle-orm/mysql-core'
+	 * 
+	 * await except(
+	 *   db.select({ courseName: depA.courseName }).from(depA),
+	 *   db.select({ courseName: depB.courseName }).from(depB)
+	 * );
+	 * ```
+	 */
 	except = this.createSetOperator('except', false);
 
+	/**
+	 * Adds `except all` set operator to the query.
+	 * 
+	 * Calling this method will retrieve all rows from the left query, except for the rows that are present in the result set of the right query.
+	 * 
+	 * See docs: {@link https://orm.drizzle.team/docs/set-operations#except-all}
+	 * 
+	 * @example
+	 * 
+	 * ```ts
+	 * // Select all products that are ordered by regular customers but not by VIP customers
+	 * await db.select({
+	 *   productId: regularCustomerOrders.productId,
+	 *   quantityOrdered: regularCustomerOrders.quantityOrdered,
+	 * })
+	 * .from(regularCustomerOrders)
+	 * .exceptAll(
+	 *   db.select({
+	 *     productId: vipCustomerOrders.productId,
+	 *     quantityOrdered: vipCustomerOrders.quantityOrdered,
+	 *   })
+	 *   .from(vipCustomerOrders)
+	 * );
+	 * // or
+	 * import { exceptAll } from 'drizzle-orm/mysql-core'
+	 * 
+	 * await exceptAll(
+	 *   db.select({
+	 *     productId: regularCustomerOrders.productId,
+	 *     quantityOrdered: regularCustomerOrders.quantityOrdered
+	 *   })
+	 *   .from(regularCustomerOrders),
+	 *   db.select({
+	 *     productId: vipCustomerOrders.productId,
+	 *     quantityOrdered: vipCustomerOrders.quantityOrdered
+	 *   })
+	 *   .from(vipCustomerOrders)
+	 * );
+	 * ```
+	 */
 	exceptAll = this.createSetOperator('except', true);
 
 	/** @internal */
@@ -822,14 +1002,194 @@ const getMySqlSetOperators = () => ({
 	exceptAll,
 });
 
+/**
+ * Adds `union` set operator to the query.
+ * 
+ * Calling this method will combine the result sets of the `select` statements and remove any duplicate rows that appear across them.
+ * 
+ * See docs: {@link https://orm.drizzle.team/docs/set-operations#union}
+ * 
+ * @example
+ * 
+ * ```ts
+ * // Select all unique names from customers and users tables
+ * import { union } from 'drizzle-orm/mysql-core'
+ * 
+ * await union(
+ *   db.select({ name: users.name }).from(users), 
+ *   db.select({ name: customers.name }).from(customers)
+ * );
+ * // or
+ * await db.select({ name: users.name })
+ *   .from(users)
+ *   .union(
+ *     db.select({ name: customers.name }).from(customers)
+ *   );
+ * ```
+ */
 export const union = createSetOperator('union', false);
 
+/**
+ * Adds `union all` set operator to the query.
+ * 
+ * Calling this method will combine the result-set of the `select` statements and keep all duplicate rows that appear across them.
+ * 
+ * See docs: {@link https://orm.drizzle.team/docs/set-operations#union-all}
+ * 
+ * @example
+ * 
+ * ```ts
+ * // Select all transaction ids from both online and in-store sales
+ * import { unionAll } from 'drizzle-orm/mysql-core'
+ * 
+ * await unionAll(
+ *   db.select({ transaction: onlineSales.transactionId }).from(onlineSales),
+ *   db.select({ transaction: inStoreSales.transactionId }).from(inStoreSales)
+ * );
+ * // or
+ * await db.select({ transaction: onlineSales.transactionId })
+ *   .from(onlineSales)
+ *   .unionAll(
+ *     db.select({ transaction: inStoreSales.transactionId }).from(inStoreSales)
+ *   );
+ * ```
+ */
 export const unionAll = createSetOperator('union', true);
 
+/**
+ * Adds `intersect` set operator to the query.
+ * 
+ * Calling this method will retain only the rows that are present in both result sets and eliminate duplicates.
+ * 
+ * See docs: {@link https://orm.drizzle.team/docs/set-operations#intersect}
+ * 
+ * @example
+ * 
+ * ```ts
+ * // Select course names that are offered in both departments A and B
+ * import { intersect } from 'drizzle-orm/mysql-core'
+ * 
+ * await intersect(
+ *   db.select({ courseName: depA.courseName }).from(depA),
+ *   db.select({ courseName: depB.courseName }).from(depB)
+ * );
+ * // or
+ * await db.select({ courseName: depA.courseName })
+ *   .from(depA)
+ *   .intersect(
+ *     db.select({ courseName: depB.courseName }).from(depB)
+ *   );
+ * ```
+ */
 export const intersect = createSetOperator('intersect', false);
 
+/**
+ * Adds `intersect all` set operator to the query.
+ * 
+ * Calling this method will retain only the rows that are present in both result sets including all duplicates.
+ * 
+ * See docs: {@link https://orm.drizzle.team/docs/set-operations#intersect-all}
+ * 
+ * @example
+ * 
+ * ```ts
+ * // Select all products and quantities that are ordered by both regular and VIP customers
+ * import { intersectAll } from 'drizzle-orm/mysql-core'
+ * 
+ * await intersectAll(
+ *   db.select({
+ *     productId: regularCustomerOrders.productId,
+ *     quantityOrdered: regularCustomerOrders.quantityOrdered
+ *   })
+ *   .from(regularCustomerOrders),
+ *   db.select({
+ *     productId: vipCustomerOrders.productId,
+ *     quantityOrdered: vipCustomerOrders.quantityOrdered
+ *   })
+ *   .from(vipCustomerOrders)
+ * );
+ * // or
+ * await db.select({ 
+ *   productId: regularCustomerOrders.productId, 
+ *   quantityOrdered: regularCustomerOrders.quantityOrdered
+ * })
+ * .from(regularCustomerOrders)
+ * .intersectAll(
+ *   db.select({ 
+ *     productId: vipCustomerOrders.productId, 
+ *     quantityOrdered: vipCustomerOrders.quantityOrdered 
+ *   })
+ *   .from(vipCustomerOrders)
+ * );
+ * ```
+ */
 export const intersectAll = createSetOperator('intersect', true);
 
+/**
+ * Adds `except` set operator to the query.
+ * 
+ * Calling this method will retrieve all unique rows from the left query, except for the rows that are present in the result set of the right query.
+ * 
+ * See docs: {@link https://orm.drizzle.team/docs/set-operations#except}
+ * 
+ * @example
+ * 
+ * ```ts
+ * // Select all courses offered in department A but not in department B
+ * import { except } from 'drizzle-orm/mysql-core'
+ * 
+ * await except(
+ *   db.select({ courseName: depA.courseName }).from(depA),
+ *   db.select({ courseName: depB.courseName }).from(depB)
+ * );
+ * // or
+ * await db.select({ courseName: depA.courseName })
+ *   .from(depA)
+ *   .except(
+ *     db.select({ courseName: depB.courseName }).from(depB)
+ *   );
+ * ```
+ */
 export const except = createSetOperator('except', false);
 
+/**
+ * Adds `except all` set operator to the query.
+ * 
+ * Calling this method will retrieve all rows from the left query, except for the rows that are present in the result set of the right query.
+ * 
+ * See docs: {@link https://orm.drizzle.team/docs/set-operations#except-all}
+ * 
+ * @example
+ * 
+ * ```ts
+ * // Select all products that are ordered by regular customers but not by VIP customers
+ * import { exceptAll } from 'drizzle-orm/mysql-core'
+ * 
+ * await exceptAll(
+ *   db.select({
+ *     productId: regularCustomerOrders.productId,
+ *     quantityOrdered: regularCustomerOrders.quantityOrdered
+ *   })
+ *   .from(regularCustomerOrders),
+ *   db.select({
+ *     productId: vipCustomerOrders.productId,
+ *     quantityOrdered: vipCustomerOrders.quantityOrdered
+ *   })
+ *   .from(vipCustomerOrders)
+ * );
+ * // or
+ * await db.select({
+ *   productId: regularCustomerOrders.productId,
+ *   quantityOrdered: regularCustomerOrders.quantityOrdered,
+ * })
+ * .from(regularCustomerOrders)
+ * .exceptAll(
+ *   db.select({
+ *     productId: vipCustomerOrders.productId,
+ *     quantityOrdered: vipCustomerOrders.quantityOrdered,
+ *   })
+ *   .from(vipCustomerOrders)
+ * );
+ * ```
+ */
 export const exceptAll = createSetOperator('except', true);
