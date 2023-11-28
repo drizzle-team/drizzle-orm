dfc7442a2f5e77b30398de0f11b15d2cdec3387e
Angelelz <angellopez20@gmail.com>
2023-11-28 01:56:00 -0500

    fix types

--- eslint-plugin-drizzle/src/index.ts
+++ eslint-plugin-drizzle/src/index.ts
@@ -4,11 +4,12 @@ import all from './configs/all';
 import recommended from './configs/recommended';
 import deleteRule from './enforce-delete-with-where';
 import updateRule from './enforce-update-with-where';
+import type { Options } from './utils/options';
 
 export const rules = {
 	'enforce-delete-with-where': deleteRule,
 	'enforce-update-with-where': updateRule,
-} satisfies Record<string, TSESLint.RuleModule<string, Array<unknown>>>;
+} satisfies Record<string, TSESLint.RuleModule<string, Options>>;
 
 export const configs = { all, recommended };
 
