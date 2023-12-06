741f58ebec0656cd7e9954ae1acd0e60b5a04822
Angelelz <angellopez20@gmail.com>
2023-12-06 02:00:15 -0500

    [MsSql] Fixed string data types

--- drizzle-orm/src/mssql-core/columns/char.ts
+++ drizzle-orm/src/mssql-core/columns/char.ts
@@ -5,25 +5,28 @@ import type { AnyMsSqlTable } from '~/mssql-core/table.ts';
 import type { Writable } from '~/utils.ts';
 import { MsSqlColumn, MsSqlColumnBuilder } from './common.ts';
 
-export type MsSqlCharBuilderInitial<TName extends string, TEnum extends [string, ...string[]]> = MsSqlCharBuilder<{
-	name: TName;
-	dataType: 'string';
-	columnType: 'MsSqlChar';
-	data: TEnum[number];
-	driverParam: number | string;
-	enumValues: TEnum;
-}>;
+export type MsSqlCharBuilderInitial<TName extends string, TEnum extends [string, ...string[]]> = MsSqlCharBuilder<
+	{
+		name: TName;
+		dataType: 'string';
+		columnType: 'MsSqlChar';
+		data: TEnum[number];
+		driverParam: number | string;
+		enumValues: TEnum;
+	}
+>;
 
-export class MsSqlCharBuilder<T extends ColumnBuilderBaseConfig<'string', 'MsSqlChar'>> extends MsSqlColumnBuilder<
-	T,
-	MsSqlCharConfig<T['enumValues']>
-> {
+export class MsSqlCharBuilder<T extends ColumnBuilderBaseConfig<'string', 'MsSqlChar'>>
+	extends MsSqlColumnBuilder<T, MsSqlCharConfig<T['enumValues']>>
+{
 	static readonly [entityKind]: string = 'MsSqlCharBuilder';
 
+	/** @internal */
 	constructor(name: T['name'], config: MsSqlCharConfig<T['enumValues']>) {
 		super(name, 'string', 'MsSqlChar');
 		this.config.length = config.length;
 		this.config.enum = config.enum;
+		this.config.nonUnicode = config.nonUnicode;
 	}
 
 	/** @internal */
@@ -43,21 +46,51 @@ export class MsSqlChar<T extends ColumnBaseConfig<'string', 'MsSqlChar'>>
 	static readonly [entityKind]: string = 'MsSqlChar';
 
 	readonly length: number | undefined = this.config.length;
+
 	override readonly enumValues = this.config.enum;
 
+	readonly nonUnicode: boolean = this.config.nonUnicode;
+
 	getSQLType(): string {
-		return this.length === undefined ? `char` : `char(${this.length})`;
+		return this.length === undefined
+			? this.nonUnicode ? `nchar` : `char`
+			: this.nonUnicode
+			? `nchar(${this.length})`
+			: `char(${this.length})`;
 	}
 }
 
-export interface MsSqlCharConfig<TEnum extends readonly string[] | string[] | undefined> {
-	length?: number;
+export type MsSqlCharConfig<TEnum extends string[] | readonly string[] | undefined> =
+	& MsSqlCharConfigInitial<TEnum>
+	& {
+		nonUnicode: boolean;
+	};
+
+export type MsSqlCharConfigInitial<
+	TEnum extends string[] | readonly string[] | undefined,
+> = {
+	length: number;
 	enum?: TEnum;
-}
+};
 
 export function char<TName extends string, U extends string, T extends Readonly<[U, ...U[]]>>(
 	name: TName,
-	config: MsSqlCharConfig<T | Writable<T>> = {},
+	config: MsSqlCharConfigInitial<T | Writable<T>>,
 ): MsSqlCharBuilderInitial<TName, Writable<T>> {
-	return new MsSqlCharBuilder(name, config);
+	return new MsSqlCharBuilder(name, { ...config, nonUnicode: false });
+}
+
+export function nChar<
+	TName extends string,
+	U extends string,
+	T extends Readonly<[U, ...U[]]>,
+>(
+	name: TName,
+	config: MsSqlCharConfigInitial<T | Writable<T>>,
+): MsSqlCharBuilderInitial<TName, Writable<T>> {
+	return new MsSqlCharBuilder(name, {
+		length: config.length,
+		enum: config.enum,
+		nonUnicode: true,
+	});
 }
