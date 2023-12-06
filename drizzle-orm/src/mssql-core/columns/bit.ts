a58400021fd3774856ed77fd89f89f247698a697
Angelelz <angellopez20@gmail.com>
2023-12-06 01:59:43 -0500

    [MsSql] Created but data type

---
+++ drizzle-orm/src/mssql-core/columns/bit.ts
@@ -0,0 +1,45 @@
+import type { ColumnBuilderBaseConfig, ColumnBuilderRuntimeConfig, MakeColumnConfig } from '~/column-builder.ts';
+import type { ColumnBaseConfig } from '~/column.ts';
+import { entityKind } from '~/entity.ts';
+import type { AnyMsSqlTable } from '~/mssql-core/table.ts';
+import { MsSqlColumnBuilderWithIdentity, MsSqlColumnWithIdentity } from './common.ts';
+
+export type MsSqlBitBuilderInitial<TName extends string> = MsSqlBitBuilder<{
+	name: TName;
+	dataType: 'boolean';
+	columnType: 'MsSqlBit';
+	data: boolean;
+	driverParam: number | string;
+	enumValues: undefined;
+}>;
+
+export class MsSqlBitBuilder<T extends ColumnBuilderBaseConfig<'boolean', 'MsSqlBit'>>
+	extends MsSqlColumnBuilderWithIdentity<T>
+{
+	static readonly [entityKind]: string = 'MsSqlBitBuilder';
+
+	constructor(name: T['name']) {
+		super(name, 'boolean', 'MsSqlBit');
+	}
+
+	/** @internal */
+	override build<TTableName extends string>(
+		table: AnyMsSqlTable<{ name: TTableName }>,
+	): MsSqlBit<MakeColumnConfig<T, TTableName>> {
+		return new MsSqlBit<MakeColumnConfig<T, TTableName>>(table, this.config as ColumnBuilderRuntimeConfig<any, any>);
+	}
+}
+
+export class MsSqlBit<T extends ColumnBaseConfig<'boolean', 'MsSqlBit'>> extends MsSqlColumnWithIdentity<T> {
+	static readonly [entityKind]: string = 'MsSqlBit';
+
+	_getSQLType(): string {
+		return `bit`;
+	}
+
+	override mapFromDriverValue = Boolean;
+}
+
+export function bit<TName extends string>(name: TName): MsSqlBitBuilderInitial<TName> {
+	return new MsSqlBitBuilder(name);
+}
