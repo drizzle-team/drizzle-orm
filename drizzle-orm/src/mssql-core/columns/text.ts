abb7dc415eb1f680c4454a3f8db34955df778063
Angelelz <angellopez20@gmail.com>
2023-12-06 02:00:15 -0500

    [MsSql] Fixed string data types

--- drizzle-orm/src/mssql-core/columns/text.ts
+++ drizzle-orm/src/mssql-core/columns/text.ts
@@ -5,8 +5,6 @@ import type { AnyMsSqlTable } from '~/mssql-core/table.ts';
 import type { Writable } from '~/utils.ts';
 import { MsSqlColumn, MsSqlColumnBuilder } from './common.ts';
 
-export type MsSqlTextColumnType = 'tinytext' | 'text' | 'mediumtext' | 'longtext';
-
 export type MsSqlTextBuilderInitial<TName extends string, TEnum extends [string, ...string[]]> = MsSqlTextBuilder<{
 	name: TName;
 	dataType: 'string';
@@ -18,14 +16,15 @@ export type MsSqlTextBuilderInitial<TName extends string, TEnum extends [string,
 
 export class MsSqlTextBuilder<T extends ColumnBuilderBaseConfig<'string', 'MsSqlText'>> extends MsSqlColumnBuilder<
 	T,
-	{ textType: MsSqlTextColumnType; enumValues: T['enumValues'] }
+	{ length: number | undefined; enumValues: T['enumValues']; nonUnicode: boolean }
 > {
 	static readonly [entityKind]: string = 'MsSqlTextBuilder';
 
-	constructor(name: T['name'], textType: MsSqlTextColumnType, config: MsSqlTextConfig<T['enumValues']>) {
+	constructor(name: T['name'], config: MsSqlTextConfig<T['enumValues']> & { nonUnicode: boolean }) {
 		super(name, 'string', 'MsSqlText');
-		this.config.textType = textType;
 		this.config.enumValues = config.enum;
+		this.config.length = config.length;
+		this.config.nonUnicode = config.nonUnicode;
 	}
 
 	/** @internal */
@@ -37,47 +36,53 @@ export class MsSqlTextBuilder<T extends ColumnBuilderBaseConfig<'string', 'MsSql
 }
 
 export class MsSqlText<T extends ColumnBaseConfig<'string', 'MsSqlText'>>
-	extends MsSqlColumn<T, { textType: MsSqlTextColumnType; enumValues: T['enumValues'] }>
+	extends MsSqlColumn<T, { length: number | undefined; enumValues: T['enumValues']; nonUnicode: boolean }>
 {
 	static readonly [entityKind]: string = 'MsSqlText';
 
-	private textType: MsSqlTextColumnType = this.config.textType;
-
 	override readonly enumValues = this.config.enumValues;
 
+	readonly length: number | undefined = this.config.length;
+
+	readonly nonUnicode: boolean = this.config.nonUnicode;
+
+	constructor(
+		table: AnyMsSqlTable<{ name: T['tableName'] }>,
+		config: MsSqlTextBuilder<T>['config'],
+	) {
+		super(table, config);
+	}
+
 	getSQLType(): string {
-		return this.textType;
+		return `${this.nonUnicode ? 'n' : ''}text${this.config.length ? `(${this.config.length})` : ''}`;
 	}
 }
 
-export interface MsSqlTextConfig<TEnum extends readonly string[] | string[] | undefined> {
+export type MsSqlTextConfig<
+	TEnum extends readonly string[] | string[] | undefined,
+> = {
+	length?: number;
 	enum?: TEnum;
-}
+};
 
-export function text<TName extends string, U extends string, T extends Readonly<[U, ...U[]]>>(
+export function text<
+	TName extends string,
+	U extends string,
+	T extends Readonly<[U, ...U[]]>,
+>(
 	name: TName,
 	config: MsSqlTextConfig<T | Writable<T>> = {},
 ): MsSqlTextBuilderInitial<TName, Writable<T>> {
-	return new MsSqlTextBuilder(name, 'text', config);
+	return new MsSqlTextBuilder(name, { ...config, nonUnicode: false });
 }
 
-export function tinytext<TName extends string, U extends string, T extends Readonly<[U, ...U[]]>>(
+export function nText<
+	TName extends string,
+	U extends string,
+	T extends Readonly<[U, ...U[]]>,
+>(
 	name: TName,
 	config: MsSqlTextConfig<T | Writable<T>> = {},
 ): MsSqlTextBuilderInitial<TName, Writable<T>> {
-	return new MsSqlTextBuilder(name, 'tinytext', config);
-}
-
-export function mediumtext<TName extends string, U extends string, T extends Readonly<[U, ...U[]]>>(
-	name: TName,
-	config: MsSqlTextConfig<T | Writable<T>> = {},
-): MsSqlTextBuilderInitial<TName, Writable<T>> {
-	return new MsSqlTextBuilder(name, 'mediumtext', config);
-}
-
-export function longtext<TName extends string, U extends string, T extends Readonly<[U, ...U[]]>>(
-	name: TName,
-	config: MsSqlTextConfig<T | Writable<T>> = {},
-): MsSqlTextBuilderInitial<TName, Writable<T>> {
-	return new MsSqlTextBuilder(name, 'longtext', config);
+	return new MsSqlTextBuilder(name, { ...config, nonUnicode: true });
 }
--- drizzle-orm/src/mssql-core/columns/varchar.ts
+++ drizzle-orm/src/mssql-core/columns/varchar.ts
@@ -2,7 +2,7 @@ import type { ColumnBuilderBaseConfig, ColumnBuilderRuntimeConfig, MakeColumnCon
 import type { ColumnBaseConfig } from '~/column.ts';
 import { entityKind } from '~/entity.ts';
 import type { AnyMsSqlTable } from '~/mssql-core/table.ts';
-import type { Writable } from '~/utils.ts';
+import type { Equal, Writable } from '~/utils.ts';
 import { MsSqlColumn, MsSqlColumnBuilder } from './common.ts';
 
 export type MsSqlVarCharBuilderInitial<TName extends string, TEnum extends [string, ...string[]]> = MsSqlVarCharBuilder<
@@ -16,16 +16,28 @@ export type MsSqlVarCharBuilderInitial<TName extends string, TEnum extends [stri
 	}
 >;
 
+export type MsSqlVarCharJsonBuilderInitial<TName extends string> = MsSqlVarCharJsonBuilder<
+	{
+		name: TName;
+		dataType: 'json';
+		columnType: 'MsSqlNVarCharJson';
+		data: unknown;
+		driverParam: string;
+		enumValues: undefined;
+	}
+>;
+
 export class MsSqlVarCharBuilder<T extends ColumnBuilderBaseConfig<'string', 'MsSqlVarChar'>>
-	extends MsSqlColumnBuilder<T, MsSqlVarCharConfig<T['enumValues']>>
+	extends MsSqlColumnBuilder<T, MsSqlVarCharConfig<'text', T['enumValues']>>
 {
 	static readonly [entityKind]: string = 'MsSqlVarCharBuilder';
 
 	/** @internal */
-	constructor(name: T['name'], config: MsSqlVarCharConfig<T['enumValues']>) {
+	constructor(name: T['name'], config: MsSqlVarCharConfig<'text', T['enumValues']>) {
 		super(name, 'string', 'MsSqlVarChar');
 		this.config.length = config.length;
 		this.config.enum = config.enum;
+		this.config.nonUnicode = config.nonUnicode;
 	}
 
 	/** @internal */
@@ -40,7 +52,7 @@ export class MsSqlVarCharBuilder<T extends ColumnBuilderBaseConfig<'string', 'Ms
 }
 
 export class MsSqlVarChar<T extends ColumnBaseConfig<'string', 'MsSqlVarChar'>>
-	extends MsSqlColumn<T, MsSqlVarCharConfig<T['enumValues']>>
+	extends MsSqlColumn<T, MsSqlVarCharConfig<'text', T['enumValues']>>
 {
 	static readonly [entityKind]: string = 'MsSqlVarChar';
 
@@ -48,19 +60,104 @@ export class MsSqlVarChar<T extends ColumnBaseConfig<'string', 'MsSqlVarChar'>>
 
 	override readonly enumValues = this.config.enum;
 
+	readonly nonUnicode: boolean = this.config.nonUnicode;
+
 	getSQLType(): string {
-		return this.length === undefined ? `varchar` : `varchar(${this.length})`;
+		return this.length === undefined
+			? this.nonUnicode ? `nvarchar` : `varchar`
+			: this.nonUnicode
+			? `nvarchar(${this.length})`
+			: `varchar(${this.length})`;
 	}
 }
 
-export interface MsSqlVarCharConfig<TEnum extends string[] | readonly string[] | undefined> {
-	length: number;
-	enum?: TEnum;
+export class MsSqlVarCharJsonBuilder<T extends ColumnBuilderBaseConfig<'json', 'MsSqlNVarCharJson'>>
+	extends MsSqlColumnBuilder<T, { length: number | undefined; nonUnicode: boolean }>
+{
+	static readonly [entityKind]: string = 'MsSqlVarCharJsonBuilder';
+
+	/** @internal */
+	constructor(name: T['name'], config: { length: number | undefined }) {
+		super(name, 'json', 'MsSqlNVarCharJson');
+		this.config.length = config.length;
+		this.config.nonUnicode = true;
+	}
+
+	/** @internal */
+	override build<TTableName extends string>(
+		table: AnyMsSqlTable<{ name: TTableName }>,
+	): MsSqlVarCharJson<MakeColumnConfig<T, TTableName>> {
+		return new MsSqlVarCharJson<MakeColumnConfig<T, TTableName>>(
+			table,
+			this.config as ColumnBuilderRuntimeConfig<any, any>,
+		);
+	}
 }
+
+export class MsSqlVarCharJson<T extends ColumnBaseConfig<'json', 'MsSqlNVarCharJson'>>
+	extends MsSqlColumn<T, { length: number | undefined; nonUnicode: boolean }>
+{
+	static readonly [entityKind]: string = 'MsSqlVarCharJson';
+
+	readonly length: number | undefined = this.config.length;
+
+	getSQLType(): string {
+		return this.length === undefined
+			? `nvarchar`
+			: `nvarchar(${this.length})`;
+	}
+
+	override mapFromDriverValue(value: string): T['data'] {
+		return JSON.parse(value);
+	}
+
+	override mapToDriverValue(value: T['data']): string {
+		return JSON.stringify(value);
+	}
+}
+
+export type MsSqlVarCharConfig<TMode extends 'text' | 'json', TEnum extends string[] | readonly string[] | undefined> =
+	& MsSqlVarCharConfigInitial<TMode, TEnum>
+	& {
+		nonUnicode: boolean;
+	};
+
+export type MsSqlVarCharConfigInitial<
+	TMode extends 'text' | 'json',
+	TEnum extends string[] | readonly string[] | undefined,
+> = TMode extends 'text' ? {
+		mode?: TMode;
+		length: number;
+		enum?: TEnum;
+	}
+	: {
+		mode?: TMode;
+		length: number;
+	};
 
 export function varchar<TName extends string, U extends string, T extends Readonly<[U, ...U[]]>>(
 	name: TName,
-	config: MsSqlVarCharConfig<T | Writable<T>>,
+	config: MsSqlVarCharConfigInitial<'text', T | Writable<T>>,
 ): MsSqlVarCharBuilderInitial<TName, Writable<T>> {
-	return new MsSqlVarCharBuilder(name, config);
+	return new MsSqlVarCharBuilder(name, { ...config, nonUnicode: false });
+}
+
+export function nVarchar<
+	TName extends string,
+	U extends string,
+	T extends Readonly<[U, ...U[]]>,
+	TMode extends 'text' | 'json' = 'text' | 'json',
+>(
+	name: TName,
+	config: MsSqlVarCharConfigInitial<TMode, T | Writable<T>>,
+): Equal<TMode, 'json'> extends true ? MsSqlVarCharJsonBuilderInitial<TName>
+	: MsSqlVarCharBuilderInitial<TName, Writable<T>>
+{
+	return config.mode === 'json'
+		? new MsSqlVarCharJsonBuilder(name, { length: config.length })
+		: new MsSqlVarCharBuilder(name, {
+			length: config.length,
+			enum: (config as any).enum,
+			nonUnicode: true,
+		}) as any;
 }
