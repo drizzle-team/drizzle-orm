fca2a10bbc1b128528e42f788f688a42e995e1f1
Angelelz <angellopez20@gmail.com>
2023-12-09 00:41:37 -0500

    [MsSql] Added support for `$onUpdate` and added tests

--- drizzle-orm/src/mssql-core/dialect.ts
+++ drizzle-orm/src/mssql-core/dialect.ts
@@ -92,20 +92,38 @@ export class MsSqlDialect {
 	}
 
 	buildUpdateSet(table: MsSqlTable, set: UpdateSet): SQL {
-		const setEntries = Object.entries(set);
+		const tableColumns = table[Table.Symbol.Columns];
 
-		const setSize = setEntries.length;
-		return sql.join(
-			setEntries
-				.flatMap(([colName, value], i): SQL[] => {
-					const col: MsSqlColumn = table[Table.Symbol.Columns][colName]!;
-					const res = sql`${sql.identifier(col.name)} = ${value}`;
-					if (i < setSize - 1) {
-						return [res, sql.raw(', ')];
-					}
-					return [res];
-				}),
+		const columnNames = Object.keys(tableColumns).filter((colName) =>
+			set[colName] !== undefined || tableColumns[colName]?.onUpdateFn !== undefined
 		);
+
+		const setSize = columnNames.length;
+		return sql.join(columnNames.flatMap((colName, i) => {
+			const col = tableColumns[colName]!;
+
+			const value = set[colName] ?? sql.param(col.onUpdateFn!(), col);
+			const res = sql`${sql.identifier(col.name)} = ${value}`;
+
+			if (i < setSize - 1) {
+				return [res, sql.raw(', ')];
+			}
+			return [res];
+		}));
+		// const setEntries = Object.entries(set);
+		//
+		// const setSize = setEntries.length;
+		// return sql.join(
+		// 	setEntries
+		// 		.flatMap(([colName, value], i): SQL[] => {
+		// 			const col: MsSqlColumn = table[Table.Symbol.Columns][colName]!;
+		// 			const res = sql`${sql.identifier(col.name)} = ${value}`;
+		// 			if (i < setSize - 1) {
+		// 				return [res, sql.raw(', ')];
+		// 			}
+		// 			return [res];
+		// 		}),
+		// );
 	}
 
 	buildUpdateQuery({ table, set, where, returning }: MsSqlUpdateConfig): SQL {
@@ -414,11 +432,14 @@ export class MsSqlDialect {
 			for (const [fieldName, col] of colEntries) {
 				const colValue = value[fieldName];
 				if (colValue === undefined || (is(colValue, Param) && colValue.value === undefined)) {
-					// eslint-disable-next-line unicorn/no-negated-condition
 					if (col.defaultFn !== undefined) {
 						const defaultFnResult = col.defaultFn();
 						const defaultValue = is(defaultFnResult, SQL) ? defaultFnResult : sql.param(defaultFnResult, col);
 						valueList.push(defaultValue);
+					} else if (!col.default && col.onUpdateFn !== undefined) {
+						const onUpdateFnResult = col.onUpdateFn();
+						const newValue = is(onUpdateFnResult, SQL) ? onUpdateFnResult : sql.param(onUpdateFnResult, col);
+						valueList.push(newValue);
 					} else {
 						valueList.push(sql`default`);
 					}
