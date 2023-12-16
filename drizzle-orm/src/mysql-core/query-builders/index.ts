586c637cb884b053a95c0723c63b0a8bc1062943
Angelelz <angellopez20@gmail.com>
2023-12-16 14:57:04 -0500

    Added query builders to the index.ts files to all the dialects

--- drizzle-orm/src/mysql-core/query-builders/index.ts
+++ drizzle-orm/src/mysql-core/query-builders/index.ts
@@ -1,6 +1,7 @@
 export * from './delete.ts';
 export * from './insert.ts';
 export * from './query-builder.ts';
+export * from './query.ts';
 export * from './select.ts';
 export * from './select.types.ts';
 export * from './update.ts';
