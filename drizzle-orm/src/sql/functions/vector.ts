22b9f9b5c403bc19e73a35e68035f1db5f29047d
Simon Sardorf <ssardorf@gmail.com>
2024-04-08 11:24:56 +0200

    Add jsdoc

--- drizzle-orm/src/sql/functions/vector.ts
+++ drizzle-orm/src/sql/functions/vector.ts
@@ -1,24 +1,71 @@
 import { AnyColumn } from "~/index";
-import { SQLWrapper, sql } from "../sql.ts";
+import { SQL, SQLWrapper, sql } from "../sql.ts";
 
 function toSql(value: number[]): string {
     return JSON.stringify(value);
 }
 
-export function l2Distance(column: SQLWrapper | AnyColumn, value: number[]) {
+
+/**
+ * Used in sorting, this specifies that the given
+ * column or expression should be sorted in an order 
+ * that is most similar to the given value in terms of L2 distance.
+ *
+ * ## Examples
+ *
+ * ```ts
+ * // Sort cars by embedding similarity
+ * // to the given embedding
+ * db.select().from(cars)
+ *   .orderBy(l2Distance(cars.embedding, embedding));
+ * ```
+ * 
+ */
+export function l2Distance(column: SQLWrapper | AnyColumn, value: number[]): SQL {
     return sql`${column} <-> ${toSql(value)}`;
 }
 
+
+/**
+ * Used in sorting, this specifies that the given
+ * column or expression should be sorted in an order 
+ * that maximizes the inner product with the given value.
+ *
+ * ## Examples
+ *
+ * ```ts
+ * // Sort cars by embedding similarity
+ * // to the given embedding
+ * db.select().from(cars)
+ *   .orderBy(maxInnerProduct(cars.embedding, embedding));
+ * ```
+ * 
+ */
 export function maxInnerProduct(
     column: SQLWrapper | AnyColumn,
     value: number[]
-) {
+): SQL {
     return sql`${column} <#> ${toSql(value)}`;
 }
 
+/**
+ * Used in sorting, this specifies that the given
+ * column or expression should be sorted in an order 
+ * that is most similar to the given value in terms of cosine distance.
+ *
+ * ## Examples
+ *
+ * ```ts
+ * // Sort cars by embedding similarity
+ * // to the given embedding
+ * db.select().from(cars)
+ *   .orderBy(cosineDistance(cars.embedding, embedding));
+ * ```
+ * 
+ */
 export function cosineDistance(
     column: SQLWrapper | AnyColumn,
     value: number[]
-) {
+): SQL {
     return sql`${column} <=> ${toSql(value)}`;
 }
