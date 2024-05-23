c7963ca9a41c2aa7195e138aae70bbf88759aa7f
AndriiSherman <andreysherman11@gmail.com>
2024-05-23 18:04:50 +0300

    Fix tests

--- integration-tests/vitest.config.ts
+++ integration-tests/vitest.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
 			'tests/replicas/**/*',
 			'tests/imports/**/*',
 			'tests/xata-http.test.ts',
+			'tests/extension/vectors/**/*',
 			// 'tests/awsdatapi.test.ts',
 		],
 		exclude: [
