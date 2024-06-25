e7cf33810834fc6d5413193a32eb5ce0487168b3
AndriiSherman <andreysherman11@gmail.com>
2024-06-25 20:41:54 +0300

    Get version 7 back

--- drizzle-orm/src/version.ts
+++ drizzle-orm/src/version.ts
@@ -1,4 +1,4 @@
 // @ts-ignore - imported using Rollup json plugin
 export { version as npmVersion } from '../package.json';
 // In version 7, we changed the PostgreSQL indexes API
-export const compatibilityVersion = 8;
+export const compatibilityVersion = 7;
