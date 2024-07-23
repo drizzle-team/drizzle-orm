f7f75e4175ca0b2ec45ed59bc2427346dac735e7
Mitchell Adair <madair@singlestore.com>
2024-07-23 12:59:57 -0400

    add uuid column

---
+++ drizzle-orm/src/singlestore-core/columns/uuid.ts
@@ -0,0 +1,45 @@
+import { ColumnBaseConfig } from "~/column";
+import { ColumnBuilderBaseConfig, ColumnBuilderRuntimeConfig, MakeColumnConfig } from "~/column-builder";
+import { entityKind } from "~/entity";
+import { SingleStoreColumn, SingleStoreColumnBuilder } from "./common";
+import { AnySingleStoreTable } from "~/singlestore-core/table";
+
+
+export type SingleStoreUUIDBuilderInitial<TName extends string> = SingleStoreUUIDBuilder<{
+    name: TName;
+    dataType: "string";
+    columnType: "SingleStoreUUID";
+    data: string;
+    driverParam: string;
+    enumValues: undefined;
+    generated: undefined;
+}>
+
+export class SingleStoreUUIDBuilder<T extends ColumnBuilderBaseConfig<"string", "SingleStoreUUID">> extends SingleStoreColumnBuilder<T> {
+    static readonly [entityKind]: string = "SingleStoreUUIDBuilder"
+
+    constructor(name: T["name"]) {
+        super(name, "string", "SingleStoreUUID")
+    }
+
+    /** @internal */
+    build<TTableName extends string>(table: AnySingleStoreTable<{name: TTableName}>): SingleStoreUUID<MakeColumnConfig<T, TTableName>> {
+        return new SingleStoreUUID(table, this.config as ColumnBuilderRuntimeConfig<any, any>);
+    }
+}
+
+export class SingleStoreUUID<T extends ColumnBaseConfig<"string", "SingleStoreUUID">> extends SingleStoreColumn<T> {
+    static readonly [entityKind]: string = "SingleStoreUUID"
+
+    constructor(table: AnySingleStoreTable<{name: T["tableName"]}>, config: SingleStoreUUIDBuilder<T>["config"]) {
+        super(table, config)
+    }
+
+    getSQLType(): string {
+        return "varchar(36)"
+    }
+}
+
+export function uuid<TName extends string>(name: TName): SingleStoreUUIDBuilderInitial<TName> {
+    return new SingleStoreUUIDBuilder(name)
+}
