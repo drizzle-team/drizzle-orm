b0ed47550ee9b5a4995d7acc03bd6cf09afc1b42
Mitchell Adair <madair@singlestore.com>
2024-07-23 14:00:39 -0400

    add guid

---
+++ drizzle-orm/src/singlestore-core/columns/guid.ts
@@ -0,0 +1,111 @@
+import { ColumnBuilderBaseConfig, ColumnBuilderRuntimeConfig, MakeColumnConfig } from "~/column-builder";
+import { entityKind } from "~/entity";
+import { SingleStoreColumn, SingleStoreColumnBuilder } from "./common";
+import { AnySingleStoreTable } from "~/singlestore-core/table";
+import { ColumnBaseConfig } from "~/column";
+import { sql } from "~/sql/sql";
+import { Equal } from "~/utils";
+
+
+export type SingleStoreGUIDBuilderInitial<TName extends string> = SingleStoreGUIDBuilder<{
+    name: TName;
+    dataType: "buffer";
+    columnType: "SingleStoreGUID";
+    data: Uint8Array;
+    driverParam: string;
+    enumValues: undefined;
+    generated: undefined;
+}>
+
+export class SingleStoreGUIDBuilder<T extends ColumnBuilderBaseConfig<"buffer", "SingleStoreGUID">> extends SingleStoreColumnBuilder<T, SingleStoreGUIDConfig> {
+    static readonly [entityKind]: string = "SingleStoreGUIDBuilder"
+
+    constructor(name: T["name"], config?: SingleStoreGUIDConfig) {
+        super(name, "buffer", "SingleStoreGUID")
+    }
+
+    /** @internal */
+    build<TTableName extends string>(table: AnySingleStoreTable<{name: TTableName}>): SingleStoreGUID<MakeColumnConfig<T, TTableName>> {
+        return new SingleStoreGUID(table, this.config as ColumnBuilderRuntimeConfig<any, any>);
+    }
+}
+
+export class SingleStoreGUID<T extends ColumnBaseConfig<"buffer", "SingleStoreGUID">> extends SingleStoreColumn<T> {
+    static readonly [entityKind]: string = "SingleStoreGUID"
+
+    constructor(table: AnySingleStoreTable<{name: T["tableName"]}>, config: SingleStoreGUIDBuilder<T>["config"]) {
+        super(table, config)
+    }
+
+    getSQLType(): string {
+        return "binary(16)"
+    }
+
+    override mapToDriverValue(value: string) {
+        return sql`UNHEX(REPLACE(${value}, "-", ""))`
+    }
+}
+
+export type SingleStoreGUIDStringBuilderInitial<TName extends string> = SingleStoreGUIDStringBuilder<{
+    name: TName;
+    dataType: "string";
+    columnType: "SingleStoreGUIDString";
+    data: string;
+    driverParam: string;
+    enumValues: undefined;
+    generated: undefined;
+}>
+
+export class SingleStoreGUIDStringBuilder<T extends ColumnBuilderBaseConfig<"string", "SingleStoreGUIDString">> extends SingleStoreColumnBuilder<T, SingleStoreGUIDConfig> {
+    static readonly [entityKind]: string = "SingleStoreGUIDStringBuilder"
+
+    constructor(name: T["name"], config?: SingleStoreGUIDConfig) {
+        super(name, "string", "SingleStoreGUIDString")
+    }
+
+    /** @internal */
+    build<TTableName extends string>(table: AnySingleStoreTable<{name: TTableName}>): SingleStoreGUIDString<MakeColumnConfig<T, TTableName>> {
+        return new SingleStoreGUIDString(table, this.config as ColumnBuilderRuntimeConfig<any, any>);
+    }
+}
+
+export class SingleStoreGUIDString<T extends ColumnBaseConfig<"string", "SingleStoreGUIDString">> extends SingleStoreColumn<T> {
+    static readonly [entityKind]: string = "SingleStoreGUIDString"
+
+    constructor(table: AnySingleStoreTable<{name: T["tableName"]}>, config: SingleStoreGUIDStringBuilder<T>["config"]) {
+        super(table, config)
+    }
+
+    getSQLType(): string {
+        return "binary(16)"
+    }
+
+    override mapToDriverValue(value: string) {
+        return sql`UNHEX(REPLACE(${value}, "-", ""))`
+    }
+
+    override mapFromDriverValue(value: Uint8Array): string {
+        const hex = Buffer.from(value).toString("hex");
+        return `${hex.substring(0, 8)}-${hex.substring(8, 12)}-${hex.substring(12, 16)}-${hex.substring(16, 20)}-${hex.substring(20)}`;
+    }
+}
+
+export interface SingleStoreGUIDConfig<TMode extends "string" | "buffer" = "string" | "buffer"> {
+    mode?: TMode;
+}
+
+/**
+ * Creates a column with the data type `BINARY(16)`
+ * 
+ * Use config `{ mode: "string" }` for a string representation of the GUID
+ */
+export function guid<TName extends string, TMode extends SingleStoreGUIDConfig["mode"] & {}>(
+    name: TName,
+    config?: SingleStoreGUIDConfig<TMode> 
+): Equal<TMode, "string"> extends true ? SingleStoreGUIDStringBuilderInitial<TName> : SingleStoreGUIDBuilderInitial<TName>;
+export function guid(name: string, config?: SingleStoreGUIDConfig) {
+    if (config?.mode === "string") {
+        return new SingleStoreGUIDStringBuilder(name, config)
+    }
+    return new SingleStoreGUIDBuilder(name, config)
+}
