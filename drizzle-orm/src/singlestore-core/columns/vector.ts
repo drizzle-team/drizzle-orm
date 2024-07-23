351510d53274b3120aa007d77ed842d9a5de737d
Diogo Rodrigues <drodrigues@singlestore.com>
2024-07-23 20:05:32 +0100

    nits

--- drizzle-orm/src/singlestore-core/columns/vector.ts
+++ drizzle-orm/src/singlestore-core/columns/vector.ts
@@ -1,67 +1,74 @@
-import { ColumnBaseConfig } from "~/column";
-import { ColumnBuilderBaseConfig, ColumnBuilderRuntimeConfig, MakeColumnConfig } from "~/column-builder";
-import { entityKind } from "~/entity";
-import { SingleStoreColumn, SingleStoreColumnBuilder } from "./common";
-import { AnySingleStoreTable } from "~/singlestore-core/table";
+import { ColumnBaseConfig } from '~/column';
+import { ColumnBuilderBaseConfig, ColumnBuilderRuntimeConfig, MakeColumnConfig } from '~/column-builder';
+import { entityKind } from '~/entity';
+import { AnySingleStoreTable } from '~/singlestore-core/table';
+import { SingleStoreColumn, SingleStoreColumnBuilder } from './common';
 
 export type SingleStoreVectorBuilderInitial<TName extends string> = SingleStoreVectorBuilder<{
-    name: TName;
-    dataType: "array";
-    columnType: "SingleStoreVector";
-    data: Array<number>;
-    driverParam: Array<number>;
-    enumValues: undefined;
-    generated: undefined;
-}>
+	name: TName;
+	dataType: 'array';
+	columnType: 'SingleStoreVector';
+	data: Array<number>;
+	driverParam: Array<number>;
+	enumValues: undefined;
+	generated: undefined;
+}>;
 
-export class SingleStoreVectorBuilder<T extends ColumnBuilderBaseConfig<"array", "SingleStoreVector">> extends SingleStoreColumnBuilder<T, SingleStoreVectorConfig> {
-    static readonly [entityKind]: string = "SingleStoreVectorBuilder"
+export class SingleStoreVectorBuilder<T extends ColumnBuilderBaseConfig<'array', 'SingleStoreVector'>>
+	extends SingleStoreColumnBuilder<T, SingleStoreVectorConfig>
+{
+	static readonly [entityKind]: string = 'SingleStoreVectorBuilder';
 
-    constructor(name: T["name"], config: SingleStoreVectorConfig) {
-        super(name, "array", "SingleStoreVector")
-        this.config.dimensions = config.dimensions
-        this.config.elementType = config.elementType
-    }
+	constructor(name: T['name'], config: SingleStoreVectorConfig) {
+		super(name, 'array', 'SingleStoreVector');
+		this.config.dimensions = config.dimensions;
+		this.config.elementType = config.elementType;
+	}
 
-    /** @internal */
-    override build<TTableName extends string>(table: AnySingleStoreTable<{name: TTableName}>): SingleStoreVector<MakeColumnConfig<T, TTableName>> {
-        return new SingleStoreVector(table, this.config as ColumnBuilderRuntimeConfig<any, any>);
-    }
+	/** @internal */
+	override build<TTableName extends string>(
+		table: AnySingleStoreTable<{ name: TTableName }>,
+	): SingleStoreVector<MakeColumnConfig<T, TTableName>> {
+		return new SingleStoreVector(table, this.config as ColumnBuilderRuntimeConfig<any, any>);
+	}
 }
 
-export class SingleStoreVector<T extends ColumnBaseConfig<"array", "SingleStoreVector">> extends SingleStoreColumn<T> {
-    static readonly [entityKind]: string = "SingleStoreVector"
+export class SingleStoreVector<T extends ColumnBaseConfig<'array', 'SingleStoreVector'>> extends SingleStoreColumn<T> {
+	static readonly [entityKind]: string = 'SingleStoreVector';
 
-    readonly dimensions: number;
-    readonly elementType: ElementType | undefined;
+	readonly dimensions: number;
+	readonly elementType: ElementType | undefined;
 
-    constructor(table: AnySingleStoreTable<{name: T["tableName"]}>, config: SingleStoreVectorBuilder<T>["config"]) {
-        super(table, config)
-        this.dimensions = config.dimensions
-        this.elementType = config.elementType
-    }
+	constructor(table: AnySingleStoreTable<{ name: T['tableName'] }>, config: SingleStoreVectorBuilder<T>['config']) {
+		super(table, config);
+		this.dimensions = config.dimensions;
+		this.elementType = config.elementType;
+	}
 
-    getSQLType(): string {
-        const et = this.elementType === undefined ? "" : `, ${this.elementType}`;
-        return `vector(${this.dimensions}${et})`
-    }
+	getSQLType(): string {
+		const et = this.elementType === undefined ? '' : `, ${this.elementType}`;
+		return `vector(${this.dimensions}${et})`;
+	}
 
-    override mapToDriverValue(value: Array<number>) {
-        return JSON.stringify(value)
-    }
+	override mapToDriverValue(value: Array<number>) {
+		return JSON.stringify(value);
+	}
 
-    override mapFromDriverValue(value: string): Array<number> {
-        return JSON.parse(value)
-    }
+	override mapFromDriverValue(value: string): Array<number> {
+		return JSON.parse(value);
+	}
 }
 
-type ElementType = "I8" | "I16" | "I32" | "I64" | "F32" | "F64";
+type ElementType = 'I8' | 'I16' | 'I32' | 'I64' | 'F32' | 'F64';
 
 export interface SingleStoreVectorConfig {
-    dimensions: number;
-    elementType: ElementType;
+	dimensions: number;
+	elementType: ElementType;
 }
 
-export function vector<TName extends string>(name: TName, config: SingleStoreVectorConfig): SingleStoreVectorBuilderInitial<TName> {
-    return new SingleStoreVectorBuilder(name, config)
+export function vector<TName extends string>(
+	name: TName,
+	config: SingleStoreVectorConfig,
+): SingleStoreVectorBuilderInitial<TName> {
+	return new SingleStoreVectorBuilder(name, config);
 }
