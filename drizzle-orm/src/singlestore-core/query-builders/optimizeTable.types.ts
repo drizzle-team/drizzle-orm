f11e5715b1e2c43d86c12692a84da5babbc6c78b
Diogo Rodrigues <drodrigues@singlestore.com>
2024-07-24 23:54:33 +0100

    more nits

--- drizzle-orm/src/singlestore-core/query-builders/optimizeTable.types.ts
+++ drizzle-orm/src/singlestore-core/query-builders/optimizeTable.types.ts
@@ -1,10 +1,6 @@
-import type { SingleStoreColumn } from ".."
-import type { ColumnBaseConfig, ColumnDataType } from "~/index"
-
 export type OptimizeTableArgument =
 	| 'FULL'
 	| 'FLUSH'
 	| 'FIX_ALTER'
 	| 'INDEX'
 
-export type SelectedColumns = [SingleStoreColumn<ColumnBaseConfig<ColumnDataType, string>, object>]
