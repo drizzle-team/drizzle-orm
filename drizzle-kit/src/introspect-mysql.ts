e0fe588f7eb11ab8ded510dbb50edf45ad574294
Karibash <y.karibayashi@gmail.com>
2024-08-01 23:36:07 +0900

    bugfix: Fix a bug that caused output without specifying index name

--- drizzle-kit/src/introspect-mysql.ts
+++ drizzle-kit/src/introspect-mysql.ts
@@ -10,7 +10,6 @@ import {
 	PrimaryKey,
 	UniqueConstraint,
 } from './serializer/mysqlSchema';
-import { indexName } from './serializer/mysqlSerializer';
 
 // time precision to fsp
 // {mode: "string"} for timestamp by default
@@ -768,12 +767,9 @@ const createTableIndexes = (
 
 		idxKey = casing(idxKey);
 
-		const indexGeneratedName = indexName(tableName, it.columns);
-		const escapedIndexName = indexGeneratedName === it.name ? '' : `"${it.name}"`;
-
 		statement += `\t\t${idxKey}: `;
 		statement += it.isUnique ? 'uniqueIndex(' : 'index(';
-		statement += `${escapedIndexName})`;
+		statement += `"${it.name}")`;
 		statement += `.on(${
 			it.columns
 				.map((it) => `table.${casing(it)}`)
