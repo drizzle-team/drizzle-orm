92dcd589e6df3fd63171f9d680fd191dd759c308
kynefuk <atsuminamihira@gmail.com>
2024-08-04 00:00:36 +0900

    Fix Cloudflare D1 example
    Cloudflare D1 example migrates to 0.21.0

---
+++ examples/cloudflare-d1/drizzle.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "drizzle-kit"
+
+export default defineConfig({
+  dialect: "sqlite",
+  driver: "d1-http",
+  schema: "src/schema.ts",
+  out: "drizzle",
+  dbCredentials: {
+    accountId: "",
+    databaseId: "",
+    token: "",
+  }
+})
