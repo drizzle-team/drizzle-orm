94cf6e90c3b66058cfe22f8fe76ad07b65e36026
veloii <85405932+veloii@users.noreply.github.com>
2024-08-07 14:51:40 +0100

    revert sqlite prepared statement test

--- integration-tests/tests/sqlite/sqlite-common.ts
+++ integration-tests/tests/sqlite/sqlite-common.ts
@@ -844,32 +844,28 @@ export function tests() {
 		test('prepared statement reuse', async (ctx) => {
 			const { db } = ctx.sqlite;
 
-			const stmt = db.insert(usersTable).values({
-				verified: sql.placeholder('verified'),
-				name: sql.placeholder('name'),
-			}).prepare();
+			const stmt = db.insert(usersTable).values({ name: sql.placeholder('name') }).prepare();
 
 			for (let i = 0; i < 10; i++) {
-				await stmt.run({ name: `John ${i}`, verified: i % 2 === 0 });
+				await stmt.run({ name: `John ${i}` });
 			}
 
 			const result = await db.select({
 				id: usersTable.id,
 				name: usersTable.name,
-				verified: usersTable.verified,
 			}).from(usersTable).all();
 
 			expect(result).toEqual([
-				{ id: 1, name: 'John 0', verified: true },
-				{ id: 2, name: 'John 1', verified: false },
-				{ id: 3, name: 'John 2', verified: true },
-				{ id: 4, name: 'John 3', verified: false },
-				{ id: 5, name: 'John 4', verified: true },
-				{ id: 6, name: 'John 5', verified: false },
-				{ id: 7, name: 'John 6', verified: true },
-				{ id: 8, name: 'John 7', verified: false },
-				{ id: 9, name: 'John 8', verified: true },
-				{ id: 10, name: 'John 9', verified: false },
+				{ id: 1, name: 'John 0' },
+				{ id: 2, name: 'John 1' },
+				{ id: 3, name: 'John 2' },
+				{ id: 4, name: 'John 3' },
+				{ id: 5, name: 'John 4' },
+				{ id: 6, name: 'John 5' },
+				{ id: 7, name: 'John 6' },
+				{ id: 8, name: 'John 7' },
+				{ id: 9, name: 'John 8' },
+				{ id: 10, name: 'John 9' },
 			]);
 		});
 
