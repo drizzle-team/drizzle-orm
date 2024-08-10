4289c9bd52959e73ee8dbcf0dada0ce66d3e28c0
Sævar Berg Sævarsson <saevar.berg.saevarsson@bs.nttdata.com>
2024-08-10 15:12:20 +0200

    Improve `expectSchemaShape` to print names of mismatching keys

--- drizzle-zod/tests/utils.ts
+++ drizzle-zod/tests/utils.ts
@@ -7,9 +7,9 @@ export function expectSchemaShape<T extends z.ZodRawShape>(t: TaskContext, expec
 			expect(Object.keys(actual.shape)).toStrictEqual(Object.keys(expected.shape));
 
 			for (const key of Object.keys(actual.shape)) {
-				expect(actual.shape[key]!._def.typeName).toStrictEqual(expected.shape[key]?._def.typeName);
+				expect(actual.shape[key]!._def.typeName, `key: ${key}`).toStrictEqual(expected.shape[key]?._def.typeName);
 				if (actual.shape[key]?._def.typeName === 'ZodOptional') {
-					expect(actual.shape[key]!._def.innerType._def.typeName).toStrictEqual(
+					expect(actual.shape[key]!._def.innerType._def.typeName, `key: ${key}`).toStrictEqual(
 						actual.shape[key]!._def.innerType._def.typeName,
 					);
 				}
