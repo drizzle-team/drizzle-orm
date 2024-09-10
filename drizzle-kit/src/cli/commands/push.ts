e4639dfb0178e56adb9a80dea6afc89bc7891eb3
akazwz <50396286+akazwz@users.noreply.github.com>
2024-09-10 23:29:35 +0800

    feat: Add d1 support for kit push

--- drizzle-kit/src/cli/commands/push.ts
+++ drizzle-kit/src/cli/commands/push.ts
@@ -523,6 +523,8 @@ export const sqlitePush = async (
 					await db.run('rollback');
 					process.exit(1);
 				}
+			} else if (credentials.driver === "d1-http") {
+				await db.run(statementsToExecute.join(''));
 			}
 			render(`[${chalk.green('✓')}] Changes applied`);
 		}
