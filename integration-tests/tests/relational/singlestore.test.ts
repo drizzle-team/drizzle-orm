9ba73fd565de410cee27bad0ee20ad85b6af1717
prodrigues <prodrigues@singlestore.com>
2024-09-27 16:44:19 +0100

    lint fix

--- integration-tests/tests/relational/singlestore.test.ts
+++ integration-tests/tests/relational/singlestore.test.ts
@@ -79,7 +79,7 @@ beforeAll(async () => {
 
 	await client.query(`CREATE DATABASE IF NOT EXISTS drizzle;`);
 	await client.changeUser({ database: 'drizzle' });
-	db = drizzle(client, { schema, logger: ENABLE_LOGGING});
+	db = drizzle(client, { schema, logger: ENABLE_LOGGING });
 });
 
 afterAll(async () => {
