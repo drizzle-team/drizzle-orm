fa8099aca3e7ddf5fb251609832eb370e4df2a50
Aleksandr Sherman <aleksandrserman@gmail.com>
2024-10-02 14:11:40 +0300

    fixed numeric introspect in pg

--- drizzle-kit/src/serializer/pgSerializer.ts
+++ drizzle-kit/src/serializer/pgSerializer.ts
@@ -1924,6 +1924,8 @@ const defaultForColumn = (column: any, internals: PgKitInternals, tableName: str
 			}
 			return columnDefaultAsString;
 		}
+	} else if (column.data_type.includes('numeric')) {
+		return `'${column.column_default}'`;
 	} else if (column.data_type === 'json' || column.data_type === 'jsonb') {
 		const jsonWithoutSpaces = JSON.stringify(JSON.parse(columnDefaultAsString.slice(1, -1)));
 		return `'${jsonWithoutSpaces}'::${column.data_type}`;
