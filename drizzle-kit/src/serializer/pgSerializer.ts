746aeed8242275abdccf53fd795d7cd4af5a0239
AndriiSherman <andreysherman11@gmail.com>
2024-10-07 16:07:52 +0300

    Add Xata workaround for identity columns

--- drizzle-kit/src/serializer/pgSerializer.ts
+++ drizzle-kit/src/serializer/pgSerializer.ts
@@ -1064,7 +1064,7 @@ export const fromDatabase = async (
 							: undefined,
 					};
 
-					if (identityName) {
+					if (identityName && typeof identityName === 'string') {
 						// remove "" from sequence name
 						delete sequencesToReturn[
 							`${tableSchema}.${
