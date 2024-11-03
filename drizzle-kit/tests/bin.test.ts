67c68fbd08ffe9331aaacad32d9b3a082b294b7e
Alex Blokh <aleksandrblokh@gmail.com>
2024-11-03 10:13:51 +0200

    imports checker with OHM grammar

---
+++ drizzle-kit/tests/bin.test.ts
@@ -0,0 +1,56 @@
+import { analyzeImports, ChainLink } from "../imports-checker/checker";
+import chalk from "chalk";
+import { assert, test } from "vitest";
+
+
+
+test("imports-issues", ()=>{
+  const issues = analyzeImports({
+    basePath: ".",
+    localPaths: ["src"],
+    whiteList: [
+      "@drizzle-team/brocli",
+      "json-diff",
+      "path",
+      "fs",
+      "fs/*",
+      "url",
+      "zod",
+      "node:*",
+      "hono",
+      "glob",
+      "hono/*",
+      "hono/**/*",
+      "@hono/*",
+      "crypto",
+      "hanji",
+    ],
+    entry: "src/cli/index.ts",
+    logger: true,
+    ignoreTypes: true,
+  }).issues;
+  
+  const chainToString = (chains: ChainLink[]) => {
+    if (chains.length === 0) throw new Error();
+  
+    let out = chains[0]!.file + "\n";
+    let indentation = 0;
+    for (let chain of chains) {
+      out +=
+        " ".repeat(indentation) +
+        "└" +
+        chain.import +
+        ` ${chalk.gray(chain.file)}\n`;
+      indentation += 1;
+    }
+    return out;
+  };
+  
+  console.log()
+  for (const issue of issues) {
+    console.log(chalk.red(issue.imports.map(it=>it.name).join("\n")))
+    console.log(issue.accessChains.map((it) => chainToString(it)).join("\n"));
+  }
+
+  assert.equal(issues.length, 0)
+})
