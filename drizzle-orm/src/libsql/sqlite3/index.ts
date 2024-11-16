6a28660d4319d4fb0601945dfebac260b4367fac
Sukairo-02 <sreka9056@gmail.com>
2024-11-16 08:50:09 +0200

    Removed type-level import existence checks on driver init (fixes #3521)

--- drizzle-orm/src/libsql/sqlite3/index.ts
+++ drizzle-orm/src/libsql/sqlite3/index.ts
@@ -1,30 +1,26 @@
 import { type Client, type Config, createClient } from '@libsql/client/sqlite3';
-import { type DrizzleConfig, type IfNotImported, type ImportTypeError, isConfig } from '~/utils.ts';
+import { type DrizzleConfig, isConfig } from '~/utils.ts';
 import { construct, type LibSQLDatabase } from '../driver-core.ts';
 
 export function drizzle<
 	TSchema extends Record<string, unknown> = Record<string, never>,
 	TClient extends Client = Client,
 >(
-	...params: IfNotImported<
-		Client,
-		[ImportTypeError<'@libsql/client'>],
-		[
-			TClient | string,
-		] | [
-			TClient | string,
-			DrizzleConfig<TSchema>,
-		] | [
-			(
-				& DrizzleConfig<TSchema>
-				& ({
-					connection: string | Config;
-				} | {
-					client: TClient;
-				})
-			),
-		]
-	>
+	...params: [
+		TClient | string,
+	] | [
+		TClient | string,
+		DrizzleConfig<TSchema>,
+	] | [
+		(
+			& DrizzleConfig<TSchema>
+			& ({
+				connection: string | Config;
+			} | {
+				client: TClient;
+			})
+		),
+	]
 ): LibSQLDatabase<TSchema> & {
 	$client: TClient;
 } {
