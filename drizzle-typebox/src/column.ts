53c5dee0cec24acfd79d89740f966e0d20b0ce15
Mario564 <ka.mario564@gmail.com>
2024-11-25 10:28:44 -0800

    Reimplement everything in drizzle-typebox

---
+++ drizzle-typebox/src/column.ts
@@ -0,0 +1,217 @@
+import { CreateType, Kind, Type as t,  } from '@sinclair/typebox';
+import { CONSTANTS } from './constants';
+import { isColumnType, isWithEnum } from './utils';
+import type { Column, ColumnBaseConfig } from 'drizzle-orm';
+import type { TSchema, Type as typebox } from '@sinclair/typebox';
+import type { BufferSchema, JsonSchema } from './utils';
+import type {
+	MySqlBigInt53,
+	MySqlChar,
+	MySqlDouble,
+	MySqlFloat,
+	MySqlInt,
+	MySqlMediumInt,
+	MySqlReal,
+	MySqlSerial,
+	MySqlSmallInt,
+	MySqlText,
+	MySqlTinyInt,
+	MySqlVarChar,
+	MySqlYear,
+} from 'drizzle-orm/mysql-core';
+import type {
+	PgArray,
+	PgBigInt53,
+	PgBigSerial53,
+	PgBinaryVector,
+	PgChar,
+	PgDoublePrecision,
+	PgGeometry,
+	PgGeometryObject,
+	PgHalfVector,
+	PgInteger,
+	PgLineABC,
+	PgLineTuple,
+	PgPointObject,
+	PgPointTuple,
+	PgReal,
+	PgSerial,
+	PgSmallInt,
+	PgSmallSerial,
+	PgUUID,
+	PgVarchar,
+	PgVector,
+} from 'drizzle-orm/pg-core';
+import type { SQLiteInteger, SQLiteReal, SQLiteText } from 'drizzle-orm/sqlite-core';
+
+
+export const literalSchema = t.Union([t.String(), t.Number(), t.Boolean(), t.Null()]);
+export const jsonSchema: JsonSchema = t.Recursive((self) => t.Union([literalSchema, t.Array(self), t.Record(t.String(), self)])) as any;
+export const bufferSchema: BufferSchema = CreateType({ [Kind]: 'Buffer', type: 'buffer' }) as any;
+
+export function mapEnumValues(values: string[]) {
+	return values.reduce((acc, value) => ({ ...acc, [value]: value }), {});
+}
+
+/** @internal */
+export function columnToSchema(column: Column, t: typeof typebox): TSchema {
+	let schema!: TSchema;
+
+	if (isWithEnum(column)) {
+		schema = column.enumValues.length ? t.Enum(mapEnumValues(column.enumValues)) : t.String();
+	}
+
+	if (!schema) {
+		// Handle specific types
+		if (isColumnType<PgGeometry<any> | PgPointTuple<any>>(column, ['PgGeometry', 'PgPointTuple'])) {
+			schema = t.Tuple([t.Number(), t.Number()]);
+		} else if (isColumnType<PgPointObject<any> | PgGeometryObject<any>>(column, ['PgGeometryObject', 'PgPointObject'])) {
+			schema = t.Object({ x: t.Number(), y: t.Number() });
+		} else if (isColumnType<PgHalfVector<any> | PgVector<any>>(column, ['PgHalfVector', 'PgVector'])) {
+			schema = t.Array(t.Number(), column.dimensions ? {
+        minItems: column.dimensions,
+        maxItems: column.dimensions
+      } : undefined);
+		} else if (isColumnType<PgLineTuple<any>>(column, ['PgLine'])) {
+			schema = t.Tuple([t.Number(), t.Number(), t.Number()]);
+		} else if (isColumnType<PgLineABC<any>>(column, ['PgLineABC'])) {
+			schema = t.Object({
+				a: t.Number(),
+				b: t.Number(),
+				c: t.Number(),
+			});
+		} // Handle other types
+		else if (isColumnType<PgArray<any, any>>(column, ['PgArray'])) {
+			schema = t.Array(columnToSchema(column.baseColumn, t), column.size ? {
+        minItems: column.size,
+        maxItems: column.size
+      } : undefined);
+		} else if (column.dataType === 'array') {
+			schema = t.Array(t.Any());
+		} else if (column.dataType === 'number') {
+			schema = numberColumnToSchema(column, t);
+		} else if (column.dataType === 'bigint') {
+			schema = bigintColumnToSchema(column, t);
+		} else if (column.dataType === 'boolean') {
+			schema = t.Boolean();
+		} else if (column.dataType === 'date') {
+			schema = t.Date();
+		} else if (column.dataType === 'string') {
+			schema = stringColumnToSchema(column, t);
+		} else if (column.dataType === 'json') {
+			schema = jsonSchema;
+		} else if (column.dataType === 'custom') {
+			schema = t.Any();
+		} else if (column.dataType === 'buffer') {
+			schema = bufferSchema;
+		}
+	}
+
+	if (!schema) {
+		schema = t.Any();
+	}
+
+	return schema;
+}
+
+function numberColumnToSchema(column: Column, t: typeof typebox): TSchema {
+	let unsigned = column.getSQLType().includes('unsigned');
+	let min!: number;
+	let max!: number;
+	let integer = false;
+
+	if (isColumnType<MySqlTinyInt<any>>(column, ['MySqlTinyInt'])) {
+		min = unsigned ? 0 : CONSTANTS.INT8_MIN;
+		max = unsigned ? CONSTANTS.INT8_UNSIGNED_MAX : CONSTANTS.INT8_MAX;
+		integer = true;
+	} else if (isColumnType<PgSmallInt<any> | PgSmallSerial<any> | MySqlSmallInt<any>>(column, ['PgSmallInt', 'PgSmallSerial', 'MySqlSmallInt'])) {
+		min = unsigned ? 0 : CONSTANTS.INT16_MIN;
+		max = unsigned ? CONSTANTS.INT16_UNSIGNED_MAX : CONSTANTS.INT16_MAX;
+		integer = true;
+	} else if (isColumnType<PgReal<any> | MySqlFloat<any> | MySqlMediumInt<any>>(column, ['PgReal', 'MySqlFloat', 'MySqlMediumInt'])) {
+		min = unsigned ? 0 : CONSTANTS.INT24_MIN;
+		max = unsigned ? CONSTANTS.INT24_UNSIGNED_MAX : CONSTANTS.INT24_MAX;
+		integer = isColumnType(column, ['MySqlMediumInt']);
+	} else if (isColumnType<PgInteger<any> | PgSerial<any> | MySqlInt<any>>(column, ['PgInteger', 'PgSerial', 'MySqlInt'])) {
+		min = unsigned ? 0 : CONSTANTS.INT32_MIN;
+		max = unsigned ? CONSTANTS.INT32_UNSIGNED_MAX : CONSTANTS.INT32_MAX;
+		integer = true;
+	} else if (isColumnType<PgDoublePrecision<any> | MySqlReal<any> | MySqlDouble<any> | SQLiteReal<any>>(column, ['PgDoublePrecision', 'MySqlReal', 'MySqlDouble', 'SQLiteReal'])) {
+		min = unsigned ? 0 : CONSTANTS.INT48_MIN;
+		max = unsigned ? CONSTANTS.INT48_UNSIGNED_MAX : CONSTANTS.INT48_MAX;
+	} else if (isColumnType<PgBigInt53<any> | PgBigSerial53<any> | MySqlBigInt53<any> | MySqlSerial<any> | SQLiteInteger<any>>(column, ['PgBigInt53', 'PgBigSerial53', 'MySqlBigInt53', 'MySqlSerial', 'SQLiteInteger'])) {
+		unsigned = unsigned || isColumnType(column, ['MySqlSerial']);
+		min = unsigned ? 0 : Number.MIN_SAFE_INTEGER;
+		max = Number.MAX_SAFE_INTEGER;
+		integer = true;
+	} else if (isColumnType<MySqlYear<any>>(column, ['MySqlYear'])) {
+		min = 1901;
+		max = 2155;
+		integer = true;
+	} else {
+		min = Number.MIN_SAFE_INTEGER;
+		max = Number.MAX_SAFE_INTEGER;
+	}
+
+  const key = integer ? 'Integer' : 'Number';
+  return t[key]({
+    minimum: min,
+    maximum: max,
+    
+  });
+}
+
+function bigintColumnToSchema(column: Column, t: typeof typebox): TSchema {
+	const unsigned = column.getSQLType().includes('unsigned');
+	let min = unsigned ? 0n : CONSTANTS.INT64_MIN;
+	let max = unsigned ? CONSTANTS.INT64_UNSIGNED_MAX : CONSTANTS.INT64_MAX;
+
+	return t.BigInt({
+    minimum: min,
+    maximum: max
+  });
+}
+
+function stringColumnToSchema(column: Column, t: typeof typebox): TSchema {
+	if (isColumnType<PgUUID<ColumnBaseConfig<'string', 'PgUUID'>>>(column, ['PgUUID'])) {
+		return t.String({ format: 'uuid' });
+	}
+
+	let max: number | undefined;
+	let regex: RegExp | undefined;
+	let fixed = false;
+
+	if (isColumnType<PgVarchar<any> | SQLiteText<any>>(column, ['PgVarchar', 'SQLiteText'])) {
+		max = column.length;
+	} else if (isColumnType<MySqlVarChar<any>>(column, ['MySqlVarChar'])) {
+		max = column.length ?? CONSTANTS.INT16_UNSIGNED_MAX;
+	} else if (isColumnType<MySqlText<any>>(column, ['MySqlText'])) {
+		if (column.textType === 'longtext') {
+			max = CONSTANTS.INT32_UNSIGNED_MAX;
+		} else if (column.textType === 'mediumtext') {
+			max = CONSTANTS.INT24_UNSIGNED_MAX;
+		} else if (column.textType === 'text') {
+			max = CONSTANTS.INT16_UNSIGNED_MAX;
+		} else {
+			max = CONSTANTS.INT8_UNSIGNED_MAX;
+		}
+	}
+
+	if (isColumnType<PgChar<any> | MySqlChar<any>>(column, ['PgChar', 'MySqlChar'])) {
+		max = column.length;
+		fixed = true;
+	}
+
+	if (isColumnType<PgBinaryVector<any>>(column, ['PgBinaryVector'])) {
+		regex = /^[01]+$/;
+		max = column.dimensions;
+	}
+
+  const options = {
+    pattern: regex !== undefined ? regex.source : undefined,
+    ...(
+      max !== undefined && fixed ? { minLength: max, maxLength: max } : max !== undefined ? { maxLength: max } : {}
+    )
+  };
+  return t.String(Object.values(options).filter((value) => value !== undefined).length > 0 ? options : undefined);
+}
---
+++ drizzle-typebox/src/schema.types.internal.ts
@@ -0,0 +1,87 @@
+import type { Assume, Column, DrizzleTypeError, SelectedFieldsFlat, Simplify, Table, View } from 'drizzle-orm';
+import type * as t from '@sinclair/typebox';
+import type { GetBaseColumn, GetEnumValuesFromColumn, GetTypeboxType, HandleColumn } from './column.types';
+import type { GetSelection, RemoveNever } from './utils';
+
+export interface Conditions {
+	never: (column?: Column) => boolean;
+	optional: (column: Column) => boolean;
+	nullable: (column: Column) => boolean;
+}
+
+export type BuildRefineColumns<
+	TColumns extends Record<string, any>,
+> = Simplify<
+	RemoveNever<
+		{
+			[K in keyof TColumns]: TColumns[K] extends infer TColumn extends Column ? GetTypeboxType<
+					TColumn['_']['data'],
+					TColumn['_']['dataType'],
+					GetEnumValuesFromColumn<TColumn>,
+					GetBaseColumn<TColumn>
+				> extends infer TSchema extends t.TAny ? TSchema
+				: t.TAny
+				: TColumns[K] extends infer TObject extends SelectedFieldsFlat<Column> | Table | View
+					? BuildRefineColumns<GetSelection<TObject>>
+				: TColumns[K];
+		}
+	>
+>;
+
+export type BuildRefine<
+	TColumns extends Record<string, any>,
+> = BuildRefineColumns<TColumns> extends infer TBuildColumns ? {
+		[K in keyof TBuildColumns]?: TBuildColumns[K] extends t.TSchema
+			? ((schema: TBuildColumns[K]) => t.TSchema) | t.TSchema
+			: TBuildColumns[K] extends Record<string, any> ? Simplify<BuildRefine<TBuildColumns[K]>>
+			: never;
+	}
+	: never;
+
+type HandleRefinement<
+	TRefinement extends t.TSchema | ((schema: t.TSchema) => t.TSchema),
+	TColumn extends Column,
+> = TRefinement extends (schema: t.TSchema) => t.TSchema
+	? TColumn['_']['notNull'] extends true ? ReturnType<TRefinement>
+	: t.TTuple<[ReturnType<TRefinement>, t.TNull]>
+	: TRefinement;
+
+export type BuildSchema<
+	TType extends 'select' | 'insert' | 'update',
+	TColumns extends Record<string, any>,
+	TRefinements extends Record<string, any> | undefined,
+> = t.TObject<
+	Simplify<
+		RemoveNever<
+			{
+				[K in keyof TColumns]: TColumns[K] extends infer TColumn extends Column
+					? TRefinements extends object
+						? TRefinements[Assume<K, keyof TRefinements>] extends
+							infer TRefinement extends t.TSchema | ((schema: t.TSchema) => t.TSchema)
+							? HandleRefinement<TRefinement, TColumn>
+						: HandleColumn<TType, TColumn>
+					: HandleColumn<TType, TColumn>
+					: TColumns[K] extends infer TObject extends SelectedFieldsFlat<Column> | Table | View ? BuildSchema<
+							TType,
+							GetSelection<TObject>,
+							TRefinements extends object
+								? TRefinements[Assume<K, keyof TRefinements>] extends infer TNestedRefinements extends object
+									? TNestedRefinements
+								: undefined
+								: undefined
+						>
+					: t.TAny;
+			}
+		>
+	>
+>;
+
+export type NoUnknownKeys<
+	TRefinement extends Record<string, any>,
+	TCompare extends Record<string, any>,
+> = {
+	[K in keyof TRefinement]: K extends keyof TCompare
+		? TRefinement[K] extends Record<string, t.TSchema> ? NoUnknownKeys<TRefinement[K], TCompare[K]>
+		: TRefinement[K]
+		: DrizzleTypeError<`Found unknown key in refinement: "${K & string}"`>;
+};
