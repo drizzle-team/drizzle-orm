440c1ee81c186abc6f70feb010e7a57f460e371d
Aleksandr Sherman <aleksandrserman@gmail.com>
2024-11-29 13:07:26 +0200

    fixed dprint issues

--- integration-tests/tests/sqlite/durable-objects/drizzle/migrations.js
+++ integration-tests/tests/sqlite/durable-objects/drizzle/migrations.js
@@ -1,12 +1,9 @@
-// This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
-
-import journal from './meta/_journal.json';
 import m0000 from './0000_cuddly_black_bolt.sql';
+import journal from './meta/_journal.json';
 
-  export default {
-    journal,
-    migrations: {
-      m0000
-    }
-  }
-  
+export default {
+	journal,
+	migrations: {
+		m0000,
+	},
+};
