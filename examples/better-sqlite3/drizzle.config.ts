54143a0424246b056e8a42e018339a2b33ee5e30
JamBalaya56562 <jambalaya.pyoncafe@outlook.jp>
2024-12-05 15:51:59 +0900

    examples: use `defineConfig` instead of `Config`

--- examples/better-sqlite3/drizzle.config.ts
+++ examples/better-sqlite3/drizzle.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from 'drizzle-kit';
+import { defineConfig } from 'drizzle-kit';
 
-export default {
+export default defineConfig({
 	schema: './src/schema.ts',
 	out: './drizzle',
 	driver: 'better-sqlite',
 	dbCredentials: {
 		url: './sqlite.db',
 	},
-} satisfies Config;
+});
