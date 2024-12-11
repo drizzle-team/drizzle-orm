8cf6a821ebee5a41eba034b87bccbdb4fa15e33f
AndriiSherman <andreysherman11@gmail.com>
2024-12-11 20:00:34 +0200

    Export SeedService

--- drizzle-seed/src/services/SeedService.ts
+++ drizzle-seed/src/services/SeedService.ts
@@ -42,7 +42,7 @@ import {
 } from './GeneratorsWrappers.ts';
 import { equalSets, generateHashFromString } from './utils.ts';
 
-class SeedService {
+export class SeedService {
 	static readonly [entityKind]: string = 'SeedService';
 
 	private defaultCountForTable = 10;
@@ -1470,5 +1470,3 @@ class SeedService {
 		}
 	};
 }
-
-export default new SeedService();
