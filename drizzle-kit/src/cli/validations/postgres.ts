f60a31bfaae64b10f6dcab429bb662c9829fc6fa
Mikko Käär <mikko.kaar@gmail.com>
2025-01-11 22:37:55 +0200

    allow passing ssl and max parameters alongside url to postgres

--- drizzle-kit/src/cli/validations/postgres.ts
+++ drizzle-kit/src/cli/validations/postgres.ts
@@ -2,6 +2,18 @@ import { boolean, coerce, literal, object, string, TypeOf, undefined, union } fr
 import { error } from '../views';
 import { wrapParam } from './common';
 
+const postgresConnectionOptions = {
+	ssl: union([
+		literal('require'),
+		literal('allow'),
+		literal('prefer'),
+		literal('verify-full'),
+		boolean(),
+		object({}).passthrough(),
+	]).optional(),
+	max: coerce.number().min(1).optional(),
+}
+
 export const postgresCredentials = union([
 	object({
 		driver: undefined(),
@@ -10,15 +22,7 @@ export const postgresCredentials = union([
 		user: string().min(1).optional(),
 		password: string().min(1).optional(),
 		database: string().min(1),
-		ssl: union([
-			literal('require'),
-			literal('allow'),
-			literal('prefer'),
-			literal('verify-full'),
-			boolean(),
-			object({}).passthrough(),
-		]).optional(),
-		max: coerce.number().min(1).optional(),
+		...postgresConnectionOptions
 	}).transform((o) => {
 		delete o.driver;
 		return o as Omit<typeof o, 'driver'>;
@@ -26,7 +30,8 @@ export const postgresCredentials = union([
 	object({
 		driver: undefined(),
 		url: string().min(1),
-	}).transform<{ url: string }>((o) => {
+		...postgresConnectionOptions
+	}).transform((o) => {
 		delete o.driver;
 		return o;
 	}),
