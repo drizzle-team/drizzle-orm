570c50621d407d22953ce55397214bed2e0773a0
Roman <nabukhotnyiroman@gmail.com>
2025-01-16 12:47:05 +0200

    fix: Fix certs util

--- drizzle-kit/src/utils/certs.ts
+++ drizzle-kit/src/utils/certs.ts
@@ -4,31 +4,34 @@ import { access, readFile } from 'fs/promises';
 import { exec, ExecOptions } from 'node:child_process';
 import { join } from 'path';
 
-const p = envPaths('drizzle-studio', {
-	suffix: '',
-});
-
-mkdirSync(p.data, { recursive: true });
-
 export function runCommand(command: string, options: ExecOptions = {}) {
-	return new Promise<{ exitCode: number }>((resolve, reject) => {
-		exec(command, options, (error, stdout, stderr) => {
+	return new Promise<{ exitCode: number }>((resolve) => {
+		exec(command, options, (error) => {
 			return resolve({ exitCode: error?.code ?? 0 });
 		});
 	});
 }
 
 export const certs = async () => {
-	const res = await runCommand(`mkcert --help`, { cwd: p.data });
-
-	// ~/.local/share/drizzle-studio
-	const keyPath = join(p.data, 'localhost-key.pem');
-	const certPath = join(p.data, 'localhost.pem');
+	const res = await runCommand('mkcert --help');
 
 	if (res.exitCode === 0) {
+		const p = envPaths('drizzle-studio', {
+			suffix: '',
+		});
+
+		// create ~/.local/share/drizzle-studio
+		mkdirSync(p.data, { recursive: true });
+
+		// ~/.local/share/drizzle-studio
+		const keyPath = join(p.data, 'localhost-key.pem');
+		const certPath = join(p.data, 'localhost.pem');
+
 		try {
+			// check if the files exist
 			await Promise.all([access(keyPath), access(certPath)]);
 		} catch (e) {
+			// if not create them
 			await runCommand(`mkcert localhost`, { cwd: p.data });
 		}
 		const [key, cert] = await Promise.all([
@@ -39,5 +42,3 @@ export const certs = async () => {
 	}
 	return null;
 };
-
-certs();
