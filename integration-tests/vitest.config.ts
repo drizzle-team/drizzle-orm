6f787bc303be503f0dc58597617fbd8a9bed314c
Aleksandr Sherman <aleksandrserman@gmail.com>
2025-02-13 12:34:09 +0200

    gel hooktimeout test

--- integration-tests/vitest.config.ts
+++ integration-tests/vitest.config.ts
@@ -70,7 +70,7 @@ export default defineConfig({
 			tsconfig: 'tsconfig.json',
 		},
 		testTimeout: 100000,
-		hookTimeout: 100000,
+		hookTimeout: 200000,
 		isolate: true,
 		poolOptions: {
 			threads: {
