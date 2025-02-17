bc8f583df165b5a74a0a241b6def66c0e5dee050
reecefenwick <reece.f@montu.com.au>
2025-02-17 12:11:17 +1000

    fix(mysql): remove redundant invariant checks now that Promise.race is gone

--- drizzle-orm/src/mysql2/session.ts
+++ drizzle-orm/src/mysql2/session.ts
@@ -144,12 +144,6 @@ export class MySql2PreparedQuery<T extends MySqlPreparedQueryConfig> extends MyS
 			const stream = driverQuery.stream();
 
 			for await (const row of stream) {
-				if (row === undefined || (Array.isArray(row) && row.length === 0)) {
-					break;
-				} else if (row instanceof Error) { // eslint-disable-line no-instanceof/no-instanceof
-					throw row;
-				}
-
 				if (hasRowsMapper) {
 					if (customResultMapper) {
 						const mappedRow = customResultMapper([row as unknown[]]);
