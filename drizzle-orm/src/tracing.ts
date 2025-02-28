c41c614873b2c61d112e9370c4ae8bdd838cda8b
Daniel Pedersen <contact@danielpedersen.se>
2025-02-28 16:37:27 +0100

    Enable tracing

--- drizzle-orm/src/tracing.ts
+++ drizzle-orm/src/tracing.ts
@@ -1,16 +1,19 @@
-import type { Span, Tracer } from '@opentelemetry/api';
+import type { Exception, Span, Tracer } from '@opentelemetry/api';
 import { iife } from '~/tracing-utils.ts';
 import { npmVersion } from '~/version.ts';
 
 let otel: typeof import('@opentelemetry/api') | undefined;
 let rawTracer: Tracer | undefined;
-// try {
-// 	otel = await import('@opentelemetry/api');
-// } catch (err: any) {
-// 	if (err.code !== 'MODULE_NOT_FOUND' && err.code !== 'ERR_MODULE_NOT_FOUND') {
-// 		throw err;
-// 	}
-// }
+
+(async () => {
+	try {
+		otel = await import('@opentelemetry/api');
+	} catch (err: any) {
+		if (err.code !== 'MODULE_NOT_FOUND' && err.code !== 'ERR_MODULE_NOT_FOUND') {
+			throw err;
+		}
+	}
+})();
 
 type SpanName =
 	| 'drizzle.operation'
@@ -35,10 +38,11 @@ export const tracer = {
 			(otel, rawTracer) =>
 				rawTracer.startActiveSpan(
 					name,
-					((span: Span) => {
+					(async (span: Span) => {
 						try {
-							return fn(span);
+							return await fn(span);
 						} catch (e) {
+							span.recordException(e as Exception);
 							span.setStatus({
 								code: otel.SpanStatusCode.ERROR,
 								message: e instanceof Error ? e.message : 'Unknown error', // eslint-disable-line no-instanceof/no-instanceof
