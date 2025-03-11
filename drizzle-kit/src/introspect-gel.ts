9fd11154e3d09735e80a9a52b5c8aa8966854951
Sukairo-02 <sreka9056@gmail.com>
2025-03-11 05:14:55 +0200

    Import fix

--- drizzle-kit/src/introspect-gel.ts
+++ drizzle-kit/src/introspect-gel.ts
@@ -1,5 +1,4 @@
 import { getTableName, is } from 'drizzle-orm';
-import { AnyGelTable } from 'drizzle-orm/gel-core';
 import {
 	createTableRelationsHelpers,
 	extractTablesRelationalConfig,
@@ -7,7 +6,8 @@ import {
 	One,
 	Relation,
 	Relations,
-} from 'drizzle-orm/relations';
+} from 'drizzle-orm/_relations';
+import { AnyGelTable } from 'drizzle-orm/gel-core';
 import './@types/utils';
 import { toCamelCase } from 'drizzle-orm/casing';
 import { Casing } from './cli/validations/common';
