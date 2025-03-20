f34186c77dbaae965c3cf02f20ef419ad7e74b17
Sukairo-02 <sreka9056@gmail.com>
2025-03-20 14:35:35 +0200

    Various fixes, features bundled for v0.41.0

--- drizzle-orm/src/pg-core/columns/numeric.ts
+++ drizzle-orm/src/pg-core/columns/numeric.ts
@@ -2,7 +2,7 @@ import type { ColumnBuilderBaseConfig, ColumnBuilderRuntimeConfig, MakeColumnCon
 import type { ColumnBaseConfig } from '~/column.ts';
 import { entityKind } from '~/entity.ts';
 import type { AnyPgTable } from '~/pg-core/table.ts';
-import { getColumnNameAndConfig } from '~/utils.ts';
+import { type Equal, getColumnNameAndConfig } from '~/utils.ts';
 import { PgColumn, PgColumnBuilder } from './common.ts';
 
 export type PgNumericBuilderInitial<TName extends string> = PgNumericBuilder<{
@@ -49,6 +49,12 @@ export class PgNumeric<T extends ColumnBaseConfig<'string', 'PgNumeric'>> extend
 		this.scale = config.scale;
 	}
 
+	override mapFromDriverValue(value: unknown): string {
+		if (typeof value === 'string') return value;
+
+		return String(value);
+	}
+
 	getSQLType(): string {
 		if (this.precision !== undefined && this.scale !== undefined) {
 			return `numeric(${this.precision}, ${this.scale})`;
@@ -60,22 +66,162 @@ export class PgNumeric<T extends ColumnBaseConfig<'string', 'PgNumeric'>> extend
 	}
 }
 
-export type PgNumericConfig =
-	| { precision: number; scale?: number }
-	| { precision?: number; scale: number }
-	| { precision: number; scale: number };
+export type PgNumericNumberBuilderInitial<TName extends string> = PgNumericNumberBuilder<{
+	name: TName;
+	dataType: 'number';
+	columnType: 'PgNumericNumber';
+	data: number;
+	driverParam: string;
+	enumValues: undefined;
+}>;
 
-export function numeric(): PgNumericBuilderInitial<''>;
-export function numeric(
-	config?: PgNumericConfig,
-): PgNumericBuilderInitial<''>;
-export function numeric<TName extends string>(
+export class PgNumericNumberBuilder<T extends ColumnBuilderBaseConfig<'number', 'PgNumericNumber'>>
+	extends PgColumnBuilder<
+		T,
+		{
+			precision: number | undefined;
+			scale: number | undefined;
+		}
+	>
+{
+	static override readonly [entityKind]: string = 'PgNumericNumberBuilder';
+
+	constructor(name: T['name'], precision?: number, scale?: number) {
+		super(name, 'number', 'PgNumericNumber');
+		this.config.precision = precision;
+		this.config.scale = scale;
+	}
+
+	/** @internal */
+	override build<TTableName extends string>(
+		table: AnyPgTable<{ name: TTableName }>,
+	): PgNumericNumber<MakeColumnConfig<T, TTableName>> {
+		return new PgNumericNumber<MakeColumnConfig<T, TTableName>>(
+			table,
+			this.config as ColumnBuilderRuntimeConfig<any, any>,
+		);
+	}
+}
+
+export class PgNumericNumber<T extends ColumnBaseConfig<'number', 'PgNumericNumber'>> extends PgColumn<T> {
+	static override readonly [entityKind]: string = 'PgNumericNumber';
+
+	readonly precision: number | undefined;
+	readonly scale: number | undefined;
+
+	constructor(table: AnyPgTable<{ name: T['tableName'] }>, config: PgNumericNumberBuilder<T>['config']) {
+		super(table, config);
+		this.precision = config.precision;
+		this.scale = config.scale;
+	}
+
+	override mapFromDriverValue(value: unknown): number {
+		if (typeof value === 'number') return value;
+
+		return Number(value);
+	}
+
+	override mapToDriverValue = String;
+
+	getSQLType(): string {
+		if (this.precision !== undefined && this.scale !== undefined) {
+			return `numeric(${this.precision}, ${this.scale})`;
+		} else if (this.precision === undefined) {
+			return 'numeric';
+		} else {
+			return `numeric(${this.precision})`;
+		}
+	}
+}
+
+export type PgNumericBigIntBuilderInitial<TName extends string> = PgNumericBigIntBuilder<{
+	name: TName;
+	dataType: 'bigint';
+	columnType: 'PgNumericBigInt';
+	data: bigint;
+	driverParam: string;
+	enumValues: undefined;
+}>;
+
+export class PgNumericBigIntBuilder<T extends ColumnBuilderBaseConfig<'bigint', 'PgNumericBigInt'>>
+	extends PgColumnBuilder<
+		T,
+		{
+			precision: number | undefined;
+			scale: number | undefined;
+		}
+	>
+{
+	static override readonly [entityKind]: string = 'PgNumericBigIntBuilder';
+
+	constructor(name: T['name'], precision?: number, scale?: number) {
+		super(name, 'bigint', 'PgNumericBigInt');
+		this.config.precision = precision;
+		this.config.scale = scale;
+	}
+
+	/** @internal */
+	override build<TTableName extends string>(
+		table: AnyPgTable<{ name: TTableName }>,
+	): PgNumericBigInt<MakeColumnConfig<T, TTableName>> {
+		return new PgNumericBigInt<MakeColumnConfig<T, TTableName>>(
+			table,
+			this.config as ColumnBuilderRuntimeConfig<any, any>,
+		);
+	}
+}
+
+export class PgNumericBigInt<T extends ColumnBaseConfig<'bigint', 'PgNumericBigInt'>> extends PgColumn<T> {
+	static override readonly [entityKind]: string = 'PgNumericBigInt';
+
+	readonly precision: number | undefined;
+	readonly scale: number | undefined;
+
+	constructor(table: AnyPgTable<{ name: T['tableName'] }>, config: PgNumericBigIntBuilder<T>['config']) {
+		super(table, config);
+		this.precision = config.precision;
+		this.scale = config.scale;
+	}
+
+	override mapFromDriverValue = BigInt;
+
+	override mapToDriverValue = String;
+
+	getSQLType(): string {
+		if (this.precision !== undefined && this.scale !== undefined) {
+			return `numeric(${this.precision}, ${this.scale})`;
+		} else if (this.precision === undefined) {
+			return 'numeric';
+		} else {
+			return `numeric(${this.precision})`;
+		}
+	}
+}
+
+export type PgNumericConfig<T extends 'string' | 'number' | 'bigint' = 'string' | 'number' | 'bigint'> =
+	| { precision: number; scale?: number; mode?: T }
+	| { precision?: number; scale: number; mode?: T }
+	| { precision?: number; scale?: number; mode: T };
+
+export function numeric<TMode extends 'string' | 'number' | 'bigint'>(
+	config?: PgNumericConfig<TMode>,
+): Equal<TMode, 'number'> extends true ? PgNumericNumberBuilderInitial<''>
+	: Equal<TMode, 'bigint'> extends true ? PgNumericBigIntBuilderInitial<''>
+	: PgNumericBuilderInitial<''>;
+export function numeric<TName extends string, TMode extends 'string' | 'number' | 'bigint'>(
 	name: TName,
-	config?: PgNumericConfig,
-): PgNumericBuilderInitial<TName>;
+	config?: PgNumericConfig<TMode>,
+): Equal<TMode, 'number'> extends true ? PgNumericNumberBuilderInitial<TName>
+	: Equal<TMode, 'bigint'> extends true ? PgNumericBigIntBuilderInitial<TName>
+	: PgNumericBuilderInitial<TName>;
 export function numeric(a?: string | PgNumericConfig, b?: PgNumericConfig) {
 	const { name, config } = getColumnNameAndConfig<PgNumericConfig>(a, b);
-	return new PgNumericBuilder(name, config?.precision, config?.scale);
+	const mode = config?.mode;
+	return mode === 'number'
+		? new PgNumericNumberBuilder(name, config?.precision, config?.scale)
+		: mode === 'bigint'
+		? new PgNumericBigIntBuilder(name, config?.precision, config?.scale)
+		: new PgNumericBuilder(name, config?.precision, config?.scale);
 }
 
 export const decimal = numeric;
