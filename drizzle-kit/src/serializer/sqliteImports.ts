abeef5bf5d488682b3383eb318d48d480fde8e89
Meno Abels <meno.abels@adviser.com>
2025-03-20 22:10:20 +0100

    Fix loading a schema in a typescript project

--- drizzle-kit/src/serializer/sqliteImports.ts
+++ drizzle-kit/src/serializer/sqliteImports.ts
@@ -1,5 +1,9 @@
+import chalk from 'chalk';
 import { is } from 'drizzle-orm';
 import { AnySQLiteTable, SQLiteTable, SQLiteView } from 'drizzle-orm/sqlite-core';
+import * as esbuild from 'esbuild';
+import { mkdtemp, rm } from 'fs/promises';
+import { join, resolve } from 'path';
 import { safeRegister } from '../cli/commands/utils';
 
 export const prepareFromExports = (exports: Record<string, unknown>) => {
@@ -25,15 +29,23 @@ export const prepareFromSqliteImports = async (imports: string[]) => {
 	const views: SQLiteView[] = [];
 
 	const { unregister } = await safeRegister();
-	for (let i = 0; i < imports.length; i++) {
-		const it = imports[i];
 
-		const i0: Record<string, unknown> = require(`${it}`);
-		const prepared = prepareFromExports(i0);
+	const outDir = await mkdtemp('.drizzle-kit.sqlite-imports-');
+	let outFile = resolve(join(outDir, 'schema.js'));
+	console.log(chalk.grey(`Reading schema files '${JSON.stringify(imports)}'`));
+	const res = esbuild.buildSync({
+		entryPoints: imports,
+		bundle: true,
+		format: 'cjs',
+		outfile: outFile,
+	});
+	const i0: Record<string, unknown> = require(outFile);
+	const prepared = prepareFromExports(i0);
 
-		tables.push(...prepared.tables);
-		views.push(...prepared.views);
-	}
+	await rm(outDir, { recursive: true });
+
+	tables.push(...prepared.tables);
+	views.push(...prepared.views);
 
 	unregister();
 
