8a9c29fbbcb14799d90a1e9b4bb43819c5845de3
gp27 <gp27@users.noreply.github.com>
2025-04-24 20:18:29 +0200

    Added support for postgres CTE Materialization
    CTEs can now be used with an additional parameter: `db.$with('sq', { materialized: true }).as(...)`.
    It can be used to force postgres to materialize/inline a subquery

--- drizzle-orm/src/subquery.ts
+++ drizzle-orm/src/subquery.ts
@@ -22,9 +22,17 @@ export class Subquery<
 		alias: TAlias;
 		isWith: boolean;
 		usedTables?: string[];
+		isMaterialized?: boolean;
 	};
 
-	constructor(sql: SQL, fields: TSelectedFields, alias: string, isWith = false, usedTables: string[] = []) {
+	constructor(
+		sql: SQL,
+		fields: TSelectedFields,
+		alias: string,
+		isWith = false,
+		usedTables: string[] = [],
+		isMaterialized?: boolean,
+	) {
 		this._ = {
 			brand: 'Subquery',
 			sql,
@@ -32,6 +40,7 @@ export class Subquery<
 			alias: alias as TAlias,
 			isWith,
 			usedTables,
+			isMaterialized,
 		};
 	}
 
