8dd23e5e9610021211b144858cfb83667558e6f6
AndriiSherman <andreysherman11@gmail.com>
2025-05-03 13:55:21 +0300

    Add fix for Gel Schemas

--- drizzle-kit/src/cli/schema.ts
+++ drizzle-kit/src/cli/schema.ts
@@ -33,7 +33,7 @@ import { error, grey, MigrateProgress } from './views';
 const optionDialect = string('dialect')
 	.enum(...dialects)
 	.desc(
-		`Database dialect: 'postgresql', 'mysql', 'sqlite', 'turso' or 'singlestore'`,
+		`Database dialect: 'gel', 'postgresql', 'mysql', 'sqlite', 'turso' or 'singlestore'`,
 	);
 const optionOut = string().desc("Output folder, 'drizzle' by default");
 const optionConfig = string().desc('Path to drizzle config file');
