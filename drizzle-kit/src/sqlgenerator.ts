8689b9eb8d7343a6e5d0a07f267f969df79aed32
AndriiSherman <andreysherman11@gmail.com>
2025-06-17 15:28:02 +0300

    Fix halfvec

--- drizzle-kit/src/sqlgenerator.ts
+++ drizzle-kit/src/sqlgenerator.ts
@@ -136,6 +136,9 @@ const parseType = (schemaPrefix: string, type: string) => {
 		'char',
 		'vector',
 		'geometry',
+		'halfvec', 
+		'sparsevec', 
+		'bit'
 	];
 	const arrayDefinitionRegex = /\[\d*(?:\[\d*\])*\]/g;
 	const arrayDefinition = (type.match(arrayDefinitionRegex) ?? []).join('');
