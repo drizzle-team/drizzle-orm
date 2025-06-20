d76ea72c84043b3d289d27d4267de79e6ecde20c
Aleksandr Sherman <aleksandrserman@gmail.com>
2025-06-20 16:43:23 +0300

    [mssql]: updated tests

--- drizzle-kit/tests/mssql/views.test.ts
+++ drizzle-kit/tests/mssql/views.test.ts
@@ -1,7 +1,24 @@
 import { sql } from 'drizzle-orm';
 import { int, mssqlSchema, mssqlTable, mssqlView } from 'drizzle-orm/mssql-core';
-import { expect, test } from 'vitest';
-import { diff } from './mocks';
+import { afterAll, beforeAll, beforeEach, expect, test } from 'vitest';
+import { diff, prepareTestDatabase, push, TestDatabase } from './mocks';
+
+// @vitest-environment-options {"max-concurrency":1}
+let _: TestDatabase;
+let db: TestDatabase['db'];
+
+beforeAll(async () => {
+	_ = await prepareTestDatabase();
+	db = _.db;
+});
+
+afterAll(async () => {
+	await _.close();
+});
+
+beforeEach(async () => {
+	await _.clear();
+});
 
 test('create table and view #1', async () => {
 	const users = mssqlTable('users', {
@@ -12,11 +29,16 @@ test('create table and view #1', async () => {
 		view: mssqlView('some_view').as((qb) => qb.select().from(users)),
 	};
 
-	const { sqlStatements } = await diff({}, to, []);
-	expect(sqlStatements).toStrictEqual([
+	const { sqlStatements: st } = await diff({}, to, []);
+	const { sqlStatements: pst } = await push({ db, to: to });
+
+	const st0 = [
 		`CREATE TABLE [users] (\n\t[id] int,\n\tCONSTRAINT [users_pkey] PRIMARY KEY([id])\n);\n`,
 		`CREATE VIEW [some_view] AS (select [id] from [users]);`,
-	]);
+	];
+
+	expect(st).toStrictEqual(st0);
+	expect(pst).toStrictEqual(st0);
 });
 
 test('create table and view #2', async () => {
@@ -28,11 +50,15 @@ test('create table and view #2', async () => {
 		view: mssqlView('some_view', { id: int('id') }).as(sql`SELECT * FROM ${users}`),
 	};
 
-	const { sqlStatements } = await diff({}, to, []);
-	expect(sqlStatements).toStrictEqual([
+	const { sqlStatements: st } = await diff({}, to, []);
+	const { sqlStatements: pst } = await push({ db, to: to });
+
+	const st0 = [
 		`CREATE TABLE [users] (\n\t[id] int,\n\tCONSTRAINT [users_pkey] PRIMARY KEY([id])\n);\n`,
 		`CREATE VIEW [some_view] AS (SELECT * FROM [users]);`,
-	]);
+	];
+	expect(st).toStrictEqual(st0);
+	expect(pst).toStrictEqual(st0);
 });
 
 test('create table and view #3', async () => {
@@ -46,14 +72,18 @@ test('create table and view #3', async () => {
 			encryption: true,
 			schemaBinding: true,
 			viewMetadata: true,
-		}).as(sql`SELECT * FROM ${users}`),
+		}).as(sql`SELECT ${users.id} FROM ${users}`),
 	};
 
-	const { sqlStatements } = await diff({}, to, []);
-	expect(sqlStatements).toStrictEqual([
+	const { sqlStatements: st } = await diff({}, to, []);
+	const { sqlStatements: pst } = await push({ db, to: to });
+
+	const st0 = [
 		`CREATE TABLE [users] (\n\t[id] int,\n\tCONSTRAINT [users_pkey] PRIMARY KEY([id])\n);\n`,
 		`CREATE VIEW [some_view1]\nWITH ENCRYPTION, SCHEMABINDING, VIEW_METADATA AS (SELECT * FROM [users])\nWITH CHECK OPTION;`,
-	]);
+	];
+	expect(st).toStrictEqual(st0);
+	expect(pst).toStrictEqual(st0);
 });
 
 test('create table and view #4', async () => {
@@ -70,19 +100,20 @@ test('create table and view #4', async () => {
 			encryption: true,
 			schemaBinding: true,
 			viewMetadata: true,
-		}).as(sql`SELECT * FROM ${users}`),
+		}).as(sql`SELECT ${users.id} FROM ${users}`),
 	};
 
-	const { sqlStatements } = await diff({}, to, []);
+	const { sqlStatements: st } = await diff({}, to, []);
+	const { sqlStatements: pst } = await push({ db, to: to });
 
-	expect(sqlStatements.length).toBe(3);
-	expect(sqlStatements[0]).toBe(`CREATE SCHEMA [new_schema];\n`);
-	expect(sqlStatements[1]).toBe(
+	const st0 = [
+		`CREATE SCHEMA [new_schema];\n`,
 		`CREATE TABLE [new_schema].[users] (\n\t[id] int,\n\tCONSTRAINT [users_pkey] PRIMARY KEY([id])\n);\n`,
-	);
-	expect(sqlStatements[2]).toBe(
 		`CREATE VIEW [new_schema].[some_view1]\nWITH ENCRYPTION, SCHEMABINDING, VIEW_METADATA AS (SELECT * FROM [new_schema].[users])\nWITH CHECK OPTION;`,
-	);
+	];
+
+	expect(st).toStrictEqual(st0);
+	expect(pst).toStrictEqual(st0);
 });
 
 test('create table and view #5', async () => {
@@ -97,6 +128,7 @@ test('create table and view #5', async () => {
 
 	// view_name_duplicate
 	await expect(diff({}, to, [])).rejects.toThrow();
+	await expect(push({ db, to: to })).rejects.toThrow();
 });
 
 test('create table and view #6', async () => {
@@ -108,13 +140,15 @@ test('create table and view #6', async () => {
 		view1: mssqlView('some_view', { id: int('id') }).with({ checkOption: true }).as(sql`SELECT * FROM ${users}`),
 	};
 
-	const { sqlStatements } = await diff({}, to, []);
+	const { sqlStatements: st } = await diff({}, to, []);
+	const { sqlStatements: pst } = await push({ db, to: to });
 
-	expect(sqlStatements.length).toBe(2);
-	expect(sqlStatements[0]).toBe(
+	const st0 = [
 		`CREATE TABLE [users] (\n\t[id] int,\n\tCONSTRAINT [users_pkey] PRIMARY KEY([id])\n);\n`,
-	);
-	expect(sqlStatements[1]).toBe(`CREATE VIEW [some_view] AS (SELECT * FROM [users])\nWITH CHECK OPTION;`);
+		`CREATE VIEW [some_view] AS (SELECT * FROM [users])\nWITH CHECK OPTION;`,
+	];
+	expect(st).toStrictEqual(st0);
+	expect(pst).toStrictEqual(st0);
 });
 
 test('create view with existing flag', async () => {
@@ -131,9 +165,12 @@ test('create view with existing flag', async () => {
 		view1: mssqlView('some_view', { id: int('id') }).with({ checkOption: true }).existing(),
 	};
 
-	const { sqlStatements } = await diff(from, to, []);
+	const { sqlStatements: st } = await diff(from, to, []);
+	await push({ db, to: from });
+	const { sqlStatements: pst } = await push({ db, to: to });
 
-	expect(sqlStatements.length).toBe(0);
+	expect(st).toStrictEqual([]);
+	expect(pst).toStrictEqual([]);
 });
 
 test('drop view #1', async () => {
@@ -150,10 +187,13 @@ test('drop view #1', async () => {
 		users: users,
 	};
 
-	const { sqlStatements } = await diff(from, to, []);
+	const { sqlStatements: st } = await diff(from, to, []);
+	await push({ db, to: from });
+	const { sqlStatements: pst } = await push({ db, to: to });
 
-	expect(sqlStatements.length).toBe(1);
-	expect(sqlStatements[0]).toBe(`DROP VIEW [some_view];`);
+	const st0 = [`DROP VIEW [some_view];`];
+	expect(st).toStrictEqual(st0);
+	expect(pst).toStrictEqual(st0);
 });
 
 test('drop view with existing flag', async () => {
@@ -170,23 +210,33 @@ test('drop view with existing flag', async () => {
 		users: users,
 	};
 
-	const { sqlStatements } = await diff(from, to, []);
-	expect(sqlStatements.length).toBe(0);
+	const { sqlStatements: st } = await diff(from, to, []);
+	await push({ db, to: from });
+	const { sqlStatements: pst } = await push({ db, to: to });
+
+	expect(st).toStrictEqual([]);
+	expect(pst).toStrictEqual([]);
 });
 
 test('rename view #1', async () => {
+	const users = mssqlTable('users', { id: int() });
 	const from = {
+		users,
 		view: mssqlView('some_view', { id: int('id') }).as(sql`SELECT * FROM [users]`),
 	};
 
 	const to = {
+		users,
 		view: mssqlView('new_some_view', { id: int('id') }).as(sql`SELECT * FROM [users]`),
 	};
 
-	const { sqlStatements } = await diff(from, to, ['dbo.some_view->dbo.new_some_view']);
+	const { sqlStatements: st } = await diff(from, to, ['dbo.some_view->dbo.new_some_view']);
+	await push({ db, to: from });
+	const { sqlStatements: pst } = await push({ db, to: to, renames: ['dbo.some_view->dbo.new_some_view'] });
 
-	expect(sqlStatements.length).toBe(1);
-	expect(sqlStatements[0]).toBe(`EXEC sp_rename 'some_view', [new_some_view];`);
+	const st0 = [`EXEC sp_rename 'some_view', [new_some_view];`];
+	expect(st).toStrictEqual(st0);
+	expect(pst).toStrictEqual(st0);
 });
 
 test('rename view with existing flag', async () => {
@@ -198,28 +248,36 @@ test('rename view with existing flag', async () => {
 		view: mssqlView('new_some_view', { id: int('id') }).existing(),
 	};
 
-	const { sqlStatements } = await diff(from, to, ['dbo.some_view->dbo.new_some_view']);
+	const { sqlStatements: st } = await diff(from, to, ['dbo.some_view->dbo.new_some_view']);
+	await push({ db, to: from });
+	const { sqlStatements: pst } = await push({ db, to: to, renames: ['dbo.some_view->dbo.new_some_view'] });
 
-	expect(sqlStatements.length).toBe(0);
+	expect(st).toStrictEqual([]);
+	expect(pst).toStrictEqual([]);
 });
 
 test('view alter schema', async () => {
 	const schema = mssqlSchema('new_schema');
+	const users = mssqlTable('users', { id: int() });
 
 	const from = {
+		users,
 		view: mssqlView('some_view', { id: int('id') }).as(sql`SELECT * FROM [users]`),
 	};
 
 	const to = {
+		users,
 		schema,
 		view: schema.view('some_view', { id: int('id') }).as(sql`SELECT * FROM [users]`),
 	};
 
-	const { sqlStatements } = await diff(from, to, ['dbo.some_view->new_schema.some_view']);
+	const { sqlStatements: st } = await diff(from, to, ['dbo.some_view->new_schema.some_view']);
+	await push({ db, to: from });
+	const { sqlStatements: pst } = await push({ db, to: to, renames: ['dbo.some_view->new_schema.some_view'] });
 
-	expect(sqlStatements.length).toBe(2);
-	expect(sqlStatements[0]).toBe(`CREATE SCHEMA [new_schema];\n`);
-	expect(sqlStatements[1]).toBe(`ALTER SCHEMA [new_schema] TRANSFER [some_view];`);
+	const st0 = [`CREATE SCHEMA [new_schema];\n`, `ALTER SCHEMA [new_schema] TRANSFER [some_view];`];
+	expect(st).toStrictEqual(st0);
+	expect(pst).toStrictEqual(st0);
 });
 
 test('view alter schema with existing flag', async () => {
@@ -234,10 +292,13 @@ test('view alter schema with existing flag', async () => {
 		view: schema.view('some_view', { id: int('id') }).existing(),
 	};
 
-	const { sqlStatements } = await diff(from, to, ['dbo.some_view->new_schema.some_view']);
+	const { sqlStatements: st } = await diff(from, to, ['dbo.some_view->new_schema.some_view']);
+	await push({ db, to: from });
+	const { sqlStatements: pst } = await push({ db, to: to, renames: ['dbo.some_view->new_schema.some_view'] });
 
-	expect(sqlStatements.length).toBe(1);
-	expect(sqlStatements[0]).toBe(`CREATE SCHEMA [new_schema];\n`);
+	const st0 = [`CREATE SCHEMA [new_schema];\n`];
+	expect(st).toStrictEqual(st0);
+	expect(pst).toStrictEqual(st0);
 });
 
 test('add with option to view #1', async () => {
@@ -255,12 +316,13 @@ test('add with option to view #1', async () => {
 		view: mssqlView('some_view').with({ encryption: true }).as((qb) => qb.select().from(users)),
 	};
 
-	const { sqlStatements } = await diff(from, to, []);
+	const { sqlStatements: st } = await diff(from, to, []);
+	await push({ db, to: from });
+	const { sqlStatements: pst } = await push({ db, to: to });
 
-	expect(sqlStatements.length).toBe(1);
-	expect(sqlStatements[0]).toBe(
-		`ALTER VIEW [some_view]\nWITH ENCRYPTION AS (select [id] from [users]);`,
-	);
+	const st0 = [`ALTER VIEW [some_view]\nWITH ENCRYPTION AS (select [id] from [users]);`];
+	expect(st).toStrictEqual(st0);
+	expect(pst).toStrictEqual(st0);
 });
 
 test('add with option to view with existing flag', async () => {
@@ -278,11 +340,15 @@ test('add with option to view with existing flag', async () => {
 		view: mssqlView('some_view', {}).with({ schemaBinding: true }).existing(),
 	};
 
-	const { sqlStatements } = await diff(from, to, []);
-	expect(sqlStatements.length).toBe(0);
+	const { sqlStatements: st } = await diff(from, to, []);
+	await push({ db, to: from });
+	const { sqlStatements: pst } = await push({ db, to: to });
+
+	expect(st).toStrictEqual([]);
+	expect(pst).toStrictEqual([]);
 });
 
-test('drop with option from view #1', async () => {
+test.only('drop with option from view #1', async () => {
 	const users = mssqlTable('users', {
 		id: int('id').primaryKey().notNull(),
 	});
@@ -299,12 +365,13 @@ test('drop with option from view #1', async () => {
 		view: mssqlView('some_view').as((qb) => qb.select().from(users)),
 	};
 
-	const { sqlStatements } = await diff(from, to, []);
+	const { sqlStatements: st } = await diff(from, to, []);
+	await push({ db, to: from, log: 'statements' });
+	const { sqlStatements: pst } = await push({ db, to: to });
 
-	expect(sqlStatements.length).toBe(1);
-	expect(sqlStatements[0]).toBe(
-		`ALTER VIEW [some_view] AS (select [id] from [users]);`,
-	);
+	const st0 = [`ALTER VIEW [some_view] AS (select [id] from [users]);`];
+	expect(st).toStrictEqual(st0);
+	expect(pst).toStrictEqual(st0);
 });
 
 test('drop with option from view with existing flag', async () => {
@@ -323,9 +390,12 @@ test('drop with option from view with existing flag', async () => {
 		view: mssqlView('some_view', {}).existing(),
 	};
 
-	const { sqlStatements } = await diff(from, to, []);
+	const { sqlStatements: st } = await diff(from, to, []);
+	await push({ db, to: from });
+	const { sqlStatements: pst } = await push({ db, to: to });
 
-	expect(sqlStatements.length).toBe(0);
+	expect(st).toStrictEqual(0);
+	expect(pst).toStrictEqual(0);
 });
 
 test('alter with option in view #1', async () => {
@@ -343,12 +413,13 @@ test('alter with option in view #1', async () => {
 		view: mssqlView('some_view').with({ checkOption: true }).as((qb) => qb.select().from(users)),
 	};
 
-	const { sqlStatements } = await diff(from, to, []);
+	const { sqlStatements: st } = await diff(from, to, []);
+	await push({ db, to: from });
+	const { sqlStatements: pst } = await push({ db, to: to });
 
-	expect(sqlStatements.length).toBe(1);
-	expect(sqlStatements[0]).toBe(
-		`ALTER VIEW [some_view] AS (select [id] from [users])\nWITH CHECK OPTION;`,
-	);
+	const st0 = [`ALTER VIEW [some_view] AS (select [id] from [users])\nWITH CHECK OPTION;`];
+	expect(st).toStrictEqual(st0);
+	expect(pst).toStrictEqual(st0);
 });
 
 test('alter with option in view with existing flag', async () => {
@@ -366,9 +437,12 @@ test('alter with option in view with existing flag', async () => {
 		view: mssqlView('some_view', {}).with({ checkOption: true }).existing(),
 	};
 
-	const { sqlStatements } = await diff(from, to, []);
+	const { sqlStatements: st } = await diff(from, to, []);
+	await push({ db, to: from });
+	const { sqlStatements: pst } = await push({ db, to: to });
 
-	expect(sqlStatements.length).toBe(0);
+	expect(st).toStrictEqual([]);
+	expect(pst).toStrictEqual([]);
 });
 
 test('alter with option in view #2', async () => {
@@ -388,12 +462,13 @@ test('alter with option in view #2', async () => {
 		) => qb.selectDistinct().from(users)),
 	};
 
-	const { sqlStatements } = await diff(from, to, []);
+	const { sqlStatements: st } = await diff(from, to, []);
+	await push({ db, to: from });
+	const { sqlStatements: pst } = await push({ db, to: to });
 
-	expect(sqlStatements.length).toBe(1);
-	expect(sqlStatements[0]).toBe(
-		`ALTER VIEW [some_view] AS (select distinct [id] from [users]);`,
-	);
+	const st0 = [`ALTER VIEW [some_view] AS (select distinct [id] from [users]);`];
+	expect(st).toStrictEqual(st0);
+	expect(pst).toStrictEqual(st0);
 });
 
 test('alter view ".as" value', async () => {
@@ -411,12 +486,13 @@ test('alter view ".as" value', async () => {
 		view: mssqlView('some_view', { id: int('id') }).with().as(sql`SELECT '1234'`),
 	};
 
-	const { sqlStatements, statements } = await diff(from, to, []);
+	const { sqlStatements: st } = await diff(from, to, []);
+	await push({ db, to: from });
+	const { sqlStatements: pst } = await push({ db, to: to });
 
-	expect(sqlStatements).toStrictEqual([
-		'DROP VIEW [some_view];',
-		`CREATE VIEW [some_view] AS (SELECT '1234');`,
-	]);
+	const st0 = ['DROP VIEW [some_view];', `CREATE VIEW [some_view] AS (SELECT '1234');`];
+	expect(st).toStrictEqual(st0);
+	expect(pst).toStrictEqual(st0);
 });
 
 test('alter view ".as" value with existing flag', async () => {
@@ -434,9 +510,12 @@ test('alter view ".as" value with existing flag', async () => {
 		view: mssqlView('some_view', { id: int('id') }).with().existing(),
 	};
 
-	const { sqlStatements } = await diff(from, to, []);
+	const { sqlStatements: st } = await diff(from, to, []);
+	await push({ db, to: from });
+	const { sqlStatements: pst } = await push({ db, to: to });
 
-	expect(sqlStatements.length).toBe(0);
+	expect(st).toStrictEqual([]);
+	expect(pst).toStrictEqual([]);
 });
 
 test('drop existing flag', async () => {
@@ -454,11 +533,13 @@ test('drop existing flag', async () => {
 		view: mssqlView('some_view', { id: int('id') }).as(sql`SELECT 'asd'`),
 	};
 
-	const { sqlStatements } = await diff(from, to, []);
+	const { sqlStatements: st } = await diff(from, to, []);
+	await push({ db, to: from });
+	const { sqlStatements: pst } = await push({ db, to: to });
 
-	expect(sqlStatements).toStrictEqual([
-		`CREATE VIEW [some_view] AS (SELECT 'asd');`,
-	]);
+	const st0 = [`CREATE VIEW [some_view] AS (SELECT 'asd');`];
+	expect(st).toStrictEqual(st0);
+	expect(pst).toStrictEqual(st0);
 });
 
 test('set existing', async () => {
@@ -476,10 +557,13 @@ test('set existing', async () => {
 		view: mssqlView('some_view', { id: int('id') }).existing(),
 	};
 
-	const { sqlStatements } = await diff(from, to, []);
+	const { sqlStatements: st } = await diff(from, to, []);
+	await push({ db, to: from });
+	const { sqlStatements: pst } = await push({ db, to: to });
 
-	expect(sqlStatements.length).toBe(1);
-	expect(sqlStatements).toStrictEqual([`DROP VIEW [some_view];`]);
+	const st0 = [`DROP VIEW [some_view];`];
+	expect(st).toStrictEqual(st0);
+	expect(pst).toStrictEqual(st0);
 });
 
 test('rename view and alter view', async () => {
@@ -493,11 +577,16 @@ test('rename view and alter view', async () => {
 		),
 	};
 
-	const { sqlStatements } = await diff(from, to, ['dbo.some_view->dbo.new_some_view']);
+	const { sqlStatements: st } = await diff(from, to, ['dbo.some_view->dbo.new_some_view']);
+	await push({ db, to: from });
+	const { sqlStatements: pst } = await push({ db, to: to, renames: ['dbo.some_view->dbo.new_some_view'] });
 
-	expect(sqlStatements.length).toBe(2);
-	expect(sqlStatements[0]).toBe(`EXEC sp_rename 'some_view', [new_some_view];`);
-	expect(sqlStatements[1]).toBe(`ALTER VIEW [new_some_view] AS (SELECT * FROM [users])\nWITH CHECK OPTION;`);
+	const st0 = [
+		`EXEC sp_rename 'some_view', [new_some_view];`,
+		`ALTER VIEW [new_some_view] AS (SELECT * FROM [users])\nWITH CHECK OPTION;`,
+	];
+	expect(st).toStrictEqual(st0);
+	expect(pst).toStrictEqual(st0);
 });
 
 test('moved schema and alter view', async () => {
@@ -514,10 +603,14 @@ test('moved schema and alter view', async () => {
 		),
 	};
 
-	const { sqlStatements } = await diff(from, to, ['dbo.some_view->my_schema.some_view']);
+	const { sqlStatements: st } = await diff(from, to, ['dbo.some_view->my_schema.some_view']);
+	await push({ db, to: from });
+	const { sqlStatements: pst } = await push({ db, to: to, renames: ['dbo.some_view->my_schema.some_view'] });
 
-	expect(sqlStatements).toStrictEqual([
+	const st0 = [
 		`ALTER SCHEMA [my_schema] TRANSFER [some_view];`,
 		`ALTER VIEW [my_schema].[some_view] AS (SELECT * FROM [users])\nWITH CHECK OPTION;`,
-	]);
+	];
+	expect(st).toStrictEqual(st0);
+	expect(pst).toStrictEqual(st0);
 });
