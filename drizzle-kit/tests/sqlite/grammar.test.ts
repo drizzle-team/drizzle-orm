a0b8cf2bd490c7355593de27d75a622c8ac6a6a7
Alex Blokh <aleksandrblokh@gmail.com>
2025-07-02 16:04:56 +0200

    +

--- drizzle-kit/tests/sqlite/grammar.test.ts
+++ drizzle-kit/tests/sqlite/grammar.test.ts
@@ -1,6 +1,6 @@
 import { parseViewSQL } from 'src/dialects/sqlite/grammar';
 import { test } from 'vitest';
 
-test.only('view definition', () => {
+test('view definition', () => {
 	console.log(parseViewSQL('CREATE VIEW current_cycle AS\nSELECT\n* from users;'));
 });
