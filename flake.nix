a80dd79b23d376280f637d5ccb5bbcc7ce6d9a6f
Jacky Zhao <j.zhao2k19@gmail.com>
2025-08-15 21:04:15 -0700

    fix flake

--- flake.nix
+++ flake.nix
@@ -9,15 +9,10 @@
       devShell = pkgs.mkShell {
         nativeBuildInputs = [ pkgs.bashInteractive ];
         buildInputs = with pkgs; [
-          nodePackages.prisma
+          nodejs
+          pnpm
+          typescript
         ];
-        shellHook = with pkgs; ''
-          export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
-          export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
-          export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
-          export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
-          export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
-        '';
       };
     });
 }
