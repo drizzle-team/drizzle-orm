4f8c7f1f51d01c7597221b9b3f24ad102f0ce433
Sukairo-02 <sreka9056@gmail.com>
2025-09-05 06:51:10 +0300

    seeder tests fix

--- drizzle-seed/tests/singlestore/softRelationsTest/singlestoreSchema.ts
+++ drizzle-seed/tests/singlestore/softRelationsTest/singlestoreSchema.ts
@@ -1,4 +1,4 @@
-import { relations } from 'drizzle-orm';
+import { relations } from 'drizzle-orm/_relations';
 import { float, int, singlestoreTable, text, timestamp, varchar } from 'drizzle-orm/singlestore-core';
 
 export const customers = singlestoreTable('customer', {
