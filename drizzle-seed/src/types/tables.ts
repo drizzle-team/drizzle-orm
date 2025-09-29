9f0da8c494facc276691b49f1a55ec6bc7b43938
OleksiiKH0240 <homenko0240@gmail.com>
2025-09-29 14:57:28 +0300

    [drizzle-seed] added support for composite unique constraints; added tests for composite unique constraints

--- drizzle-seed/src/types/tables.ts
+++ drizzle-seed/src/types/tables.ts
@@ -1,12 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import type { AnyColumn } from 'drizzle-orm';
-import type { CockroachTable, ForeignKey as CockroachFK } from 'drizzle-orm/cockroach-core';
-import type { ForeignKey as MsSqlFK, MsSqlTable } from 'drizzle-orm/mssql-core';
-import type { ForeignKey as MySqlFK, MySqlTable } from 'drizzle-orm/mysql-core';
-import type { ForeignKey as PgFK, PgTable } from 'drizzle-orm/pg-core';
-import type { SingleStoreTable } from 'drizzle-orm/singlestore-core';
-import type { ForeignKey as SQLiteFK, SQLiteTable } from 'drizzle-orm/sqlite-core';
+import type {
+	CockroachTable,
+	ForeignKey as CockroachFK,
+	UniqueConstraint as CockroachUniCon,
+} from 'drizzle-orm/cockroach-core';
+import type { ForeignKey as MsSqlFK, MsSqlTable, UniqueConstraint as MsSqlUniCon } from 'drizzle-orm/mssql-core';
+import type { ForeignKey as MySqlFK, MySqlTable, UniqueConstraint as MySqlUniCon } from 'drizzle-orm/mysql-core';
+import type { ForeignKey as PgFK, PgTable, UniqueConstraint as PgUniCon } from 'drizzle-orm/pg-core';
+import type { SingleStoreTable, UniqueConstraint as SingleStoreUniCon } from 'drizzle-orm/singlestore-core';
+import type { ForeignKey as SQLiteFK, SQLiteTable, UniqueConstraint as SQLiteUniCon } from 'drizzle-orm/sqlite-core';
 
 export type Column = {
 	name: string;
@@ -34,6 +38,7 @@ export type Column = {
 export type Table = {
 	name: string;
 	columns: Column[];
+	uniqueConstraints: string[][];
 	primaryKeys: string[];
 };
 
@@ -58,4 +63,18 @@ export type Prettify<T> =
 
 export type DrizzleTable = PgTable | MySqlTable | SQLiteTable | CockroachTable | MsSqlTable | SingleStoreTable;
 export type DrizzleForeignKey = PgFK | MySqlFK | SQLiteFK | CockroachFK | MsSqlFK;
-export type TableConfigT = { name: string; schema?: string; columns: AnyColumn[]; foreignKeys?: DrizzleForeignKey[] };
+export type DrizzleUniqueConstraint =
+	| PgUniCon
+	| MySqlUniCon
+	| SQLiteUniCon
+	| CockroachUniCon
+	| MsSqlUniCon
+	| SingleStoreUniCon;
+
+export type TableConfigT = {
+	name: string;
+	schema?: string;
+	columns: AnyColumn[];
+	uniqueConstraints: DrizzleUniqueConstraint[];
+	foreignKeys?: DrizzleForeignKey[];
+};
