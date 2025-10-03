17c242e0205a3768b4f1cbe998a7a0c2eb55cbcb
Sukairo-02 <sreka9056@gmail.com>
2025-10-03 05:11:20 +0300

    Export fix

--- drizzle-orm/src/bun-sql/session.ts
+++ drizzle-orm/src/bun-sql/session.ts
@@ -1,4 +1,1 @@
-export * as mysql from './mysql/session.ts';
 export * from './postgres/session.ts';
-export * as postgres from './postgres/session.ts';
-export * as sqlite from './sqlite/session.ts';
