0da40ec2431e6623f33b5b6c4310413cb9c8ef8d
Alex Blokh <aleksandrblokh@gmail.com>
2025-10-26 11:58:15 +0100

    fix singlestore

--- integration-tests/tests/singlestore/singlestore-common.ts
+++ integration-tests/tests/singlestore/singlestore-common.ts
@@ -281,6 +281,8 @@ export function tests(driver?: string) {
 				db.execute(sql`drop table if exists ${rqbUser};`),
 				db.execute(sql`drop table if exists ${rqbPost};`),
 				db.execute(sql`drop schema if exists \`mySchema\`;`),
+				db.execute(sql`drop schema if exists aggregate_table;`),
+				// db.execute(sql`drop schema if exists \`mySchema\`;`),
 			]);
 			await db.execute(sql`create schema if not exists \`mySchema\`;`);
 			await Promise.all([
