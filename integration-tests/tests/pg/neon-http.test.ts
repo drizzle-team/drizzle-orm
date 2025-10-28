fa0d349798d5b768690d36d84320f92842988fc8
Sukairo-02 <sreka9056@gmail.com>
2025-10-28 17:41:56 +0200

    Updated all types test

--- integration-tests/tests/pg/neon-http.test.ts
+++ integration-tests/tests/pg/neon-http.test.ts
@@ -579,6 +579,7 @@ test('all types - neon-http', async (ctx) => {
 		smallserial: 15,
 		bigint53: 9007199254740991,
 		bigint64: 5044565289845416380n,
+		bigintString: '5044565289845416380',
 		bigserial53: 9007199254740991,
 		bigserial64: 5044565289845416380n,
 		bool: true,
@@ -628,6 +629,7 @@ test('all types - neon-http', async (ctx) => {
 		varchar: 'C4-',
 		arrbigint53: [9007199254740991],
 		arrbigint64: [5044565289845416380n],
+		arrbigintString: ['5044565289845416380'],
 		arrbool: [true],
 		arrbytea: [Buffer.from('BYTES')],
 		arrchar: ['c'],
@@ -684,6 +686,7 @@ test('all types - neon-http', async (ctx) => {
 		int: number | null;
 		bigint53: number | null;
 		bigint64: bigint | null;
+		bigintString: string | null;
 		bool: boolean | null;
 		bytea: Buffer | null;
 		char: string | null;
@@ -726,6 +729,7 @@ test('all types - neon-http', async (ctx) => {
 		arrint: number[] | null;
 		arrbigint53: number[] | null;
 		arrbigint64: bigint[] | null;
+		arrbigintString: string[] | null;
 		arrbool: boolean[] | null;
 		arrbytea: Buffer[] | null;
 		arrchar: string[] | null;
@@ -771,6 +775,7 @@ test('all types - neon-http', async (ctx) => {
 			int: 621,
 			bigint53: 9007199254740991,
 			bigint64: 5044565289845416380n,
+			bigintString: '5044565289845416380',
 			bool: true,
 			bytea: null,
 			char: 'c',
@@ -806,6 +811,7 @@ test('all types - neon-http', async (ctx) => {
 			arrint: [621],
 			arrbigint53: [9007199254740991],
 			arrbigint64: [5044565289845416380n],
+			arrbigintString: ['5044565289845416380'],
 			arrbool: [true],
 			arrbytea: [Buffer.from('BYTES')],
 			arrchar: ['c'],
