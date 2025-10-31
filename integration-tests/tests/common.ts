b39ef94533e242be164a723632f3aedcb60c5299
Alex Blokh <aleksandrblokh@gmail.com>
2025-10-31 22:34:11 +0100

    +

--- integration-tests/tests/common.ts
+++ integration-tests/tests/common.ts
@@ -1,7 +1,9 @@
-export function skipTests() {
-	// beforeEach((ctx) => {
-	// 	if (ctx.task.suite?.name === 'common' && names.includes(ctx.task.name)) {
-	// 		ctx.skip();
-	// 	}
-	// });
+import { beforeEach } from 'vitest';
+
+export function skipTests(names: string[]) {
+	beforeEach((ctx) => {
+		if (ctx.task.suite?.name === 'common' && names.includes(ctx.task.name)) {
+			ctx.skip();
+		}
+	});
 }
