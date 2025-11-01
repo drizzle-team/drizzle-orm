13e494fda7128964d049e924a1c1fd8ac1d0f4dd
Alex Blokh <aleksandrblokh@gmail.com>
2025-11-01 16:52:25 +0100

    massive perf boost for imports tests

--- integration-tests/tests/imports/index.test.ts
+++ integration-tests/tests/imports/index.test.ts
@@ -9,39 +9,7 @@ const IMPORTS_FOLDER = 'tests/imports/files';
 const folderPath = '../drizzle-orm/dist/package.json';
 const pj = JSON.parse(fs.readFileSync(folderPath, 'utf8'));
 
-if (!fs.existsSync(IMPORTS_FOLDER)) {
-	fs.mkdirSync(IMPORTS_FOLDER);
-}
-
-it('dynamic imports check for CommonJS', async () => {
-	const promises: ProcessPromise[] = [];
-	for (const [i, key] of Object.keys(pj['exports']).entries()) {
-		const o1 = path.join('drizzle-orm', key);
-		if (
-			o1.startsWith('drizzle-orm/bun-sqlite') || o1.startsWith('drizzle-orm/pglite')
-			|| o1.startsWith('drizzle-orm/expo-sqlite') || o1.startsWith('drizzle-orm/libsql/wasm')
-			|| o1.startsWith('drizzle-orm/bun-sql') || o1.startsWith('drizzle-orm/tursodatabase/wasm')
-			|| o1.startsWith('drizzle-orm/prisma')
-		) {
-			continue;
-		}
-		fs.writeFileSync(`${IMPORTS_FOLDER}/imports_${i}.cjs`, 'requ');
-		fs.appendFileSync(`${IMPORTS_FOLDER}/imports_${i}.cjs`, 'ire("' + o1 + '");\n', {});
-
-		// fs.writeFileSync(`${IMPORTS_FOLDER}/imports_${i}.mjs`, 'imp');
-		// fs.appendFileSync(`${IMPORTS_FOLDER}/imports_${i}.mjs`, 'ort "' + o1 + '"\n', {});
-
-		promises.push(
-			$`node ${IMPORTS_FOLDER}/imports_${i}.cjs`.nothrow(),
-			// $`node ${IMPORTS_FOLDER}/imports_${i}.mjs`.nothrow(),
-		);
-	}
-	const results = await Promise.all(promises);
-
-	for (const result of results) {
-		expect(result.exitCode, result.message).toBe(0);
-	}
-});
+fs.mkdirSync(IMPORTS_FOLDER, { recursive: true });
 
 function chunk<T>(arr: T[], size: number): T[][] {
 	const chunks: T[][] = [];
@@ -49,6 +17,41 @@ function chunk<T>(arr: T[], size: number): T[][] {
 		chunks.push(arr.slice(i, i + size));
 	}
 	return chunks;
+}
+
+const promisesCJS: ProcessPromise[] = [];
+for (const [i, key] of Object.keys(pj['exports']).entries()) {
+	const o1 = path.join('drizzle-orm', key);
+	if (
+		o1.startsWith('drizzle-orm/bun-sqlite') || o1.startsWith('drizzle-orm/pglite')
+		|| o1.startsWith('drizzle-orm/expo-sqlite') || o1.startsWith('drizzle-orm/libsql/wasm')
+		|| o1.startsWith('drizzle-orm/bun-sql') || o1.startsWith('drizzle-orm/tursodatabase/wasm')
+		|| o1.startsWith('drizzle-orm/prisma')
+	) {
+		continue;
+	}
+	fs.writeFileSync(`${IMPORTS_FOLDER}/imports_${i}.cjs`, 'requ');
+	fs.appendFileSync(`${IMPORTS_FOLDER}/imports_${i}.cjs`, 'ire("' + o1 + '");\n', {});
+
+	// fs.writeFileSync(`${IMPORTS_FOLDER}/imports_${i}.mjs`, 'imp');
+	// fs.appendFileSync(`${IMPORTS_FOLDER}/imports_${i}.mjs`, 'ort "' + o1 + '"\n', {});
+
+	promisesCJS.push(
+		$`node ${IMPORTS_FOLDER}/imports_${i}.cjs`.nothrow(),
+		// $`node ${IMPORTS_FOLDER}/imports_${i}.mjs`.nothrow(),
+	);
+}
+
+const chunksCJS = chunk(promisesCJS, 20);
+
+for (const c of chunksCJS) {
+	it.concurrent('dynamic imports check for CommonJS chunk', async () => {
+		const results = await Promise.all(c);
+
+		for (const result of results) {
+			expect(result.exitCode, result.message).toBe(0);
+		}
+	});
 }
 
 const promises: ProcessPromise[] = [];
@@ -68,9 +71,10 @@ for (const [i, key] of Object.keys(pj['exports']).entries()) {
 		$`node --import import-in-the-middle/hook.mjs ${IMPORTS_FOLDER}/imports_${i}.mjs`.nothrow(),
 	);
 }
-const chunks = chunk(promises, 10);
 
-for (const c of chunks) {
+const chunksESM = chunk(promises, 20);
+
+for (const c of chunksESM) {
 	it.concurrent('dynamic imports check for ESM chunk', async () => {
 		const results = await Promise.all(c);
 
