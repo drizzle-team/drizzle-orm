7dc5abd729fd49d30ecc81e9f9fe6115d5f03856
Alex Blokh <aleksandrblokh@gmail.com>
2025-11-03 17:30:59 +0100

    cockroach more concurrent tests

--- drizzle-kit/tests/cockroach/defaults-without-tx.test.ts
+++ drizzle-kit/tests/cockroach/defaults-without-tx.test.ts
@@ -2,25 +2,25 @@ import { char, string, varchar } from 'drizzle-orm/cockroach-core';
 import { expect } from 'vitest';
 import { diffDefault, test } from './mocks';
 
-test('char + char arrays', async ({ db }) => {
+test.concurrent('char + char arrays', async ({ db }) => {
 	const res1_0 = await diffDefault(db, char().default('text'), `'text'`, { expectError: true });
 	// char is less than default
 	const res10 = await diffDefault(db, char({ length: 2 }).default('text'), `'text'`, { expectError: true });
 
-	expect.soft(res1_0).toStrictEqual([`Insert default failed`]);
-	expect.soft(res10).toStrictEqual([`Insert default failed`]);
+	expect(res1_0).toStrictEqual([`Insert default failed`]);
+	expect(res10).toStrictEqual([`Insert default failed`]);
 });
 
-test('varchar + varchar arrays', async ({ db }) => {
+test.concurrent('varchar + varchar arrays', async ({ db }) => {
 	// varchar length is less than default
 	const res10 = await diffDefault(db, varchar({ length: 2 }).default('text'), `'text'`, { expectError: true });
 
-	expect.soft(res10).toStrictEqual([`Insert default failed`]);
+	expect(res10).toStrictEqual([`Insert default failed`]);
 });
 
-test('string + string arrays', async ({ db }) => {
+test.concurrent('string + string arrays', async ({ db }) => {
 	// varchar length is less than default
 	const res10 = await diffDefault(db, string({ length: 2 }).default('text'), `'text'`, { expectError: true });
 
-	expect.soft(res10).toStrictEqual([`Insert default failed`]);
+	expect(res10).toStrictEqual([`Insert default failed`]);
 });
