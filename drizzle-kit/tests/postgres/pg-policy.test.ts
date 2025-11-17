a15212435d0999568eba54ba070cf2affb49fbff
OleksiiKH0240 <homenko0240@gmail.com>
2025-11-17 22:00:50 +0200

    [drizzle-kit] updated test

--- drizzle-kit/tests/postgres/pg-policy.test.ts
+++ drizzle-kit/tests/postgres/pg-policy.test.ts
@@ -1528,45 +1528,141 @@ test('alter policy in the table: using', async (t) => {
 });
 
 // https://github.com/drizzle-team/drizzle-orm/issues/4198
-test.skip('alter policy ...', async () => {
-	// TODO: finish the test
+test('create/alter policy for select, insert, update, delete', async () => {
 	const authenticatedRole = pgRole('authenticated');
-	const policyForSelect = pgPolicy('Access pipeline tasks based on permissions and ownership', {
-		as: 'permissive',
-		to: authenticatedRole,
-		for: 'select',
-		using: sql`
-      (
-        (SELECT auth.uid()) = "assignedUserId" OR 
-        (SELECT auth.uid())::uuid = ANY("assignedUserIdsTeam") OR
-        (SELECT auth.uid()) = "createdByUserId"
-      )
-      OR
-      (
-        "driverNameForExtraInfo" = 'sales' AND 
-        has_permission((SELECT auth.uid()), 'select::pipeline::sales2')
-      )
-    `,
+
+	const schema1 = {
+		authenticatedRole,
+		users: pgTable('users', {
+			id: integer('id').primaryKey(),
+		}, (t) => [
+			pgPolicy('policy 1', {
+				as: 'permissive',
+				to: authenticatedRole,
+				for: 'select',
+				using: sql`
+(
+	(SELECT current_setting('auth.uid', true)) = 'some-user-id'
+)
+`,
+			}),
+			pgPolicy('policy 2', {
+				as: 'permissive',
+				to: authenticatedRole,
+				for: 'insert',
+				withCheck: sql`((SELECT current_setting('auth.uid', true)) = 'some-user-id')`,
+			}),
+			pgPolicy('policy 3', {
+				as: 'permissive',
+				to: authenticatedRole,
+				for: 'update',
+				using: sql`((SELECT current_setting('auth.uid', true)) = 'some-user-id')`,
+				withCheck: sql`((SELECT current_setting('auth.uid', true)) = 'some-user-id')`,
+			}),
+			pgPolicy('policy 4', {
+				as: 'permissive',
+				to: authenticatedRole,
+				for: 'delete',
+				using: sql`((SELECT current_setting('auth.uid', true)) = 'some-user-id')`,
+			}),
+		]),
+	};
+
+	const { sqlStatements: st1, next: n1 } = await diff({}, schema1, []);
+
+	await db.query(`SET auth.uid = 'some-user-id';`);
+	const { sqlStatements: pst1 } = await push({
+		db,
+		to: schema1,
+		entities: { roles: { include: [authenticatedRole.name] } },
 	});
-	const policyForInsert = pgPolicy('Only owners and team can insert task data', {
-		as: 'permissive',
-		to: authenticatedRole,
-		for: 'insert',
-		withCheck: sql`(SELECT auth.uid()) = "assignedUserId" OR (SELECT auth.uid())::uuid = ANY("assignedUserIdsTeam")`,
+	const expectedSt1 = [
+		'CREATE ROLE "authenticated";',
+		'CREATE TABLE "users" (\n\t"id" integer PRIMARY KEY\n);\n',
+		'ALTER TABLE "users" ENABLE ROW LEVEL SECURITY;',
+		'CREATE POLICY "policy 1" ON "users" AS PERMISSIVE FOR SELECT TO "authenticated" USING (\n'
+		+ '(\n'
+		+ "\t(SELECT current_setting('auth.uid', true)) = 'some-user-id'\n"
+		+ ')\n'
+		+ ');',
+		`CREATE POLICY "policy 2" ON "users" AS PERMISSIVE FOR INSERT TO "authenticated" WITH CHECK (((SELECT current_setting('auth.uid', true)) = 'some-user-id'));`,
+		`CREATE POLICY "policy 3" ON "users" AS PERMISSIVE FOR UPDATE TO "authenticated" USING (((SELECT current_setting('auth.uid', true)) = 'some-user-id')) WITH CHECK (((SELECT current_setting('auth.uid', true)) = 'some-user-id'));`,
+		`CREATE POLICY "policy 4" ON "users" AS PERMISSIVE FOR DELETE TO "authenticated" USING (((SELECT current_setting('auth.uid', true)) = 'some-user-id'));`,
+	];
+	expect(st1).toStrictEqual(expectedSt1);
+	expect(pst1).toStrictEqual(expectedSt1);
+
+	const schema2 = {
+		authenticatedRole,
+		users: pgTable('users', {
+			id: integer('id').primaryKey(),
+		}, (t) => [
+			pgPolicy('policy 1', {
+				as: 'permissive',
+				to: authenticatedRole,
+				for: 'select',
+				using: sql`
+(
+	(SELECT current_setting('auth.uid', true)) = 'some-user-id1'
+)
+`,
+			}),
+			pgPolicy('policy 2', {
+				as: 'permissive',
+				to: authenticatedRole,
+				for: 'insert',
+				withCheck: sql`((SELECT current_setting('auth.uid', true)) = 'some-user-id1')`,
+			}),
+			pgPolicy('policy 3', {
+				as: 'permissive',
+				to: authenticatedRole,
+				for: 'update',
+				using: sql`((SELECT current_setting('auth.uid', true)) = 'some-user-id1')`,
+				withCheck: sql`((SELECT current_setting('auth.uid', true)) = 'some-user-id1')`,
+			}),
+			pgPolicy('policy 4', {
+				as: 'permissive',
+				to: authenticatedRole,
+				for: 'delete',
+				using: sql`((SELECT current_setting('auth.uid', true)) = 'some-user-id1')`,
+			}),
+		]),
+	};
+
+	const { sqlStatements: st2, next: n2 } = await diff(n1, schema2, []);
+	console.log(st2);
+
+	const { sqlStatements: pst2 } = await push({
+		db,
+		to: schema2,
+		entities: { roles: { include: [authenticatedRole.name] } },
 	});
-	const policyForUpdate = pgPolicy('Only owners and team can update task data', {
-		as: 'permissive',
-		to: authenticatedRole,
-		for: 'update',
-		using: sql`(SELECT auth.uid()) = "assignedUserId" OR (SELECT auth.uid())::uuid = ANY("assignedUserIdsTeam")`,
-		withCheck: sql`(SELECT auth.uid()) = "assignedUserId" OR (SELECT auth.uid())::uuid = ANY("assignedUserIdsTeam")`,
+
+	const expectedSt2 = [
+		'ALTER POLICY "policy 1" ON "users" TO "authenticated" USING (\n'
+		+ '(\n'
+		+ "\t(SELECT current_setting('auth.uid', true)) = 'some-user-id1'\n"
+		+ ')\n'
+		+ ');',
+		`ALTER POLICY "policy 2" ON "users" TO "authenticated" WITH CHECK (((SELECT current_setting('auth.uid', true)) = 'some-user-id1'));`,
+		`ALTER POLICY "policy 3" ON "users" TO "authenticated" USING (((SELECT current_setting('auth.uid', true)) = 'some-user-id1')) WITH CHECK (((SELECT current_setting('auth.uid', true)) = 'some-user-id1'));`,
+		`ALTER POLICY "policy 4" ON "users" TO "authenticated" USING (((SELECT current_setting('auth.uid', true)) = 'some-user-id1'));`,
+	];
+	expect(st2).toStrictEqual(expectedSt2);
+	expect(pst2).toStrictEqual(expectedSt2);
+
+	const { sqlStatements: st3 } = await diff(n2, schema2, []);
+
+	const { sqlStatements: pst3 } = await push({
+		db,
+		to: schema2,
+		entities: { roles: { include: [authenticatedRole.name] } },
 	});
-	const policyForDelete = pgPolicy('Only owners and team can delete task data', {
-		as: 'permissive',
-		to: authenticatedRole,
-		for: 'delete',
-		using: sql`(SELECT auth.uid()) = "assignedUserId" OR (SELECT auth.uid())::uuid = ANY("assignedUserIdsTeam")`,
-	});
+
+	expect(st3).toStrictEqual([]);
+	expect(pst3).toStrictEqual([]);
+
+	await db.query(`RESET auth.uid;`);
 });
 
 // https://github.com/drizzle-team/drizzle-orm/issues/4198
