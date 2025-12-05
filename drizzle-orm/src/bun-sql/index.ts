247d74ea18849df83bc6002f0ec9e33b5000caa6
Sven Rogge <mail@sven-rogge.com>
2025-12-05 11:13:15 +0100

    Enable SQLite usage via Bun SQL

--- drizzle-orm/src/bun-sql/index.ts
+++ drizzle-orm/src/bun-sql/index.ts
@@ -1,2 +1,8 @@
-export * from './driver.ts';
-export * from './session.ts';
+export { BunSQLPGDatabase as BunSQLDatabase, drizzle } from './pg/driver.ts';
+export {
+	BunSQLPGPreparedQuery as BunSQLPreparedQuery,
+	type BunSQLPGQueryResultHKT as BunSQLQueryResultHKT,
+	BunSQLPGSession as BunSQLSession,
+	type BunSQLPGSessionOptions as BunSQLSessionOptions,
+	BunSQLPGTransaction as BunSQLTransaction,
+} from './pg/session.ts';
