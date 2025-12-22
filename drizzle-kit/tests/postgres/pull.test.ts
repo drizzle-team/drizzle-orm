82d5c456b73254c747f31fe792f46a258eaca1f4
AndriiSherman <andreysherman11@gmail.com>
2025-12-22 10:01:40 +0200

    Update postpone

--- drizzle-kit/tests/postgres/pull.test.ts
+++ drizzle-kit/tests/postgres/pull.test.ts
@@ -1270,10 +1270,7 @@ test('introspect view with table filter', async () => {
 });
 
 // https://github.com/drizzle-team/drizzle-orm/issues/4144
-test('introspect sequences with table filter', async () => {
-	// postpone cc: @AlexSherman
-	if (Date.now() < +new Date('2025-12-20')) return;
-
+test.skipIf(Date.now() < +new Date('2026-01-15'))('introspect sequences with table filter', async () => {
 	// can filter sequences with select pg_get_serial_sequence('"schema_name"."table_name"', 'column_name')
 
 	// const seq1 = pgSequence('seq1');
