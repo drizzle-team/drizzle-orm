5b1901aa946de3be3591ee7abfad06663da6cede
OleksiiKH0240 <homenko0240@gmail.com>
2026-01-12 18:50:31 +0200

    added tests

--- integration-tests/tests/pg/common-rqb.ts
+++ integration-tests/tests/pg/common-rqb.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/consistent-type-imports
 import { and, eq, inArray, not, sql } from 'drizzle-orm';
 import type { PgColumnBuilder } from 'drizzle-orm/pg-core';
-import { integer, pgEnum, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { bigint, integer, pgEnum, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 import { describe, expect, expectTypeOf } from 'vitest';
 import type { Test } from './instrumentation';
 
@@ -973,5 +973,26 @@ export function tests(test: Test) {
 				});
 			},
 		);
+
+		// https://github.com/drizzle-team/drizzle-orm/issues/5227
+		test.concurrent('select with bigint array in inArray', async ({ db, push }) => {
+			const users = pgTable('users_110', {
+				id: bigint('id', { mode: 'bigint' }).primaryKey(),
+				name: text('name').notNull(),
+			});
+
+			await push({ users });
+
+			await db.insert(users).values([{ id: 1n, name: 'John' }, { id: 2n, name: 'Jane' }, {
+				id: 9223372036854775807n,
+				name: 'Jane',
+			}]);
+			const result = await db
+				.select({ name: users.name })
+				.from(users)
+				.where(inArray(users.id, [9223372036854775807n, 2n]));
+
+			expect(result).toEqual([{ name: 'Jane' }, { name: 'Jane' }]);
+		});
 	});
 }
