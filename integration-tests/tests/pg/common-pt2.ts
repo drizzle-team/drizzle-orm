df1d5d038ac8ff050978e8e3678c5e47867c90b6
Aleksandr Sherman <aleksandrserman@gmail.com>
2026-01-19 12:22:44 +0200

    merged beta-fixes and updated changelog for orm

--- integration-tests/tests/pg/common-pt2.ts
+++ integration-tests/tests/pg/common-pt2.ts
@@ -2613,25 +2613,6 @@ export function tests(test: Test) {
 			);
 		});
 
-		// https://github.com/drizzle-team/drizzle-orm/issues/5049
-		test('view #3', async ({ db }) => {
-			const table1 = pgTable('table1', {
-				id: integer(),
-				name: text(),
-			});
-
-			const view1 = pgView('view1').as((qb) => qb.select().from(table1));
-
-			const query = db.select({
-				id: view1.id,
-				name: view1.name,
-			}).from(view1)
-				.innerJoin(table1, eq(view1.id, table1.id));
-			expect(query.toSQL().sql).toEqual(
-				'select "view1"."id", "view1"."name" from "view1" inner join "table1" on "view1"."id" = "table1"."id"',
-			);
-		});
-
 		test.concurrent('select from a many subquery', async ({ db, push }) => {
 			const citiesTable = pgTable('cities_many_subquery', {
 				id: serial('id').primaryKey(),
