6386987e27a028f8133c1d9100744b3ba4e65bf0
Aleksandr Sherman <102579553+AleksandrSherman@users.noreply.github.com>
2026-03-12 22:59:03 +0200

    support node sqlite for kit (#5475)
    * added node:sqlite support driver to drizzle-kit
    
    * fix
    
    * updated compatibilityVersion
    
    * added node version warn for node:sqlite

--- drizzle-orm/src/version.ts
+++ drizzle-orm/src/version.ts
@@ -2,4 +2,5 @@
 export { version as npmVersion } from '../package.json';
 // In version 7, we changed the PostgreSQL indexes API
 // In version 12, we changed the migration folder structure and migrate function
-export const compatibilityVersion = 12;
+// In version 13, we added node-sqlite driver support in orm and kit
+export const compatibilityVersion = 13;
