06ebf67a40c685badf32d271e737ed2acd94b2e4
Sukairo-02 <sreka9056@gmail.com>
2026-03-16 20:45:41 +0200

    Switched to compatible `tsdown` ver

--- drizzle-orm/scripts/build.ts
+++ drizzle-orm/scripts/build.ts
@@ -63,9 +63,7 @@ async function main() {
 		format: ['cjs', 'es'],
 		unbundle: true,
 		platform: 'node',
-		deps: {
-			neverBundle: [/^[^./]/], // everything?
-		},
+		external: [/^[^./]/], // everything?
 		tsconfig: 'tsconfig.json',
 		sourcemap: true,
 		dts: true,
