905b79a60ec4399c97f1fe3bc098b3f1138783a8
AndriiSherman <andreysherman11@gmail.com>
2026-03-17 12:02:46 +0200

    remove as number

--- drizzle-kit/src/cli/utils.ts
+++ drizzle-kit/src/cli/utils.ts
@@ -81,7 +81,7 @@ export const assertOrmCoreVersion = async () => {
 
 		await import('drizzle-orm/_relations');
 
-		if (compatibilityVersion && compatibilityVersion as number === requiredApiVersion as number) {
+		if (compatibilityVersion && compatibilityVersion === requiredApiVersion) {
 			return;
 		}
 
