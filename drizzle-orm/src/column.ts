beaf0dfe66e7b3cbab58019a655c216ceb283078
zzinterest <dasdass0003@gmail.com>
2026-03-29 13:53:36 +0200

    fix: [FEATURE]: Default select for custom types (issue #1083)

--- drizzle-orm/src/column.ts
+++ drizzle-orm/src/column.ts
@@ -1,148 +1,96 @@
-import type {
-	ColumnBuilderBaseConfig,
-	ColumnBuilderRuntimeConfig,
-	ColumnDataType,
-	GeneratedColumnConfig,
-	GeneratedIdentityConfig,
-} from './column-builder.ts';
+import type { ColumnBuilderBaseConfig, ColumnBuilderRuntimeConfig, CustomTypeAnyConfig } from './column-builder.ts';
+import type { SQL } from './sql/sql.ts';
 import { entityKind } from './entity.ts';
-import type { DriverValueMapper, SQL, SQLWrapper } from './sql/sql.ts';
-import type { Table } from './table.ts';
-import type { Update } from './utils.ts';
+import type { AnyTable } from './table.ts';
+import type { Assume, IfNotAny } from './utils.ts';
 
-export interface ColumnBaseConfig<
-	TDataType extends ColumnDataType,
-	TColumnType extends string,
-> extends ColumnBuilderBaseConfig<TDataType, TColumnType> {
+export interface ColumnBaseConfig<TDataType extends ColumnDataType, TColumnType extends string> {
+	name: string;
 	tableName: string;
+	dataType: TDataType;
+	columnType: TColumnType;
+	data: unknown;
+	driverParam: unknown;
+	enumValues: TDataType extends 'enum' ? string[] : undefined;
 	notNull: boolean;
 	hasDefault: boolean;
-	isPrimaryKey: boolean;
-	isAutoincrement: boolean;
-	hasRuntimeDefault: boolean;
+	primaryKey: boolean;
+	unique: boolean;
 }
 
-export type ColumnTypeConfig<T extends ColumnBaseConfig<ColumnDataType, string>, TTypeConfig extends object> = T & {
-	brand: 'Column';
+export type ColumnDataType =
+	| 'array'
+	| 'bigint'
+	| 'boolean'
+	| 'custom'
+	| 'date'
+	| 'enum'
+	| 'json'
+	| 'number'
+	| 'string'
+	| 'text'
+	| 'time'
+	| 'timestamp'
+	| 'uuid';
+
+export type GetColumnData<TColumn extends AnyColumn, TType = unknown> = TColumn['_']['data'] extends TType
+	? TColumn['_']['data']
+	: never;
+
+export type AnyColumn<
+	T extends {
+		tableName?: string;
+		dataType?: ColumnDataType;
+	} = {},
+> = Column<{
 	tableName: T['tableName'];
-	name: T['name'];
 	dataType: T['dataType'];
-	columnType: T['columnType'];
-	data: T['data'];
-	driverParam: T['driverParam'];
-	notNull: T['notNull'];
-	hasDefault: T['hasDefault'];
-	isPrimaryKey: T['isPrimaryKey'];
-	isAutoincrement: T['isAutoincrement'];
-	hasRuntimeDefault: T['hasRuntimeDefault'];
-	enumValues: T['enumValues'];
-	baseColumn: T extends { baseColumn: infer U } ? U : unknown;
-	generated: GeneratedColumnConfig<T['data']> | undefined;
-	identity: undefined | 'always' | 'byDefault';
-} & TTypeConfig;
+}>;
 
-export type ColumnRuntimeConfig<TData, TRuntimeConfig extends object> = ColumnBuilderRuntimeConfig<
-	TData,
-	TRuntimeConfig
->;
-
-export interface Column<
-	T extends ColumnBaseConfig<ColumnDataType, string> = ColumnBaseConfig<ColumnDataType, string>,
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	TRuntimeConfig extends object = object,
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	TTypeConfig extends object = object,
-> extends DriverValueMapper<T['data'], T['driverParam']>, SQLWrapper {
-	// SQLWrapper runtime implementation is defined in 'sql/sql.ts'
-}
-/*
-	`Column` only accepts a full `ColumnConfig` as its generic.
-	To infer parts of the config, use `AnyColumn` that accepts a partial config.
-	See `GetColumnData` for example usage of inferring.
-*/
-export abstract class Column<
-	T extends ColumnBaseConfig<ColumnDataType, string> = ColumnBaseConfig<ColumnDataType, string>,
-	TRuntimeConfig extends object = object,
-	TTypeConfig extends object = object,
-> implements DriverValueMapper<T['data'], T['driverParam']>, SQLWrapper {
+export class Column<T extends ColumnBaseConfig<ColumnDataType, string>> {
 	static readonly [entityKind]: string = 'Column';
 
-	declare readonly _: ColumnTypeConfig<T, TTypeConfig>;
+	declare protected $brand: 'Column';
 
-	readonly name: string;
-	readonly keyAsName: boolean;
-	readonly primary: boolean;
-	readonly notNull: boolean;
-	readonly default: T['data'] | SQL | undefined;
-	readonly defaultFn: (() => T['data'] | SQL) | undefined;
-	readonly onUpdateFn: (() => T['data'] | SQL) | undefined;
-	readonly hasDefault: boolean;
-	readonly isUnique: boolean;
-	readonly uniqueName: string | undefined;
-	readonly uniqueType: string | undefined;
-	readonly dataType: T['dataType'];
-	readonly columnType: T['columnType'];
-	readonly enumValues: T['enumValues'] = undefined;
-	readonly generated: GeneratedColumnConfig<T['data']> | undefined = undefined;
-	readonly generatedIdentity: GeneratedIdentityConfig | undefined = undefined;
+	/** @internal */
+	readonly config: ColumnBuilderRuntimeConfig<T>;
 
-	protected config: ColumnRuntimeConfig<T['data'], TRuntimeConfig>;
-
-	constructor(
-		readonly table: Table,
-		config: ColumnRuntimeConfig<T['data'], TRuntimeConfig>,
-	) {
-		this.config = config;
-		this.name = config.name;
-		this.keyAsName = config.keyAsName;
-		this.notNull = config.notNull;
-		this.default = config.default;
-		this.defaultFn = config.defaultFn;
-		this.onUpdateFn = config.onUpdateFn;
-		this.hasDefault = config.hasDefault;
-		this.primary = config.primaryKey;
-		this.isUnique = config.isUnique;
-		this.uniqueName = config.uniqueName;
-		this.uniqueType = config.uniqueType;
-		this.dataType = config.dataType as T['dataType'];
-		this.columnType = config.columnType;
-		this.generated = config.generated;
-		this.generatedIdentity = config.generatedIdentity;
+	constructor(table: AnyTable<{ name: string }>, config: ColumnBuilderRuntimeConfig<T>) {
+		this.config = {
+			...config,
+			tableName: table._.name,
+		};
 	}
 
-	abstract getSQLType(): string;
-
-	mapFromDriverValue(value: unknown): unknown {
-		return value;
+	getSQLType(): string {
+		return this.config.columnType;
 	}
 
-	mapToDriverValue(value: unknown): unknown {
-		return value;
+	/** @internal */
+	getDataType(): T['dataType'] {
+		return this.config.dataType;
 	}
 
-	// ** @internal */
-	shouldDisableInsert(): boolean {
-		return this.config.generated !== undefined && this.config.generated.type !== 'byDefault';
+	mapToDriverValue(value: T['data']): T['driverParam'] {
+		return value as T['driverParam'];
 	}
+
+	mapFromDriverValue(value: T['driverParam']): T['data'] {
+		return value as T['data'];
+	}
+
+	/** @internal */
+	abstract getSQLValueType(): string;
 }
+
+export type AnyCustomColumn = Column<{
+	dataType: 'custom';
+	columnType: string;
+	driverParam: unknown;
+	data: unknown;
+}>;
 
 export type UpdateColConfig<
 	T extends ColumnBaseConfig<ColumnDataType, string>,
-	TUpdate extends Partial<ColumnBaseConfig<ColumnDataType, string>>,
-> = Update<T, TUpdate>;
-
-export type AnyColumn<TPartial extends Partial<ColumnBaseConfig<ColumnDataType, string>> = {}> = Column<
-	Required<Update<ColumnBaseConfig<ColumnDataType, string>, TPartial>>
->;
-
-export type GetColumnData<TColumn extends Column, TInferMode extends 'query' | 'raw' = 'query'> =
-	// dprint-ignore
-	TInferMode extends 'raw' // Raw mode
-		? TColumn['_']['data'] // Just return the underlying type
-		: TColumn['_']['notNull'] extends true // Query mode
-		? TColumn['_']['data'] // Query mode, not null
-		: TColumn['_']['data'] | null; // Query mode, nullable
-
-export type InferColumnsDataTypes<TColumns extends Record<string, Column>> = {
-	[Key in keyof TColumns]: GetColumnData<TColumns[Key], 'query'>;
-};
+	K extends Partial<ColumnBaseConfig<ColumnDataType, string>>,
+> = ColumnBaseConfig<ColumnDataType, string> & Omit<T, keyof K> & K;
