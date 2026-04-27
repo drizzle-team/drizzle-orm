fdcf2238576adbf2ee2f2be41ab07a4951433c59
Sukairo-02 <sreka9056@gmail.com>
2026-04-27 19:53:42 +0300

    Bumb internal api ver

--- drizzle-orm/src/version.ts
+++ drizzle-orm/src/version.ts
@@ -3,4 +3,5 @@ export { version as npmVersion } from '../package.json';
 // In version 7, we changed the PostgreSQL indexes API
 // In version 12, we changed the migration folder structure and migrate function
 // In version 13, we added node-sqlite driver support in orm and kit
-export const compatibilityVersion = 13;
+// In version 14, we remade casing api (db instance's config => db entity constructors)
+export const compatibilityVersion = 14;
