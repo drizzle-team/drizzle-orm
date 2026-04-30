70a7c4461423a72eec4a688b8426f9e8bf825b62
Dan Kochetov <danil.kochetov@gmail.com>
2026-04-30 23:37:42 +0300

    Restructure push-mysql aborted block to interactive-only

--- drizzle-kit/src/cli/commands/push-mysql.ts
+++ drizzle-kit/src/cli/commands/push-mysql.ts
@@ -110,11 +110,7 @@ export const handle = async (
 		return;
 	}
 
-	if (!force && suggestionHints.length > 0) {
-		if (json) {
-			printJsonOutput(abortedJsonOutput('mysql', suggestionHints));
-			process.exit(0);
-		}
+	if (!force && !json && suggestionHints.length > 0) {
 		const { data } = await render(new Select(['No, abort', 'Yes, I want to execute all statements']));
 
 		if (data?.index === 0) {
