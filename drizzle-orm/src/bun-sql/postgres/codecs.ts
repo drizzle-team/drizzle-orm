93e0847f71c14284a051f456bdb8345ae477f145
Sukairo-02 <sreka9056@gmail.com>
2026-05-01 19:49:32 +0300

    minor fixes

--- drizzle-orm/src/bun-sql/postgres/codecs.ts
+++ drizzle-orm/src/bun-sql/postgres/codecs.ts
@@ -100,6 +100,8 @@ export const bunSqlPgCodecs = refineGenericPgCodecs({
 	datemultirange: { normalizeParamArray: makePgArray },
 	daterange: { normalizeParamArray: makePgArray },
 	float8: { normalizeParamArray: makePgArray },
+	'geography(point)': { normalizeParamArray: makePgArray },
+	'geography(point):tuple': { normalizeParamArray: makePgArray },
 	inet: { normalizeParamArray: makePgArray },
 	int4multirange: { normalizeParamArray: makePgArray },
 	int4range: { normalizeParamArray: makePgArray },
