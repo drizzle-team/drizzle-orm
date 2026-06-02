6c6d16a9a5e6cb4c33c732a041037a5307778e6c
Sukairo-02 <sreka9056@gmail.com>
2026-06-02 09:07:48 +0300

    Import fix

--- drizzle-orm/src/tursodatabase-serverless/driver.ts
+++ drizzle-orm/src/tursodatabase-serverless/driver.ts
@@ -2,9 +2,9 @@ import { connect, type Connection, type Statement } from '@tursodatabase/serverl
 import { entityKind } from '~/entity.ts';
 import { DefaultLogger } from '~/logger.ts';
 import type { AnyRelations, EmptyRelations } from '~/relations.ts';
-import type { DrizzleSQLiteConfig } from '~/sqlite-core';
 import { BaseSQLiteDatabase } from '~/sqlite-core/db.ts';
 import { SQLiteAsyncDialect } from '~/sqlite-core/dialect.ts';
+import type { DrizzleSQLiteConfig } from '~/sqlite-core/utils.ts';
 import { jitCompatCheck } from '~/utils.ts';
 import { TursoDatabaseServerlessSession } from './session.ts';
 
