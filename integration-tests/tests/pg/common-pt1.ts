d20e720c7200208d9e249ae230ef6936407cb007
Sukairo-02 <sreka9056@gmail.com>
2026-06-24 12:14:50 +0300

    Extended test postpone date, fixed test schemas

--- integration-tests/tests/pg/common-pt1.ts
+++ integration-tests/tests/pg/common-pt1.ts
@@ -1066,7 +1066,7 @@ export function tests(test: Test) {
 		});
 
 		// https://github.com/drizzle-team/drizzle-orm/issues/2872
-		test.skipIf(Date.now() < +new Date('2026-06-24')).concurrent(
+		test.skipIf(Date.now() < +new Date('2026-07-01')).concurrent(
 			'prepared statement with placeholder in .inArray',
 			async ({ db, push }) => {
 				const usersTable = pgTable('users_392', {
