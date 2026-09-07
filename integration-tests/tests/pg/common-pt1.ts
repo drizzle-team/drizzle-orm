0764bab5b37c738b2935e8708b7e5222f4b0218e
Alex Sherman <aleksandrserman@gmail.com>
2026-09-07 11:44:29 +0300

    postponed tests

--- integration-tests/tests/pg/common-pt1.ts
+++ integration-tests/tests/pg/common-pt1.ts
@@ -1235,7 +1235,7 @@ export function tests(test: Test) {
 		});
 
 		// https://github.com/drizzle-team/drizzle-orm/issues/2872
-		test.skipIf(Date.now() < +new Date('2026-09-05')).concurrent(
+		test.skipIf(Date.now() < +new Date('2026-09-12')).concurrent(
 			'prepared statement with placeholder in .inArray',
 			async ({ db, push }) => {
 				const usersTable = pgTable('users_392', {
