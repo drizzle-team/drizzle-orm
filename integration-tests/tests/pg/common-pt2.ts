32a8fd8732a524ce1cb86eb04e0c299efee8530f
Sukairo-02 <sreka9056@gmail.com>
2026-09-16 04:31:48 +0300

    Fixed drizzle constructor breaking postgres-js serializers (fixes #3171, #4426, #5789)

--- integration-tests/tests/pg/common-pt2.ts
+++ integration-tests/tests/pg/common-pt2.ts
@@ -59,6 +59,7 @@ import {
 	varchar,
 } from 'drizzle-orm/pg-core';
 import { PgliteDatabase } from 'drizzle-orm/pglite';
+import { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import { describe, expect, expectTypeOf } from 'vitest';
 import {
 	type AllTypes,
@@ -1384,7 +1385,9 @@ export function tests(test: Test) {
 						'insert into "mySchema_15"."users" ("id", "name", "verified", "jsonb", "created_at") values (default, $1, default, $2, default) on conflict ("id","name") do update set "name" = $3',
 					params: [
 						'John',
-						is(db, PgliteDatabase) ? ['foo', 'bar'] : JSON.stringify(['foo', 'bar']),
+						is(db, PgliteDatabase) || is(db, PostgresJsDatabase)
+							? ['foo', 'bar']
+							: JSON.stringify(['foo', 'bar']),
 						'John1',
 					],
 				});
@@ -1416,7 +1419,9 @@ export function tests(test: Test) {
 						'insert into "mySchema_16"."users" ("id", "name", "verified", "jsonb", "created_at") values (default, $1, default, $2, default) on conflict ("id") do nothing',
 					params: [
 						'John',
-						is(db, PgliteDatabase) ? ['foo', 'bar'] : JSON.stringify(['foo', 'bar']),
+						is(db, PgliteDatabase) || is(db, PostgresJsDatabase)
+							? ['foo', 'bar']
+							: JSON.stringify(['foo', 'bar']),
 					],
 				});
 			},
@@ -1748,9 +1753,7 @@ export function tests(test: Test) {
 		});
 
 		// https://github.com/drizzle-team/drizzle-orm/issues/3171
-		// TODO: review case
-		// Fails in `postgres-js` if not inlined - driver expects stringified jsons
-		test.skipIf(Date.now() < +new Date('2026-09-19')).concurrent(
+		test.concurrent(
 			'proper json and jsonb handling - sql operator',
 			async ({ db, push }) => {
 				const jsonTable = pgTable('json_table_sql_3', {
